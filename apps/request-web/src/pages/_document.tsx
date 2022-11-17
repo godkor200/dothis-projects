87d4e8d4e816e125325de87b90a3ab233c56ebd6
hjm1 <rjcnd123@gmail.com>
2022-11-17 15:19:28 +0900

    pwa setting 중

--- apps/request-web/src/pages/_document.tsx
+++ apps/request-web/src/pages/_document.tsx
@@ -4,6 +4,8 @@ import type { DocumentContext } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
+import { colors } from '~/../../packages/share/lib/styles/chakraTheme';
+
 const isBrowser = typeof document !== 'undefined';
 
 // On the client side, Create a meta tag at the top of the <head> and set it as insertionPoint.
@@ -26,6 +28,9 @@ function createEmotionCache() {
     insertionPoint,
   });
 }
+
+const description = '콘텐츠 요청 크리에이터 스폰서쉽 플랫폼 서비스';
+const title = '두디스 요청하기';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -68,12 +73,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <meta name="title" content="DoThis" />
+          <meta name="title" content={title} />
+          <AppMeta />
 
-          <meta
-            name="description"
-            content="콘텐츠 요청 크리에이터 스폰서쉽 플랫폼 서비스"
-          />
+          <meta name="description" content={description} />
           {/*<meta name="emotion-insertion-point" content="" />*/}
           {(this.props as any).emotionStyleTags}
         </Head>
@@ -86,3 +89,73 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export const AppMeta = () => (
+  <>
+    <meta name="application-name" content="두디스 리퀘스트" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-title" content={title} />
+    <meta name="description" content={description} />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+    <meta name="msapplication-TileColor" content={colors.primary.default} />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="theme-color" content={colors.primary.default} />
+
+    <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="/icons/touch-icon-ipad.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/icons/touch-icon-iphone-retina.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="167x167"
+      href="/icons/touch-icon-ipad-retina.png"
+    />
+
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/icons/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/icons/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:url" content="https://dothis.world/" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta
+      name="twitter:image"
+      content="https://yourdomain.com/icons/android-chrome-192x192.png"
+    />
+    <meta name="twitter:creator" content="dothis" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content={title} />
+    <meta
+      property="og:description"
+      content="콘텐츠 요청 크리에이터 스폰서쉽 플랫폼 서비스"
+    />
+    <meta property="og:site_name" content={title} />
+    <meta property="og:url" content="https://dothis.world/" />
+    <meta
+      property="og:image"
+      content="https://yourdomain.com/icons/apple-touch-icon.png"
+    />
+  </>
+);
