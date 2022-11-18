f0c42f8646a63e8fe6850732d6297414f8b10107
godkor200 <godkor200@gmail.com>
2022-11-18 13:56:33 +0900

    수정: 경로 및 환경설정 파일 정리

--- apps/back-end/apps/api/src/user/UserApi.service.ts
+++ apps/back-end/apps/api/src/user/UserApi.service.ts
@@ -1,7 +1,7 @@
-import { User } from '../../../../libs/entity/src/domain/user/User.entity';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '@Libs/entity/src/domain/user/User.entity';
 import { UserApiQueryRepository } from './UserApiQueryRepository';
 
 @Injectable()
