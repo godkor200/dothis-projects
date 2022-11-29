9ddaae27adee264e29a4d88e65de27eded143904
godkor200 <godkor200@gmail.com>
2022-11-29 15:05:39 +0900

    수정 : 환경변수 의존성 추가

--- apps/back-end/libs/entity/src/config/database.mysql.ts
+++ apps/back-end/libs/entity/src/config/database.mysql.ts
@@ -2,21 +2,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../domain/user/User.entity';
 import { Channel } from '../domain/channel/Channel.entity';
 import { DailyViews } from '../domain/daily_views/DailyViews.entity';
+import { Inject } from '@nestjs/common';
+import dbConfig from '@Libs/entity/src/config/db.env';
+import { ConfigType } from '@nestjs/config';
 
-export async function createDatabaseConnection() {
+export class createDatabaseConnection {
   // const entityPath = path.join(
   //   __dirname,
   //   `/../../libs/entity/src/domain/**/*.entity.js`,
   // );
   // console.log(entityPath);
-  return TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: 'root',
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.DB_SCHEMA,
-    entities: [User, Channel, DailyViews],
-    synchronize: false,
-  });
+  db;
+
+  constructor(
+    @Inject(dbConfig.KEY) private config: ConfigType<typeof dbConfig>,
+  ) {
+    this.db = TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.DB_HOST,
+      port: Number(config.DB_PORT),
+      username: 'root',
+      password: config.MYSQL_ROOT_PASSWORD,
+      database: config.DB_SCHEMA,
+      entities: [User, Channel, DailyViews],
+      synchronize: false,
+    });
+  }
+  set() {
+    console.log('!!!!', this.db);
+    return this.db;
+  }
 }
