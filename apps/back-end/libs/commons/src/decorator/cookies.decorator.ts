2003ae58569b1455e978aa062c5553195ffd86f4
godkor200 <godkor200@gmail.com>
2023-01-18 18:28:16 +0900

    수정 : 쿠키 객체화 되도록 수정

--- apps/back-end/libs/commons/src/decorator/cookies.decorator.ts
+++ apps/back-end/libs/commons/src/decorator/cookies.decorator.ts
@@ -2,6 +2,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const Cookies = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.cookies;
+    return request.headers.cookie.split('; ').reduce((prev, current) => {
+      const [name, ...value] = current.split('=');
+      prev[name] = value.join('=');
+      return prev;
+    }, {});
   },
 );
