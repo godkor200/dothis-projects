9f21897c0afcc44389ec36676e9f404f03e0bb75
godkor200 <godkor200@gmail.com>
2023-01-19 17:30:22 +0900

    수정 : 기본 sql쿼리는 선언 필요없어서 주석처리

--- apps/back-end/apps/api/src/user/v1/db/user.repository.port.ts
+++ apps/back-end/apps/api/src/user/v1/db/user.repository.port.ts
@@ -1,5 +1,5 @@
 import { RepositoryPort } from '@Libs/commons/src/ddd/repository.port';
 import { User } from '@Libs/entity/src/domain/user/User.entity';
 export interface UserRepositoryPort extends RepositoryPort<User> {
-  findOneById(id: string): Promise<User>;
+  // findOneById(id: string): Promise<User>;
 }
