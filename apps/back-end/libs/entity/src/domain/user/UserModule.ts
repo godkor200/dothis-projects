f45b42da558b9d5977411106c5aa5c172ddbd6ff
godkor200 <godkor200@gmail.com>
2023-01-19 17:15:46 +0900

    수정 : 쿼리 레포 삭제에 따른 수정

--- apps/back-end/libs/entity/src/domain/user/UserModule.ts
+++ apps/back-end/libs/entity/src/domain/user/UserModule.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { User } from './User.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserQueryRepository } from './UserQueryRepository';
 import { UserChannelData } from '@Libs/entity/src/domain/userChannelData/UserChannelData.entity';
 import { Subscribe } from '@Libs/entity/src/domain/subscribe/Subscribe.entity';
 import { Channel } from '@Libs/entity/src/domain/channel/Channel.entity';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      User,
-      UserChannelData,
-      Subscribe,
-      Channel,
-      UserQueryRepository,
-    ]),
+    TypeOrmModule.forFeature([User, UserChannelData, Subscribe, Channel]),
   ],
   exports: [TypeOrmModule],
   providers: [],
