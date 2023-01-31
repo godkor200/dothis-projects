33dbfc50e7350a0ede8d2207ccc39ca08b0611f3
godkor200 <godkor200@gmail.com>
2023-01-31 13:16:36 +0900

    수정 : 마이그레이션

--- apps/back-end/libs/commons/src/typeorm/migrations/1672194541260-channelIdtypechange.ts
+++ apps/back-end/libs/commons/src/typeorm/migrations/1672194541260-channelIdtypechange.ts
@@ -2,9 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class channelIdtypechange1672194541260 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'alter table UserChannelData modify channel_id varchar(100) not null',
-    );
+    // await queryRunner.query(
+    //   'alter table UserChannelData modify channel_id varchar(100) not null',
+    // );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
