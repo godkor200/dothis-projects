13da9f585b52ceec6a118d68fd4e44e11cdfc45e
godkor200 <godkor200@gmail.com>
2023-02-06 13:50:13 +0900

    수정 : swagger 수정

--- apps/back-end/apps/api/src/user/v1/commands/get-user/get-user.http.controller.ts
+++ apps/back-end/apps/api/src/user/v1/commands/get-user/get-user.http.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { FindUserCommand } from '@Apps/api/src/user/v1/commands/get-user/get-user.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserDto } from '@Libs/commons/src/types/dto.types';
 
+@ApiTags('user')
 @Controller('/user')
 export class GetUserHttpController {
   constructor(private readonly commandBus: CommandBus) {}
@@ -11,13 +18,16 @@ export class GetUserHttpController {
   @Get('/:id')
   @ApiOperation({ summary: '유저를 가져옵니다.' })
   @ApiResponse({
-    description: '유저를 찾습니다.',
+    description: '유저 찾아옵니다.',
     status: HttpStatus.OK,
     type: [UserDto],
   })
-  @ApiResponse({
+  @ApiNotFoundResponse({
     status: HttpStatus.NOT_FOUND,
     description: 'Not Found',
+  })
+  @ApiInternalServerErrorResponse({
+    description: '서버에 문제가 있으면 리턴한다.',
   })
   async getUser(@Param('id') id: string) {
     const command = new FindUserCommand({ userId: id });
