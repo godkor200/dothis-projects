a4e844d777bb3033808e5f89bb1c005065ebf683
godkor200 <godkor200@gmail.com>
2023-02-06 13:50:24 +0900

    수정 : 안쓰는 파일 삭제

--- apps/back-end/libs/entity/src/domain/user/UserQueryRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './User.entity';
-import { CustomRepository } from '@Libs/commons/src/typeorm/type-orm-ext.decorater';
-
-@CustomRepository(User)
-export class UserQueryRepository extends Repository<User> {}
