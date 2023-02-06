482df817513df5b6e12ab4dc21e037bc6d2d452d
godkor200 <godkor200@gmail.com>
2023-02-06 13:49:46 +0900

    수정 : dto 분리에 다른 import 추가

--- apps/back-end/apps/api/src/user/v1/commands/get-channel-data/get-channel-data.service.spec.ts
+++ apps/back-end/apps/api/src/user/v1/commands/get-channel-data/get-channel-data.service.spec.ts
@@ -1,12 +1,10 @@
-import {
-  GetChannelDataCommandDto,
-  GetChannelDataCommandHandler,
-} from './get-channel-data.service';
+import { GetChannelDataCommandHandler } from './get-channel-data.service';
 import {
   ChannelDataMock,
   UserChannelDataMock,
 } from '@Apps/api/src/user/__mock__/get-channel-data.mock';
 import { Channel } from '@Libs/entity/src/domain/channel/Channel.entity';
+import { GetChannelDataCommandDto } from '@Apps/api/src/user/v1/commands/get-channel-data/get-channel-data.command.dto';
 import { google } from 'googleapis';
 
 jest.mock('googleapis', () => {
