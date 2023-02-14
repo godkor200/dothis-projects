f1d7fe577d2e48f36acbd853bab5b9f93f54351c
godkor200 <godkor200@gmail.com>
2023-02-14 22:45:44 +0900

    수정 : 웹팩 파일 추가 수정

--- apps/back-end/webpack.config.js
+++ apps/back-end/webpack.config.js
@@ -9,7 +9,6 @@ const path = require('path');
 const lazyImports = [
   '@nestjs/microservices/microservices-module',
   '@nestjs/websockets/socket-module',
-  '@nestjs/platform-express',
   'class-transformer/storage', // https://github.com/nestjs/mapped-types/issues/486#issuecomment-932715880
 ];
 
@@ -17,17 +16,17 @@ module.exports = (options, webpack) => ({
   ...options,
   mode: 'production',
   target: 'node14',
-  entry: {
-    index: './lib/lambda.js',
-  },
-  output: {
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    path: path.join(process.cwd(), 'build/dist'),
-  },
-  externals: {
-    'aws-sdk': 'aws-sdk',
-  },
+  // entry: {
+  //   index: './lib/lambda.js',
+  // },
+  // output: {
+  //   filename: '[name].js',
+  //   libraryTarget: 'umd',
+  //   path: path.join(process.cwd(), 'build/dist'),
+  // },
+  // externals: {
+  //   'aws-sdk': 'aws-sdk',
+  // },
   plugins: [
     ...options.plugins,
     new webpack.IgnorePlugin({
