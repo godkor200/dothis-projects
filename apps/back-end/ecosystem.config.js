26edcc92d045d2eeabb44072493131bbc019bd11
godkor200 <godkor200@gmail.com>
2023-02-15 14:20:44 +0900

    수정 : ec2 인스턴스 로컬 디비에도 접속 할수 있도록 수정

--- apps/back-end/ecosystem.config.js
+++ apps/back-end/ecosystem.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, 'production.env') });
+require('dotenv').config({
+  path: path.join(
+    __dirname,
+    process.env.NODE_ENV === 'development'
+      ? 'development.env'
+      : 'production.env',
+  ),
+});
 
 module.exports = {
   apps: [
     {
       name: 'dothis',
       cwd: './dist/apps/api',
-      script: 'ts-node ./main.js',
+      script: './main.js',
       instances: 2,
       exec_mode: 'cluster',
       autorestart: true,
@@ -21,7 +28,7 @@ module.exports = {
         MYSQL_USER: process.env.MYSQL_USER,
         MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
         DB_PORT: process.env.DB_PORT,
-        DB_HOST: process.env.DB_HOST,
+        DB_HOST: process.env.DB_HOST_LOCAL,
         DB_SCHEMA: process.env.DB_SCHEMA,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
