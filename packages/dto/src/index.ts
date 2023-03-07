cd5d32844586cbae266696a281a5354eb75a5726
godkor200 <godkor200@gmail.com>
2023-03-07 18:40:27 +0900

    수정 : @dothis/dto 만의 불러오끔 수정

--- packages/dto/src/index.ts
+++ packages/dto/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/auth';
-export * from './lib/channel/channel.model';
+export * from './lib/channel';
 export * from './lib/user';
 export * from './lib/user-channel-data';
-export * from './lib/contract';
 export * from './lib/apiRouter';
-//# sourceMappingURL=index.d.ts.map
+export * from './lib/contract';
