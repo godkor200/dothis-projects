ba0b6b6ed5dd024a8399e70212a6a7f0dabd4f96
godkor200 <godkor200@gmail.com>
2023-03-08 11:54:02 +0900

    캐쉬 제거

--- packages/dto/src/lib/contract.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.c = void 0;
-const core_1 = require("@ts-rest/core");
-exports.c = (0, core_1.initContract)();
-//# sourceMappingURL=contract.js.map
