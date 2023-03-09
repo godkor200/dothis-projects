a58bb3da9a0c4036b7c4776928e4375b2d8967fb
godkor200 <godkor200@gmail.com>
2023-03-09 12:59:43 +0900

    수정 : 탐색어 업데이트 자체 로그 추가

--- apps/api/src/modules/channel/v1/commands/get-dic-search-term/get-dic-searth-term.service.ts
+++ apps/api/src/modules/channel/v1/commands/get-dic-search-term/get-dic-searth-term.service.ts
@@ -1,15 +1,16 @@
-import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { CHANNEL_DATA_REPOSITORY } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
 import { ChannelDataRepositoryPost } from '@Apps/modules/channel/v1/db/channel-data.repository.post';
-
 @Injectable()
 export class GetDicSearchTermCommandHandler {
+  private readonly logger = new Logger(GetDicSearchTermCommandHandler.name);
   constructor(
     @Inject(CHANNEL_DATA_REPOSITORY)
     protected readonly channelDataRepo: ChannelDataRepositoryPost,
   ) {}
+
   async execute() {
+    this.logger.log('탐색어 업데이트 시작합니다.');
     const arr = await this.channelDataRepo.findAll();
     return arr
       .map((ev) => [...ev.tag.split(','), ...ev.keyword.split(',')])
