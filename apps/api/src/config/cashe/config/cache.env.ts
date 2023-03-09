73c405a14e05d3dd157d169d7b03323104c80126
godkor200 <godkor200@gmail.com>
2023-03-09 13:01:25 +0900

    수정 : 케쉬 관련 삭제

--- apps/api/src/config/cashe/config/cache.env.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('cache', () => ({
-  REDIS_PORT: process.env.REDIS_PORT,
-  REDIS_HOSTNAME: process.env.REDIS_HOSTNAME ?? 'localhost',
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-}));
