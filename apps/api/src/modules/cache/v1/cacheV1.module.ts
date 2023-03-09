d9d283d06b6b65529227ce7ab0bcd0e288cccf66
godkor200 <godkor200@gmail.com>
2023-03-09 12:44:26 +0900

    수정 : CACHE_SET_DIC_TERM 토큰 추가

--- apps/api/src/modules/cache/v1/cacheV1.module.ts
+++ apps/api/src/modules/cache/v1/cacheV1.module.ts
@@ -1,9 +1,14 @@
 import { Module, Provider } from '@nestjs/common';
 import { FindDicTermHandler } from '@Apps/modules/cache/v1/queries/find-dic-term/find-dic-term.handler';
 import { CqrsModule } from '@nestjs/cqrs';
-import { CACHE_FIND_ALL_QUERY } from '@Apps/modules/cache/constants/cache.contants';
+import {
+  CACHE_FIND_ALL_QUERY,
+  CACHE_SET_DIC_TERM,
+} from '@Apps/modules/cache/constants/cache.di-token';
 import { FindDicTermImplement } from '@Apps/modules/cache/v1/infra/find-dic-term.implement';
-import { FindDicTermService } from "@Apps/modules/cache/v1/queries/find-dic-term/find-dic-term.service";
+import { FindDicTermService } from '@Apps/modules/cache/v1/queries/find-dic-term/find-dic-term.service';
+import { SetDicTermImplement } from '@Apps/modules/cache/v1/infra/set-dic-term.implement';
+import { SetDicTermHandler } from '@Apps/modules/cache/v1/commands/set-dic-term/set-dic-term.service';
 
 const controllers = [FindDicTermHandler];
 
@@ -12,10 +17,14 @@ const infra: Provider[] = [
     provide: CACHE_FIND_ALL_QUERY,
     useClass: FindDicTermImplement,
   },
-  FindDicTermService
+  {
+    provide: CACHE_SET_DIC_TERM,
+    useClass: SetDicTermImplement,
+  },
+  FindDicTermService,
 ];
 
-const providers: Provider[] = [...infra];
+const providers: Provider[] = [...infra, SetDicTermHandler];
 
 @Module({
   imports: [CqrsModule],
