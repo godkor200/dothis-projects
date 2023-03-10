5776d8211754e95cbb2e2e7e945ae77435860753
hoejun <rjcnd123@gmail.com>
2023-03-10 18:09:34 +0900

    storybook nextjs 방식으로 변경, 패키지 업데이트로 인한 버그 수정 (#261)

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -2,5 +2,4 @@ export * from './useAnimateNumber';
 export * from './useBoolean';
 export * from './useIsomorphicLayoutEffect';
 export * from './useMatch';
-export * from './useParsedQuery';
 export * from './useScroll';
