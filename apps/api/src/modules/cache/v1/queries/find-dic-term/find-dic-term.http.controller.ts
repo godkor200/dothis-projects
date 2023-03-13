5117d86b4c4164f3556bc3d4b5f78e690e9f2d49
godkor200 <godkor200@gmail.com>
2023-03-13 16:53:30 +0900

    수정 : swagger 정의

--- apps/api/src/modules/cache/v1/queries/find-dic-term/find-dic-term.http.controller.ts
+++ apps/api/src/modules/cache/v1/queries/find-dic-term/find-dic-term.http.controller.ts
@@ -1,12 +1,30 @@
-import { IQueryHandler, QueryBus, QueryHandler } from '@nestjs/cqrs';
+import { QueryBus } from '@nestjs/cqrs';
 import { FindDicTermQuery } from '@Apps/modules/cache/v1/queries/find-dic-term/find-dic-term.query';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
 import { FindDicTermRes } from '@Apps/modules/cache/v1/queries/find-dic-term/find-dic-term.res';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { apiRouter, cacheBaseApiUrl } from '@dothis/dto';
+import { nestControllerContract, TsRest } from '@ts-rest/nest';
+const { get } = nestControllerContract(apiRouter.cache);
+const { description, summary, responses } = get;
 
-@Controller('/cache')
+@Controller()
 export class FindDicTermHttpController {
   constructor(private readonly queryBus: QueryBus) {}
-  @Get('term')
+  @ApiTags(cacheBaseApiUrl)
+  @ApiOkResponse({ description: responses[200] })
+  @ApiUnauthorizedResponse({ description: responses[401] })
+  @ApiInternalServerErrorResponse({ description: responses[500] })
+  @ApiQuery({ name: 'key', examples: { key: { value: 'dic-term' } } })
+  @ApiOperation({ summary, description })
+  @TsRest(get)
   async handler(
     @Query() queryParams: FindDicTermQuery,
   ): Promise<FindDicTermRes> {
