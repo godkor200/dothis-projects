4ae3108df11e79e9cf99f6ce09a70a7d851d5ffd
godkor200 <godkor200@gmail.com>
2023-04-07 13:51:37 +0900

    feat : 관련어 불러오기 api share

--- packages/dto/src/lib/apiRouter.ts
+++ packages/dto/src/lib/apiRouter.ts
@@ -2,9 +2,11 @@ import { c } from './contract';
 import { userApi } from './user';
 import { authApi } from './auth';
 import { cacheApi } from './cache';
+import { relWordsApi } from './rel-words';
 
 export const apiRouter = c.router({
   auth: authApi,
   user: userApi,
   cache: cacheApi,
+  relwords: relWordsApi,
 });
