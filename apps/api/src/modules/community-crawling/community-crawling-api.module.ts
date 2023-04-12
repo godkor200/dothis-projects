832e7100bf5e32c346a91862358e26c130a12c98
godkor200 <godkor200@gmail.com>
2023-04-12 20:56:09 +0900

    no changed

--- apps/api/src/modules/community-crawling/community-crawling-api.module.ts
+++ apps/api/src/modules/community-crawling/community-crawling-api.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { CommunityCrawlingEntityModule } from '@Apps/config/database/domain';
 import { RouterModule } from '@nestjs/core';
 import { CommunityCrawlingApiV1Module } from '@Apps/modules/community-crawling/v1/community-crawling-api-v1.module';
 
