d097e93b6081e47507c4cf3b1b7914f384a015c8
godkor200 <godkor200@gmail.com>
2023-04-12 20:48:35 +0900

    refacter : UserChannelData 빼기

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { UserApiModule } from '@Apps/modules/user/user.module';
 import { AuthApiModule } from '@Apps/modules/auth/AuthApi.module';
 import { ChannelApiModule } from './modules/channel/channel-api.module';
-import { UserChannelDataApiModule } from '@Apps/modules/user-channel-data/user-channel-data-api.module';
 import { validationSchema } from '@Apps/config/validationsSchema';
 import dbConfig from '@Apps/config/database/config/db.env';
 import cacheConfig from '@Apps/config/cache/config/cache.env';
@@ -16,6 +15,7 @@ import { CacheConfigModule } from '@Apps/config/cache/cache.config.module';
 import { CacheApiModule } from '@Apps/modules/cache/cache.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { RelWordsApiModules } from './modules/rel-words/rel-words.module';
+import { DailyViewsApiModule } from './modules/daily_views/daily-views-api.module';
 
 @Module({
   controllers: [HeathApiController],
@@ -36,9 +36,9 @@ import { RelWordsApiModules } from './modules/rel-words/rel-words.module';
     UserApiModule,
     ChannelApiModule,
     AuthApiModule,
-    UserChannelDataApiModule,
     CacheApiModule,
     RelWordsApiModules,
+    DailyViewsApiModule,
   ],
   providers: [HealthService],
 })
