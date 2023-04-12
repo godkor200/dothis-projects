69db3df21ed301e685380547134ace9e829f76ca
godkor200 <godkor200@gmail.com>
2023-04-12 21:07:17 +0900

    rename : 디렉토리 이동

--- apps/api/src/modules/rel-words/v1/queries/find-rel/find-rel.adapter.ts
+++
@@ -1,5 +0,0 @@
-import { RelatedWordsEntity } from '@Apps/config/database/domain/entities/related_words/related_words.entity';
-
-export interface FindRelAdapter {
-  findOneByKeyword: (option: string) => Promise<RelatedWordsEntity>;
-}
