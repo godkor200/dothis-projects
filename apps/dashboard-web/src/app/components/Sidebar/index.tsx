948e494be42cb1b636cdcb7724e98faf505b3489
mimi <62527898+HM970506@users.noreply.github.com>
2023-05-21 17:20:42 +0900

    feat: css 적용

--- apps/dashboard-web/src/app/components/Sidebar/index.tsx
+++ apps/dashboard-web/src/app/components/Sidebar/index.tsx
@@ -1,5 +1,14 @@
-import "../../tempcss.css";
+import "./style.css"
 
 export default function Sidebar(){
-    return <div className="sidebar">sidebar</div>
+    return <nav className="sidebar">
+        <div className="logo">logo</div>
+        <div className="navButtonsContainer">
+        <button className="navButton">1</button>
+        <button className="navButton">1</button>
+        <button className="navButton">1</button>
+        <button className="navButton">1</button>
+        <button className="navButton">1</button>
+        </div>
+    </nav>
 }
