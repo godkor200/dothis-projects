27cb0d7e987a9bd3fbfba508703b3808079e917f
mini <jma1020@naver.com>
2023-06-03 23:19:32 +0900

    feat: styled components 초기 세팅 설정

--- apps/dashboard-web/src/app/layout.tsx
+++ apps/dashboard-web/src/app/layout.tsx
@@ -1,16 +1,17 @@
 import './global.css';
+import './tempcss.css';
 
 import localFont from '@next/font/local';
 import clsx from 'clsx';
 import type { ReactNode } from 'react';
-import "./tempcss.css";
-import ClientContext from './ClientContext';
-import GNB from './components/GNB';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
 
+import ClientContext from './ClientContext';
+import Footer from './components/Footer';
+import GNB from './components/GNB';
+import Header from './components/Header';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import StyledComponentsRegistry from './lib/registry';
 
 // nextjs font 최적화
 const font = localFont({
@@ -27,13 +28,14 @@ export default function RootLayout({ children }: Props) {
     <html lang="ko" className={clsx(font.className, 'text-[16px]')}>
       <body className="flex min-h-screen">
         <ClientContext>
-          <div className='template'>
-          <Sidebar/>
-          <Header/>
-          <Main children={children}/>
-          <div className='footer'>footer</div>
-          </div>
-          
+          <StyledComponentsRegistry>
+            <div className="template">
+              <Sidebar />
+              <Header />
+              <Main children={children} />
+              <div className="footer">footer</div>
+            </div>
+          </StyledComponentsRegistry>
         </ClientContext>
       </body>
     </html>
