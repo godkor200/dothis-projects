5ec94ab3bac88ceb1162b857cd65accf0b66229e
mini <jma1020@naver.com>
2023-06-08 01:09:24 +0900

    design: Login 페이지 스타일 추가 및 사용하지않는 코드 제거

--- apps/dashboard-web/src/app/login/page.tsx
+++ apps/dashboard-web/src/app/login/page.tsx
@@ -1,25 +1,23 @@
-'use client';
-import type { Route } from 'next';
-import type { LinkProps } from 'next/link';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { signIn } from 'next-auth/react';
-
+import GoogleBtn from '@/components/Login/GoogleBtn';
 import SvgComp from '@/components/share/SvgComp';
 
 function LoginPage() {
-  let goo_Url = 'www.naver.com';
-
-  const router = useRouter();
   return (
-    <div>
-      로그인
-      <br />
-      <SvgComp icon="LogoSvg" size={60} />
-      <p></p>
-      <button onClick={() => signIn('google')}>
-        <span>구글로 시작하기</span>
-      </button>
+    <div className="flex  items-center flex-col pt-14">
+      <SvgComp icon="LogoSvg" width={80} />
+
+      <h2 className="mt-10 text-2xl font-bold ">
+        간편 로그인으로 내 채널 분석까지
+      </h2>
+      <p className="mt-3 text-gray-500">
+        빅데이터 기반 콘텐츠 키워드 분석 플랫폼 ‘두디스’
+      </p>
+
+      <GoogleBtn />
+
+      <p className="mt-14 text-slate-200 text-xs">
+        Copyright © 2023 Dothis Corp. All rights reserved.
+      </p>
     </div>
   );
 }
