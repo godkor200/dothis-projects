2b4541729f2e38254075738b8c18f2dc828ae275
mini <jma1020@naver.com>
2023-06-08 00:18:27 +0900

    test: next-auth google 테스트

--- apps/dashboard-web/src/app/login/page.tsx
+++ apps/dashboard-web/src/app/login/page.tsx
@@ -1,11 +1,25 @@
+'use client';
+import type { Route } from 'next';
+import type { LinkProps } from 'next/link';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+
 import SvgComp from '@/components/share/SvgComp';
 
 function LoginPage() {
+  let goo_Url = 'www.naver.com';
+
+  const router = useRouter();
   return (
     <div>
       로그인
       <br />
       <SvgComp icon="LogoSvg" size={60} />
+      <p></p>
+      <button onClick={() => signIn('google')}>
+        <span>구글로 시작하기</span>
+      </button>
     </div>
   );
 }
