767a8f2330071ab367ce87c736319b837ebe3d71
mimi <62527898+HM970506@users.noreply.github.com>
2023-06-11 16:39:18 +0900

    design: page2

--- apps/dashboard-web/src/components/Landing/page2/index.tsx
+++ apps/dashboard-web/src/components/Landing/page2/index.tsx
@@ -1,19 +1,58 @@
 import Image from 'next/image';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin-left: 196px;
+  gap: 40px;
+
+  h2 {
+    font-size: 36px;
+    font-weight: bolder;
+  }
+  p {
+    font-size: 20px;
+  }
+  button {
+    width: 140px;
+    height: 54px;
+    border-radisu: 4px;
+    border: 1px solid white;
+    font-size: 20px;
+  }
+`;
 
 export default function Page2() {
   return (
-    <div>
+    <Background>
       <Image
         src={'/images/landing/page2.png'}
         alt={''}
-        width={100}
-        height={100}
+        width={680}
+        height={560}
       />
-      <div>
-        <h2>test text</h2>
-        <p>test text</p>
-        <button>button</button>
-      </div>
-    </div>
+      <Main>
+        <h2>
+          유튜브 콘텐츠, <br /> 10분만에 기획을 간단하게
+        </h2>
+        <p>
+          조회수 높이는 맞춤형 소재부터 포맷에 따른 <br />
+          스토리보드 도구까지 한곳에서 간편하게!
+        </p>
+        <button>더 알아보기</button>
+      </Main>
+    </Background>
   );
 }
