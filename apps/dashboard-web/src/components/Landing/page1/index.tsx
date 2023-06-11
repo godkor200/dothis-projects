de8ea92eaef401412fe8e8f8cc18bbd4b0ee942e
mimi <62527898+HM970506@users.noreply.github.com>
2023-06-11 16:32:08 +0900

    design: page1

--- apps/dashboard-web/src/components/Landing/page1/index.tsx
+++ apps/dashboard-web/src/components/Landing/page1/index.tsx
@@ -1,36 +1,95 @@
 import Image from 'next/image';
+import styled from 'styled-components';
 
 import Topbar from './topbar';
 
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const Main = styled.main`
+  margin-top: 240px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 40px;
+  margin-top: 30px;
+
+  button {
+    width: 195px;
+    height: 62px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    border: 1px solid black;
+    gap: 8px;
+    font-size: 20px;
+    font-weight: bolder;
+  }
+`;
+
+const Texts = styled.div`
+  position: absolute;
+  top: 678px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 64px;
+    font-weight: bolder;
+  }
+  h3 {
+    margin-top: 30px;
+    font-size: 24px;
+  }
+`;
+
 export default function Page1() {
   return (
-    <div>
+    <Background>
       <Topbar />
-      <div>
+      <Main>
         <Image
           src="/images/landing/page1.png"
-          width={500}
-          height={500}
+          width={1285}
+          height={358}
           alt={'page1 image'}
         />
-        <h1>test text</h1>
-        <p>text text</p>
-        <div>
-          <button>
-            <Image
-              src={'/images/landing/logo_small.png'}
-              alt={'logo_small'}
-              width={100}
-              height={100}
-            />
-            <p>button</p>
-          </button>
-          <button>
-            <Image src={'./search.svg'} alt={''} width={100} height={100} />
-            <p>button</p>
-          </button>
-        </div>
-      </div>
-    </div>
+        <Texts>
+          <h1>어떤 영상을 찍어야 조회수가 잘 나올까?</h1>
+          <h3>
+            콘텐츠 기획부터 키워드 추천, 트렌드 분석까지 바로 지금 Do this
+            하세요
+          </h3>
+          <Buttons>
+            <button>
+              <Image
+                src={'/images/landing/logo_small.png'}
+                alt={'logo_small'}
+                width={30}
+                height={30}
+              />
+              <p>무료 체험하기</p>
+            </button>
+            <button>
+              <Image src={'./search.svg'} alt={''} width={30} height={30} />
+              <p>요금제 알아보기</p>
+            </button>
+          </Buttons>
+        </Texts>
+      </Main>
+    </Background>
   );
 }
--- apps/dashboard-web/src/components/Landing/page1/topbar.tsx
+++ apps/dashboard-web/src/components/Landing/page1/topbar.tsx
@@ -1,32 +1,71 @@
 import Image from 'next/image';
+import styled from 'styled-components';
+
+const Bar = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  padding-top: 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  max-width: 1440px;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  gap: 10px;
+
+  button {
+    width: 160px;
+    height: 48px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radisu: 4px;
+
+    font-size: 20px;
+  }
+`;
+
+const SVG_SIZE = 32;
 
 export default function Topbar() {
+  const SVG = ({ src }: { src: string }) => {
+    return <Image src={src} alt={''} width={32} height={32} />;
+  };
+
   return (
-    <div>
+    <Bar>
       <Image
         src={'/images/landing/logo_medium.png'}
         alt={'logo_medium'}
-        width={100}
-        height={100}
+        width={204}
+        height={48}
       />
-      <div>
+      <ButtonsContainer>
         <button>
-          <Image src={'./content.svg'} alt={''} width={100} height={100} />{' '}
-          <p>text</p>
+          <SVG src={'./content.svg'} />
+          <p>콘텐츠 분석</p>
         </button>
         <button>
-          <Image src={'./magicpen.svg'} alt={''} width={100} height={100} />{' '}
-          <p>text</p>
+          <SVG src={'./magicpen.svg'} />
+          <p>키워드 분석</p>
         </button>
         <button>
-          <Image src={'./user.svg'} alt={''} width={100} height={100} />{' '}
-          <p>text</p>
+          <SVG src={'./user.svg'} />
+          <p>내 채널 분석</p>
         </button>
         <button>
-          <Image src={'./contact.svg'} alt={''} width={100} height={100} />{' '}
-          <p>text</p>
+          <SVG src={'./contact.svg'} />
+          <p>Contact</p>
         </button>
-      </div>
-    </div>
+      </ButtonsContainer>
+    </Bar>
   );
 }
