3548acc7664764c28ee960bf4b42dfe9e0522ae3
mini <jma1020@naver.com>
2023-06-22 02:13:58 +0900

    Design: NavSlide UI 작성 마무리 및 테스트

--- apps/dashboard-web/src/layouts/MainLayout/NavSlide.tsx
+++ apps/dashboard-web/src/layouts/MainLayout/NavSlide.tsx
@@ -10,18 +10,64 @@ import NavSliderContent from './NavSlideContent';
 function NavSlider() {
   return (
     <KeywordTapContiner>
-      <Button $active={true}>
-        먹방
-        <SvgComp icon="NavSlideDelete" size="1rem" />
-      </Button>
-      <Button $active={true}>
-        먹방
-        <SvgComp icon="NavSlideDelete" size="1rem" />
-      </Button>
-      <Button $active={true}>
-        먹방맛집
-        <SvgComp icon="NavSlideDelete" size="1rem" />
-      </Button>
+      <ResetButton>
+        <SvgComp icon="NavSlideReset" size="1.5rem" />
+      </ResetButton>
+      <ButtonContainer>
+        <Button $active={true}>
+          먹방
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+        <Button $active={true}>
+          먹방맛집
+          <SvgComp icon="NavSlideDelete" size="1rem" />
+        </Button>
+      </ButtonContainer>
+      <ArrowButton />
 
       <NavSliderContent />
     </KeywordTapContiner>
@@ -33,7 +79,7 @@ export default NavSlider;
 const KeywordTapContiner = styled.div`
   display: flex;
   flex-wrap: nowrap;
-  gap: 1rem;
+  gap: 1.5rem;
 
   margin-bottom: 1.5rem;
   padding: 1.875rem 3rem;
@@ -41,12 +87,8 @@ const KeywordTapContiner = styled.div`
   position: sticky;
   top: 0rem;
 
-  overflow-x: auto;
+  overflow-x: hidden;
   justify-content: start;
-
-  @media screen and (min-width: 1280px) {
-    overflow-x: visible;
-  }
 
   background-color: white;
 
@@ -58,26 +100,91 @@ const KeywordTapContiner = styled.div`
   }
 `;
 
-export const Button = styled.button<{ $active: boolean }>`
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 1rem;
+
+  overflow-x: hidden;
+`;
+
+const Button = styled.button<{ $active: boolean }>`
   display: flex;
   gap: 0.5rem;
   align-items: center;
   flex-shrink: 0;
 
+  position: relative;
+
   padding: 0.5rem 1.25rem;
+  border: 1px solid white;
   border-radius: 0.5rem;
+  box-sizing: border-box;
 
   font-size: 1rem;
   font-weight: 500;
+
   color: ${theme.colors.grey40};
   background-color: ${theme.colors.grey00};
+  box-shadow: inset 0 0 0 2px ${theme.colors.grey40};
 
-  border: 1px solid ${theme.colors.grey40};
   ${({ $active }) =>
     $active &&
     css`
-      border-color: ${theme.colors.primary40};
+      border: 1px solid white;
+
       background-color: rgba(${theme.colors.primary10}, 0.1);
       color: ${theme.colors.primary40};
+      box-shadow: inset 0 0 0 2px ${theme.colors.primary40};
     `}
+`;
+
+const ArrowButton = styled.button`
+  position: relative;
+
+  padding: 0.5rem 1.25rem;
+  border: 1px solid ${theme.colors.grey40};
+  border-radius: 0.5rem;
+
+  &::before {
+    content: '';
+    display: inline-block;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    /* ArrowButton에 좌우 패팅만큼  */
+    right: 2.625rem;
+
+    width: 10rem;
+
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.2) 10%,
+      rgba(255, 255, 255, 0.4) 20%,
+      rgba(255, 255, 255, 0.6) 40%,
+      rgba(255, 255, 255, 0.7) 50%,
+      rgba(255, 255, 255, 0.9) 60%,
+      white 70%
+    );
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: 50%;
+
+    border: solid black;
+    border-width: 0 2px 2px 0;
+    padding: 0.25rem;
+
+    transform: translate(20%, -50%) rotate(-45deg);
+  }
+`;
+
+const ResetButton = styled.button`
+  padding: 0.5rem 1.25rem;
+  border: 1px solid ${theme.colors.grey40};
+  border-radius: 0.5rem;
 `;
