fd51cbd9a01967a15d5314ba008196de651565a8
mini <jma1020@naver.com>
2023-07-02 02:44:30 +0900

    feat: Nivo Chart 적용

--- apps/dashboard-web/src/app/(main)/chart/page.tsx
+++ apps/dashboard-web/src/app/(main)/chart/page.tsx
@@ -1,12 +1,33 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+import LineChart from '@/components/chart/LineChart';
+import LineTwo from '@/components/chart/LineTwo';
+import MyResponsivePie from '@/components/chart/Test';
+
+// const LineChart = dynamic(() => import('../../../components/chart/LineChart'), {
+//   ssr: false,
+// });
+
+// const MyResponsivePie = dynamic(() => import('../../../components/chart/Test'));
+
 function ChartPage() {
   return (
-    <div>
-      <p style={{ fontSize: '10rem' }}>
+    <div
+      style={{
+        height: '100vh',
+        margin: '4rem',
+      }}
+    >
+      {/* <p style={{ fontSize: '10rem' }}>
         lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nemo
         voluptates, earum ratione aliquid voluptas nesciunt delectus magni
         obcaecati commodi eligendi placeat recusandae quibusdam voluptatibus,
         quis impedit animi sequi est.
-      </p>
+      </p> */}
+      {/* <LineChart /> */}
+      <LineTwo />
     </div>
   );
 }
