88bfd2899aeb8d0df6508e4aa2dc68158a924e74
mini <jma1020@naver.com>
2023-07-03 12:50:53 +0900

    feat: 숫자 단위 util 생성

--- apps/dashboard-web/src/components/chart/utils.ts
+++ apps/dashboard-web/src/components/chart/utils.ts
@@ -3,3 +3,10 @@ const NUMBER_FORMAT_REGX = /\B(?=(\d{3})+(?!\d))/g;
 const numberFormat = (value: number) => {
   return value.toString().replace(NUMBER_FORMAT_REGX, ',');
 };
+
+const setUnitText = (numbers: number[]) => {
+  const unit = ['', '만', '억', '조', '경'];
+  return numbers.map((number, index) =>
+    !!number ? numberFormat(number) + unit[unit.length - 1 - index] : number,
+  );
+};
