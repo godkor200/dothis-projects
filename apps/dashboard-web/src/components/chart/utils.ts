29a5a0d9f352377abb5af534d0cbbc0779946c46
mini <jma1020@naver.com>
2023-07-03 13:25:29 +0900

    feat: number 포맷util 추가

--- apps/dashboard-web/src/components/chart/utils.ts
+++ apps/dashboard-web/src/components/chart/utils.ts
@@ -23,3 +23,30 @@ export function unitFormat(value: any) {
 
   return compactNumber.format(value);
 }
+
+function numberToKorean(number: number) {
+  var inputNumber = number < 0 ? false : number;
+  var unitWords = ['', '만', '억', '조', '경'];
+  var splitUnit = 10000;
+  var splitCount = unitWords.length;
+  var resultArray = [];
+  var resultString = '';
+
+  for (var i = 0; i < splitCount; i++) {
+    var unitResult =
+      ((inputNumber as number) % Math.pow(splitUnit, i + 1)) /
+      Math.pow(splitUnit, i);
+    unitResult = Math.floor(unitResult);
+    if (unitResult > 0) {
+      resultArray[i] = unitResult;
+    }
+  }
+
+  for (var i = 0; i < resultArray.length; i++) {
+    if (!resultArray[i]) continue;
+    resultString =
+      String(numberFormat(resultArray[i])) + unitWords[i] + resultString;
+  }
+
+  return resultString;
+}
