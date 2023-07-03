bf0bb45e376f2d4694b88150b61d7f4b608a6e75
mini <jma1020@naver.com>
2023-07-03 12:53:01 +0900

    docs: 주석 추가

--- apps/dashboard-web/src/components/chart/utils.ts
+++ apps/dashboard-web/src/components/chart/utils.ts
@@ -10,3 +10,5 @@ const setUnitText = (numbers: number[]) => {
     !!number ? numberFormat(number) + unit[unit.length - 1 - index] : number,
   );
 };
+
+// 근데 이 부분은 넘어오는 number 형식에 따라 고민 중 (배열?? , 현재 format은 value: number로 제공 )
