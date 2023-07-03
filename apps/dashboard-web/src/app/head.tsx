ac0e781af720dd383bd920dded75ca98ad3b3d54
mini <jma1020@naver.com>
2023-07-03 13:02:17 +0900

    feat: desciption 추가 및  og 설정

--- apps/dashboard-web/src/app/head.tsx
+++ apps/dashboard-web/src/app/head.tsx
@@ -2,9 +2,18 @@ export default function RootHeader() {
   return (
     <>
       <title>두디스</title>
-      <meta name="description" content="" />
+      <meta
+        name="description"
+        content=" 데이터로 기획하는 영상 기획 보조 도구, 두디스"
+      />
 
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="두디스(Dothis)" />
+
+      <meta
+        property="og:description"
+        content="데이터로 기획하는 영상 기획 보조 도구, 두디스."
+      />
     </>
   );
 }
