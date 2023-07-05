0b17532878c1af6817a773cd0f25c40d75297e3a
mini <jma1020@naver.com>
2023-07-05 09:14:22 +0900

    feat: 구글, 네이버 사이트 등록 meta 태그 추가

--- apps/dashboard-web/src/app/head.tsx
+++ apps/dashboard-web/src/app/head.tsx
@@ -18,6 +18,18 @@ export default function RootHeader() {
         content="https://dothis-lending.s3.ap-northeast-2.amazonaws.com/dothisLogo.png"
       />
       {/* 해당 size는 배포 후 확인해보고 수정해보겠습니다.*/}
+
+      <meta
+        name="google-site-verification"
+        content="mTY9n8vpWvhgrwD6mm80iaAIT6-GscpvxkEq7hOCmas"
+      />
+      {/* 구글 서치 콘솔  */}
+
+      <meta
+        name="naver-site-verification"
+        content="61cf01795f8a825e98136f98401fd7f3aa1bfa20"
+      />
+      {/* 네이버 서치어드바이저  */}
     </>
   );
 }
