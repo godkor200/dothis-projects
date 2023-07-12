6831b25203dc17b6378a96c874d518bce7af6f38
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-07-13 02:21:29 +0900

    feat(ChartSidebar): 차트 사이드바 컴포넌트 구현

---
+++ apps/dashboard-web/src/components/ChartSidebar/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const RelatedWordList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 206px;
+  padding: 40px 20px;
+  border: 1px solid #d4d4d8;
+  border-radius: 8px;
+  background-color: #ffffff;
+`;
