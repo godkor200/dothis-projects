0f11dc1009e8ae41be5a1a4b22da8b121afb2d00
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-07-13 16:19:55 +0900

    feat(mocks): 관련어 더미데이터 생성

--- apps/dashboard-web/src/components/ChartSidebar/index.tsx
+++ apps/dashboard-web/src/components/ChartSidebar/index.tsx
@@ -1,83 +1,12 @@
+import { RELATED_WORDS } from '@/mocks';
+
 import RelatedWord from './RelatedWord';
 import { RelatedWordList } from './style';
-
-const RELATED_WORD_LIST = [
-  {
-    id: '1',
-    rank: 1,
-    word: '물냉면물냉면물냉면물냉면물냉면물냉면물냉면',
-    views: 10,
-    gap: 'UP',
-  },
-  {
-    id: '2',
-    rank: 2,
-    word: '냉장고',
-    views: 1,
-    gap: 'DOWN',
-  },
-  {
-    id: '3',
-    rank: 3,
-    word: '불닭볶음면',
-    views: 1,
-    gap: 'UP',
-  },
-  {
-    id: '4',
-    rank: 4,
-    word: '물냉면물냉면물냉면물냉면물냉면물냉면물냉면',
-    views: 10,
-    gap: 'UP',
-  },
-  {
-    id: '5',
-    rank: 5,
-    word: '냉장고',
-    views: 1,
-    gap: 'DOWN',
-  },
-  {
-    id: '6',
-    rank: 6,
-    word: '불닭볶음면',
-    views: 1,
-    gap: 'UP',
-  },
-  {
-    id: '7',
-    rank: 7,
-    word: '물냉면물냉면물냉면물냉면물냉면물냉면물냉면',
-    views: 10,
-    gap: 'UP',
-  },
-  {
-    id: '8',
-    rank: 8,
-    word: '냉장고',
-    views: 1,
-    gap: 'DOWN',
-  },
-  {
-    id: '9',
-    rank: 9,
-    word: '불닭볶음면',
-    views: 1,
-    gap: 'UP',
-  },
-  {
-    id: '10',
-    rank: 10,
-    word: '불닭볶음면',
-    views: 1,
-    gap: 'UP',
-  },
-];
 
 function ChartSidebar() {
   return (
     <RelatedWordList>
-      {RELATED_WORD_LIST.map((relatedWord) => (
+      {RELATED_WORDS.map((relatedWord) => (
         <RelatedWord key={relatedWord.id} relatedWord={relatedWord} />
       ))}
     </RelatedWordList>
