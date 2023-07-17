b31912a3cd3e6bc5f71e33aa1dfa3fe5dc649671
godkor200 <godkor200@gmail.com>
2023-07-17 12:43:50 +0900

    feat : keyword, tags 모델 추구

--- packages/dto/src/lib/channel/channel.model.ts
+++ packages/dto/src/lib/channel/channel.model.ts
@@ -13,6 +13,13 @@ export const zChannelData = z.object({
   channelNormalVideos: z.number(),
   channelCountry: z.string(),
   channelLink: z.string(),
+  channel_keywords: z.array(z.string()),
+  channel_tags: z.array(z.string()),
 });
 
 export type ChannelModel = z.TypeOf<typeof zChannelData>;
+
+export const zChannelTagsKeywordsData = zChannelData.pick({
+  channel_keywords: true,
+  channel_tags: true,
+});
