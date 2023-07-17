23546159f976140fe3d444e34aa4cb71debfb68d
godkor200 <godkor200@gmail.com>
2023-07-17 13:06:07 +0900

    refacter : 스웨거 토큰 기능 추가

--- apps/server/apps/api/src/modules/user/v2/get-keyword-byUser/get-keyword-byUser.http.controller.ts
+++ apps/server/apps/api/src/modules/user/v2/get-keyword-byUser/get-keyword-byUser.http.controller.ts
@@ -1,20 +1,16 @@
-import {
-  Controller,
-  HttpStatus,
-  Param,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, HttpStatus, UseGuards } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { FindKeywordTagByUserCommand } from 'apps/api/src/modules/user/v2/get-keyword-byUser/get-keyword-byUser.service';
 import {
+  ApiBearerAuth,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiTags,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { UserDto } from '@Libs/commons/src/types/dto.types';
+
 import {
   nestControllerContract,
   TsRest,
@@ -23,9 +19,11 @@ import {
 import { apiRouter } from '@dothis/dto';
 import { JwtAccessGuard, User } from '@Libs/commons/src';
 import { UserInfoCommandDto } from '@Apps/common/auth/v1/commands/google-login-redirect/google-login-redirect.service';
+import { ChannelKeywordOrtagDtos } from '@Apps/modules/user/dtos/channel-keywordOrtag.dtos';
 const c = nestControllerContract(apiRouter.user);
 const { pathParams, summary, responses, description } = c.getUserKeyword;
 type RequestShapes = NestRequestShapes<typeof c>;
+
 @ApiTags(pathParams)
 @Controller()
 export class GetKeywordByUserHttpController {
@@ -39,16 +37,20 @@ export class GetKeywordByUserHttpController {
   })
   @ApiOkResponse({
     description: '유저의 태그나 키워드를 찾아 옵니다.',
-    type: UserDto,
+    type: ChannelKeywordOrtagDtos,
   })
   @ApiNotFoundResponse({
     status: HttpStatus.NOT_FOUND,
-    description: responses[401],
+    description: responses[404],
   })
+  @ApiUnauthorizedResponse({ description: 'Authentication failed' })
   @ApiInternalServerErrorResponse({
     description: responses[500],
   })
-  async getKeywordTag(@User() userInfo: UserInfoCommandDto) {
+  @ApiBearerAuth('Authorization')
+  async getKeywordTag(
+    @User() userInfo: UserInfoCommandDto,
+  ): Promise<ChannelKeywordOrtagDtos> {
     const command = new FindKeywordTagByUserCommand({
       userId: userInfo.id,
     });
