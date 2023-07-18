1e16aa27ff2516f0dac9af5f505b755cc27b2501
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-07-19 01:02:03 +0900

    chore(dashboard-web): @dothis/theme tailwind config 확장

--- apps/dashboard-web/tailwind.config.js
+++ apps/dashboard-web/tailwind.config.js
@@ -1,11 +1,12 @@
+const { theme, typography } = require('@dothis/theme/dashboard/index.js');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme,
+  plugins: [typography],
 };
