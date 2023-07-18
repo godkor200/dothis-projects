c455e5f535e22b30b635169f452bea3a8c7ede72
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-07-18 18:51:24 +0900

    refactor: DashBoard 폴더 내 style 파일에 Summary style 코드 작성

--- apps/dashboard-web/src/components/Chart/DashBoard/style.ts
+++ apps/dashboard-web/src/components/Chart/DashBoard/style.ts
@@ -2,6 +2,37 @@ import { theme } from '@dothis/theme/dashboard';
 import { px2rem } from '@dothis/theme/utils/unit';
 import { styled } from 'styled-components';
 
+/* Summary */
+export const SummaryList = styled.ul`
+  display: flex;
+  gap: 22px;
+`;
+
+export const SummaryWrapper = styled.li`
+  flex: 1;
+  min-width: 300px;
+  padding: 20px 0;
+  border-radius: 8px;
+  background-color: #fef7f8;
+`;
+
+export const Border = styled.div`
+  padding-left: 25px;
+  border-left: 2px solid #f0516d;
+`;
+
+export const SummaryTitle = styled.div`
+  margin-bottom: 4px;
+  font-weight: 700;
+  color: #71717a;
+`;
+
+export const Content = styled.div`
+  font-size: 26px;
+  font-weight: 700;
+  color: #f0516d;
+`;
+
 /* DailyView */
 export const Wrapper = styled.div`
   display: flex;
