f10b6ef75df3490e66d6056a567980dc36e73b7f
mini <jma1020@naver.com>
2023-07-26 19:29:10 +0900

    feat: checkbox 컴포넌트 생성

---
+++ apps/dashboard-web/src/components/common/Checkbox/index.tsx
@@ -0,0 +1,89 @@
+import type { PropsWithChildren } from 'react';
+import React, {
+  createContext,
+  forwardRef,
+  ReactElement,
+  useContext,
+} from 'react';
+
+type CheckboxContextProps = {
+  id: string;
+  isChecked: boolean;
+  onChange: (event: React.MouseEvent<HTMLInputElement>) => void;
+};
+
+type CheckboxProps = CheckboxContextProps & React.PropsWithChildren<{}>;
+
+const CheckboxContext = createContext<CheckboxContextProps>({
+  id: '',
+  isChecked: false,
+  onChange: () => {},
+});
+
+const CheckboxContainer = ({
+  id,
+  isChecked,
+  onChange,
+  children,
+}: CheckboxProps) => {
+  const value = {
+    id,
+    isChecked,
+    onChange,
+  };
+
+  return (
+    <CheckboxContext.Provider value={value}>
+      {children}
+    </CheckboxContext.Provider>
+  );
+};
+// Context
+
+const useCheckboxContext = () => {
+  const context = useContext(CheckboxContext);
+  return context;
+};
+// useContext hook
+
+const Checkbox = forwardRef(
+  ({ as, ...props }: { as: React.ElementType }, ref) => {
+    const { id, isChecked, onChange } = useCheckboxContext();
+
+    const Trigger =
+      as ||
+      React.forwardRef<HTMLInputElement>(({ ...props }, ref) => (
+        <input
+          type="checkbox"
+          id={id}
+          checked={isChecked}
+          onClick={onChange}
+          readOnly
+          {...props}
+          ref={ref}
+        />
+      ));
+
+    return (
+      <>
+        {/* <input type="checkbox" id={id} checked={isChecked} onClick={onChange} readOnly {...props} ref={ref} /> */}
+
+        <Trigger isChecked={isChecked} onChange={onChange} id={id} ref={ref} />
+      </>
+    );
+  },
+);
+
+const Label = ({ children, ...props }: PropsWithChildren<{}>) => {
+  const { id } = useCheckboxContext();
+  return (
+    <label htmlFor={id} {...props}>
+      {children}
+    </label>
+  );
+};
+
+CheckboxContainer.Checkbox = Checkbox;
+CheckboxContainer.Label = Label;
+
+export default CheckboxContainer;
