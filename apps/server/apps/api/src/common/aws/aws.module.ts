c2fa789026b643b05ea518d9fd7f7399db1cdd4b
godkor200 <godkor200@gmail.com>
2023-08-07 18:39:38 +0900

    refacter : 변화 없음

--- apps/server/apps/api/src/common/aws/aws.module.ts
+++ apps/server/apps/api/src/common/aws/aws.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AwsAthenaService } from './service/aws.athena.service';
-import { AwsOpensearchService } from '@Apps/common/aws/service/aws.opensearch.service';
+import { AwsOpensearchConnetionService } from '@Apps/common/aws/service/aws.opensearch.service';
 import { AwsCredentialsService } from '@Apps/config/aws/config/aws.config';
 
 @Module({
-  providers: [AwsAthenaService, AwsOpensearchService, AwsCredentialsService],
-  exports: [AwsAthenaService, AwsOpensearchService, AwsCredentialsService],
+  providers: [
+    AwsAthenaService,
+    AwsOpensearchConnetionService,
+    AwsCredentialsService,
+  ],
+  exports: [
+    AwsAthenaService,
+    AwsOpensearchConnetionService,
+    AwsCredentialsService,
+  ],
 })
 export class AwsModule {}
