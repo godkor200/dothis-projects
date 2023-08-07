d7eefc53fdc296aacdaa1a2c92907711f77cc18b
godkor200 <godkor200@gmail.com>
2023-08-07 23:11:23 +0900

    refacter : 데일리뷰 뽑기

--- apps/server/apps/api/src/modules/video/database/video.query-handler.ts
+++ apps/server/apps/api/src/modules/video/database/video.query-handler.ts
@@ -1,16 +1,17 @@
 import { from, lastValueFrom, map, mergeMap, pluck, toArray } from 'rxjs';
 import { VideoServicePort } from './video.service.port';
-import { AwsOpensearchService } from '@Apps/common/aws/service/aws.opensearch.service';
-import { FindVideoQuery } from '@Apps/modules/video/queries/v1/find-video/find-video.service';
+import { AwsOpensearchConnetionService } from '@Apps/common/aws/service/aws.opensearch.service';
+import { FindVideoQuery } from '@Apps/modules/video/queries/v1/find-video/find-video.query-handler';
 import { IfindManyVideoResult } from '@Apps/modules/video/interface/find-many-video.interface';
+import { FindDailyViewsQuery } from '@Apps/modules/daily_views/interface/find-daily-views.dto';
 
 export class VideoQueryHandler
-  extends AwsOpensearchService
+  extends AwsOpensearchConnetionService
   implements VideoServicePort
 {
   async findManyVideo(tag: string): Promise<string[]> {
     const searchQuery = {
-      index: 'video',
+      index: 'new_video',
       body: {
         query: {
           bool: {
@@ -41,7 +42,7 @@ export class VideoQueryHandler
     words: FindVideoQuery,
   ): Promise<IfindManyVideoResult[]> {
     let searchQuery = {
-      index: 'video',
+      index: 'new_video',
       body: {
         query: {
           bool: {
@@ -79,5 +80,66 @@ export class VideoQueryHandler
     );
 
     return await lastValueFrom(observable$);
+  }
+
+  async findvideoIdfullScanAndVideos(
+    query: FindDailyViewsQuery,
+  ): Promise<string[]> {
+    console.log(query);
+    let searchQuery = {
+      index: 'new_video',
+      scroll: '10s',
+      size: 10000,
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                bool: {
+                  should: [
+                    {
+                      wildcard: {
+                        video_tag: `*${query.keyword}*`,
+                      },
+                    },
+                    {
+                      wildcard: {
+                        video_title: `*${query.keyword}*`,
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                range: {
+                  crawled_date: {
+                    gte: query.from + ' 00:00:00', // 시작 날짜 (greater than or equal)
+                    lte: query.to + ' 00:00:00', // 종료 날짜 (less than or equal)
+                  },
+                },
+              },
+            ],
+          },
+        },
+        _source: ['video_id'],
+      },
+    };
+    if (query.relationKeyword)
+      searchQuery.body.query.bool.must[0].bool.should.push(
+        {
+          wildcard: {
+            video_tag: `*${query.relationKeyword}*`,
+          },
+        },
+        {
+          wildcard: {
+            video_title: `*${query.relationKeyword}*`,
+          },
+        },
+      );
+    const res = await this.fullScan(searchQuery);
+    const observable: string[] = res.map((e) => e.video_id);
+
+    return await observable;
   }
 }
