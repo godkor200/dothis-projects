7b936eb58ce0e69ca8f9f9fa785e5d043f8df955
godkor200 <godkor200@gmail.com>
2023-08-07 23:13:10 +0900

    refacter : 관련어 api 준비중

---
+++ apps/server/apps/api/src/modules/video/interface/find-video.os.adapter.ts
@@ -0,0 +1,3 @@
+import { VideoServicePort } from '@Apps/modules/video/database/video.service.port';
+
+export interface FindVideoOsAdapter extends VideoServicePort {}
---
+++ apps/server/apps/api/src/modules/video_history/interface/find-video.history.resposne.ts
@@ -0,0 +1,8 @@
+export interface FindVideoHistoryResposne {
+  video_id: string;
+  video_views: number;
+  video_like: number;
+  video_comments: number;
+  crawled_date: string;
+  performance: number;
+}
