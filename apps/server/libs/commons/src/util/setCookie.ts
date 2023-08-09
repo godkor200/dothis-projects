c752209fc13672af121853646a60b41779063f70
godkor200 <godkor200@gmail.com>
2023-08-09 13:44:02 +0900

    refacter : 로컬에서 정상적으로 쿠키 받아와짐

--- apps/server/libs/commons/src/util/setCookie.ts
+++ apps/server/libs/commons/src/util/setCookie.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
+
+export function envDiscrimination(req: Request): boolean {
+  return req.host.includes('localhost');
+}
+
 // 쿠키 설정 함수
-export function setCookie(res: Response, name: string, value: string) {
-  res.cookie(name, value, {
-    domain:
-      process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
-        ? '.dothis.kr'
-        : 'localhost',
+export function setCookie(
+  req: Request,
+  res: Response,
+  name: string,
+  value: string,
+) {
+  return res.cookie(name, value, {
+    domain: envDiscrimination(req) ? '.dothis.kr' : 'localhost',
     path: '/',
     httpOnly: true,
   });
