147c59333f9887bf205639f8ae2da289ac368a9b
godkor200 <godkor200@gmail.com>
2023-09-07 11:15:52 +0900

    refecter : 환경변수 NODE_ENV 추가

--- apps/server/apps/api/src/config/app/config/app.env.ts
+++ apps/server/apps/api/src/config/app/config/app.env.ts
@@ -5,4 +5,5 @@ export default registerAs('app', () => ({
   APP_PORT: process.env.APP_PORT,
   APP_HOSTNAME: process.env.HOSTNAME ?? 'localhost',
   JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 }));
