2b42583bd091d53d1b7c834fe2c48341c9fbcc70
godkor200 <godkor200@gmail.com>
2023-09-07 13:08:01 +0900

    refecter : versioning

--- apps/server/apps/api/src/modules/rel-words/queries/v2/find-rel/find-rel.query-handler.ts
+++ apps/server/apps/api/src/modules/rel-words/queries/v2/find-rel/find-rel.query-handler.ts
@@ -1,15 +1,18 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { FindRelQuery } from '@Apps/modules/rel-words/queries/dtos/find-rel.dto';
+import { FindRelV2Query } from '@Apps/modules/rel-words/interface/dtos/find-rel.dto';
 import { Inject } from '@nestjs/common';
 import { RELWORDS_DI_TOKEN } from '@Apps/modules/rel-words/constants/rel-words.enum.di-token.constant';
 import { FindRelCachePort } from '@Apps/modules/rel-words/repository/cache/find-rel.cache.port';
 
-@QueryHandler(FindRelQuery)
+@QueryHandler(FindRelV2Query)
 export class FindRelQueryHandler
-  implements IQueryHandler<FindRelQuery, string>
+  implements IQueryHandler<FindRelV2Query, string>
 {
-  @Inject(RELWORDS_DI_TOKEN.FIND_ONE_BY_ELASTICACHE) query: FindRelCachePort;
-  execute(query: FindRelQuery): Promise<string> {
-    return this.query.findOneRelWord(query.keyword);
+  constructor(
+    @Inject(RELWORDS_DI_TOKEN.FIND_ONE_BY_ELASTICACHE)
+    protected readonly relCache: FindRelCachePort,
+  ) {}
+  async execute(query: FindRelV2Query): Promise<string> {
+    return await this.relCache.findOneRelWord(query.keyword);
   }
 }
