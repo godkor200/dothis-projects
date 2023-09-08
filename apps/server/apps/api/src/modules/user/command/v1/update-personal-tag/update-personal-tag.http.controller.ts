d1a316c8767ac89ca35c62a9f22335c154269268
godkor200 <godkor200@gmail.com>
2023-09-08 18:08:10 +0900

    refecter : 스웨거수정

--- apps/server/apps/api/src/modules/user/command/v1/update-personal-tag/update-personal-tag.http.controller.ts
+++ apps/server/apps/api/src/modules/user/command/v1/update-personal-tag/update-personal-tag.http.controller.ts
@@ -1,32 +1,42 @@
-import { Body, Controller, HttpStatus, Param } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Req, UseGuards } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import {
+  ApiHeaders,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiParam,
+  ApiTags,
 } from '@nestjs/swagger';
 import { UserDto } from '@Libs/commons/src/types/dto.types';
 import { nestControllerContract, TsRest } from '@ts-rest/nest';
 import { apiRouter } from '@dothis/dto';
 import { IResDto } from '@Libs/commons/src/types/res.types';
 import { UpdatePersonalTagDto } from '@Apps/modules/user/dtos/update-personal-tag.dto';
+import { JwtAccessGuard } from '@Libs/commons/src';
 const c = nestControllerContract(apiRouter.user);
 
 const { putUpdatePersonalTag } = c;
 const { responses, description, summary } = putUpdatePersonalTag;
 
 @Controller()
+@ApiTags('유저 관련')
 export class UpdatePersonalTagHttpController {
   constructor(private readonly commandBus: CommandBus) {}
 
   @TsRest(putUpdatePersonalTag)
-  @ApiParam({ name: 'id', required: true, description: '유저 아이디' })
+  @UseGuards(JwtAccessGuard)
   @ApiOperation({
     summary,
     description,
   })
+  @ApiHeaders([
+    {
+      name: 'Authorization',
+      description: "우리 사이트 accessToken(ex:'Bearer ~~~~~~')",
+    },
+  ])
   @ApiOkResponse({
     description: '유저 찾아옵니다.',
     type: UserDto,
@@ -39,10 +49,10 @@ export class UpdatePersonalTagHttpController {
     description: responses[500],
   })
   async execute(
-    @Param('id') id: string,
+    @Req() req,
     @Body('tag') tag: string[],
-  ): Promise<IResDto> {
-    const arg = new UpdatePersonalTagDto({ id, tag });
+  ): Promise<IResDto<void>> {
+    const arg = new UpdatePersonalTagDto({ id: req.user.id, tag });
     if (await this.commandBus.execute(arg)) return { success: true };
   }
 }
