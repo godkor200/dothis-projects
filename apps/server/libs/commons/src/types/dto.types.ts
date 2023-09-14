8bb9d2b54c63f2c801682aa276269f105ac22271
godkor200 <godkor200@gmail.com>
2023-09-14 17:57:35 +0900

    refecter : zod inputDto 수정

--- apps/server/libs/commons/src/types/dto.types.ts
+++ apps/server/libs/commons/src/types/dto.types.ts
@@ -6,6 +6,8 @@ import {
   zVideoHistory,
   zVideoModel,
 } from '@dothis/dto';
+import { z } from 'zod';
+import { IQuery } from '@nestjs/cqrs';
 export class UserDto extends createZodDto(extendApi(zUserModel)) {}
 export class UserRes extends UserDto {}
 export class VideoHistoryRes extends createZodDto(extendApi(zVideoHistory)) {}
@@ -24,3 +26,16 @@ export class RelwordsRes extends createZodDto(extendApi(zRelWordsArray)) {}
 //     dateSignIn: true,
 //   }),
 // ) {}
+export const zPaginatedQuery = z.object({
+  limit: z.number().describe('Specifies a limit of returned records'),
+  last: z.string().describe('Last index returned').optional(),
+});
+
+export const zKeyword = z.object({
+  search: z.string().describe('탐색어'),
+  related: z.string().describe('연관어').optional(),
+});
+export interface IKeyword extends z.TypeOf<typeof zKeyword> {}
+export interface IPageQuery extends z.TypeOf<typeof zPaginatedQuery> {}
+
+export interface IFindVideoPageQuery extends IKeyword, IPageQuery, IQuery {}
