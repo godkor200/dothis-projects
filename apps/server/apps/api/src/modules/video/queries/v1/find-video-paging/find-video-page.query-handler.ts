12294f6881a4b9e11f09f157b33480fce8c00fda
godkor200 <godkor200@gmail.com>
2023-09-19 18:35:19 +0900

    refecter : 이셉션 한곳으로 받도록 수정 더 연구 해봐야됨

--- apps/server/apps/api/src/modules/video/queries/v1/find-video-paging/find-video-page.query-handler.ts
+++ apps/server/apps/api/src/modules/video/queries/v1/find-video-paging/find-video-page.query-handler.ts
@@ -1,18 +1,23 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { FindVideoPageQuery } from '@Apps/modules/video/queries/v1/find-video-paging/find-video-paging.req.dto';
 import { IPagingRes } from '@Apps/modules/video/interface/find-many-video.interface';
-import { Inject } from '@nestjs/common';
+import { Inject, NotFoundException } from '@nestjs/common';
 import { VIDEO_DI_TOKEN } from '@Apps/modules/video/video.di-token';
 import { VideoServicePort } from '@Apps/modules/video/database/video.service.port';
+import { Ok, Result, Err } from 'oxide.ts';
 @QueryHandler(FindVideoPageQuery)
 export class FindVideoPageQueryHandler
-  implements IQueryHandler<FindVideoPageQuery, IPagingRes>
+  implements IQueryHandler<FindVideoPageQuery, Result<IPagingRes, Error>>
 {
   constructor(
     @Inject(VIDEO_DI_TOKEN.FIND_VIDEO_DATA_REPOSITORY_BY_OS)
     protected readonly video: VideoServicePort,
   ) {}
-  async execute(arg: FindVideoPageQuery): Promise<IPagingRes> {
-    return await this.video.findVideoPaging(arg);
+  async execute(
+    arg: FindVideoPageQuery,
+  ): Promise<Result<IPagingRes, NotFoundException>> {
+    const res = await this.video.findVideoPaging(arg);
+    if (!res.total.value) return Err(new NotFoundException());
+    return Ok(res);
   }
 }
