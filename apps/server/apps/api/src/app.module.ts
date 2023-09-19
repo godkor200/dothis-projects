273a42080a60182af7a84d7afcb757545c8b9c42
godkor200 <godkor200@gmail.com>
2023-09-19 18:27:48 +0900

    refecter : 오류 인덱스 nanoid 수정

--- apps/server/apps/api/src/app.module.ts
+++ apps/server/apps/api/src/app.module.ts
@@ -6,14 +6,26 @@ import cacheConfig from 'apps/api/src/config/cache/config/cache.env';
 import appConfig from 'apps/api/src/config/app/config/app.env';
 import awsConfig from '@Apps/config/aws/config/aws.env';
 import { TypeormModule } from 'apps/api/src/config/database/database.module';
-
+import { RequestContextModule } from 'nestjs-request-context';
 import { HeathApiController } from 'apps/api/src/health.controller';
 import { HealthService } from 'apps/api/src/health.service';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { BusinessModules } from '@Apps/modules/modules';
 import { CommonModule } from '@Apps/common/common.module';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ContextInterceptor } from '@Libs/commons/src/application/context/context.interceptor';
+import { ExceptionInterceptor } from '@Libs/commons/src/application/interceptors/exception.Interceptor';
+const interceptors = [
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ContextInterceptor,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ExceptionInterceptor,
+  },
+];
 @Module({
   controllers: [HeathApiController],
   imports: [
@@ -23,12 +35,13 @@ import { CommonModule } from '@Apps/common/common.module';
       load: [dbConfig, cacheConfig, appConfig, awsConfig],
       validationSchema,
     }),
+    RequestContextModule,
     ScheduleModule.forRoot(),
     TypeormModule,
     CommonModule,
     //module
     BusinessModules,
   ],
-  providers: [HealthService],
+  providers: [HealthService, ...interceptors],
 })
 export class AppModule {}
