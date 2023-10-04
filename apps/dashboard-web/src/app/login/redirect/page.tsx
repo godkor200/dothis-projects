4eeb71de0fd67f3b4181dc227eb89824f54d1291
mini <jma1020@naver.com>
2023-10-04 18:05:45 +0900

    remove: 주석 제거

--- apps/dashboard-web/src/app/login/redirect/page.tsx
+++ apps/dashboard-web/src/app/login/redirect/page.tsx
@@ -19,13 +19,7 @@ const RedirectPage = async ({
   const refreshToken = searchParams.refreshToken;
   const isNewUser = searchParams.isNewUser;
 
-  // cookie set은 server action이나 route Handler에서만 가능하다.
-  // cookiesStore.set('refreshToken', `Bearer ${searchParams.refreshToken}`);
-
   // AuthProvider는 useEffect 에 polling으로 accessToken을 주기적으로 보내주는 것이다. (polling 은 풀릴걱정이 없다 ->  렌더링 상관없이 주기적으로 요청을 보내는 것이기 때문)
-  // 병국님께 여쭤보기  verifyToken이  access토큰만인지??? 아니면 refresh도 포함인지
-  // 만약 refresh가 포함이 아니라면 refresh도 관통하는 로직도 생각해봐야함
-  // 그럼 이제 자동로그인은 state가 있어야한다.
 
   const queryClient = getQueryClient();
   await queryClient.prefetchQuery(['my'], () => apiServer.auth.getOwnInfo());
