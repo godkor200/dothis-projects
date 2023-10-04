a10b5fb7bf720ec8fb3190ccaa79e6b3e04787ed
mini <jma1020@naver.com>
2023-10-04 21:37:39 +0900

    feat: Content 및 ContentList 적용

--- apps/dashboard-web/src/features/chart/SocialMedia.tsx
+++ apps/dashboard-web/src/features/chart/SocialMedia.tsx
@@ -1,10 +1,16 @@
+import Content from '@/components/common/ContentCard/Content';
 import ContentCard from '@/components/common/ContentCard/ContentCard';
 import ContentCardHeader from '@/components/common/ContentCard/ContentCardHeader';
+import ContentList from '@/components/common/ContentCard/ContentList';
 
 const SocialMedia = () => {
   return (
     <ContentCard className="mb-[2.25rem]">
       <ContentCardHeader title="뉴스" />
+      <div className="mt-10 flex gap-[1.25rem]">
+        <Content />
+        <ContentList />
+      </div>
     </ContentCard>
   );
 };
