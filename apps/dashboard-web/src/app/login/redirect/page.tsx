ceaef77e50cef2b09368f4f62f120cd8cdd8278e
mini <jma1020@naver.com>
2023-10-04 18:28:26 +0900

    remove: 사용하지 않는 코드 제거

--- apps/dashboard-web/src/app/login/redirect/page.tsx
+++ apps/dashboard-web/src/app/login/redirect/page.tsx
@@ -1,11 +1,5 @@
 // import 'server-only';
 
-import { dehydrate } from '@tanstack/query-core';
-
-import getQueryClient from '@/query/getQueryClient';
-import ReactQueryHydrate from '@/query/ReactQueryHydrate';
-import { apiServer } from '@/utils/apiServer';
-
 import Client from './client';
 
 const RedirectPage = async ({
@@ -21,19 +15,12 @@ const RedirectPage = async ({
 
   // AuthProvider는 useEffect 에 polling으로 accessToken을 주기적으로 보내주는 것이다. (polling 은 풀릴걱정이 없다 ->  렌더링 상관없이 주기적으로 요청을 보내는 것이기 때문)
 
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(['my'], () => apiServer.auth.getOwnInfo());
-
-  const dehydratedState = dehydrate(queryClient);
-
   return (
-    <ReactQueryHydrate state={dehydratedState}>
-      <Client
-        accessToken={accessToken}
-        refreshToken={refreshToken}
-        isNewUser={isNewUser}
-      />
-    </ReactQueryHydrate>
+    <Client
+      accessToken={accessToken}
+      refreshToken={refreshToken}
+      isNewUser={isNewUser}
+    />
   );
 };
 
