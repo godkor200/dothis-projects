1e42779067d0445f8c2baaf9f97d6b0959b07f73
mini <jma1020@naver.com>
2023-10-04 21:35:19 +0900

    design: 스타일 수정 및 tailwind eslint로 인한 수정

--- apps/dashboard-web/src/components/common/ContentCard/ContentCard.tsx
+++ apps/dashboard-web/src/components/common/ContentCard/ContentCard.tsx
@@ -6,7 +6,7 @@ const ContentCard = ({
   children,
 }: StrictPropsWithChildren<ContentCardProps>) => {
   return (
-    <div className="mx-[3rem] mb-[2.25rem] rounded-lg border border-solid border-grey300 bg-grey00 px-[2.5rem] pt-[2.5rem]">
+    <div className="border-grey300 bg-grey00 mx-[3rem] mb-[2.25rem] rounded-lg border border-solid px-[2.5rem] pt-[2.5rem]">
       {children}
     </div>
   );
