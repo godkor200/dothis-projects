73cfc3f69e55304ce00ad66f7a71b1f5626a9b5a
mini <jma1020@naver.com>
2023-10-04 16:56:23 +0900

    remove: news api 제거

--- apps/dashboard-web/src/app/my/page.tsx
+++ apps/dashboard-web/src/app/my/page.tsx
@@ -4,44 +4,11 @@ import { useEffect } from 'react';
 
 import { apiClient } from '@/utils/apiClient';
 
-let obj = {
-  access_key: 'eb75ee2e-b1f6-4ada-a964-9bf94c5a2f26',
-  argument: {
-    query: '아시안게임',
-    published_at: {
-      from: '2023-09-01',
-      until: '2023-09-26',
-    },
-    // provider: ['경향신문'],
-    // category: ['정치>정치일반', 'IT_과학'],
-    // category_incident: ['범죄', '교통사고', '재해>자연재해'],
-    // byline: '',
-
-    sort: { date: 'desc' },
-    hilight: 200,
-    return_from: 0,
-    return_size: 5,
-    fields: ['byline', 'category', 'category_incident', 'provider_news_id'],
-  },
-};
-
 const MyPage = () => {
-  useEffect(() => {
-    async function aa() {
-      const test = await fetch(
-        'https://tools.kinds.or.kr/search/news?access_key=eb75ee2e-b1f6-4ada-a964-9bf94c5a2f26',
-        { method: 'POST', body: JSON.stringify(obj) },
-      );
-      const dd = await test.json();
-      console.log(dd);
-    }
-    aa();
-  }, []);
-
-  const { data } = apiClient.auth.getOwnInfo.useQuery(['my'], {});
+  const { data } = apiClient.auth.getOwnInfo.useQuery(['my_1'], {});
 
   console.log(data);
-  useEffect(() => {}, []);
+
   return <>마이페이지</>;
 };
 
