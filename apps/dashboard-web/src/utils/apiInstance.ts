de4f6903cd1308ddadf8e8c64c0ebdd45aca5284
mini <jma1020@naver.com>
2023-10-04 16:44:44 +0900

    fix: 프론트단 refresh config 세팅 관련 로직 제거

--- apps/dashboard-web/src/utils/apiInstance.ts
+++ apps/dashboard-web/src/utils/apiInstance.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-import { HTTP_BASE_URL } from '@/constants/http';
+export const HTTP_BASE = '  api.dothis.kr';
 
 const isServer = typeof window === 'undefined';
 
 export const apiInstance = axios.create({
-  baseURL: HTTP_BASE_URL,
+  baseURL: HTTP_BASE,
   withCredentials: true,
 });
 
@@ -13,23 +13,17 @@ apiInstance.interceptors.request.use(async (config) => {
   if (isServer) {
     const { cookies } = await import('next/headers');
     const token = cookies().get('accessToken')?.value;
-    const refreshToken = cookies().get('refreshToken')?.value;
 
     if (token) {
       config.headers.Authorization = `${token}`;
-      /**
-       * config.headers['cookie'] = `refreshToken=${refreshToken}`;
-       * 해당 부분 Cookie 도메인을 설정할 수 있으면 이렇게 수동으로 안넣어줘도 되지않을까 싶어서 주석처리를 진행하였습니다
-       */
     }
   } else {
     const { getCookie } = await import('cookies-next');
 
     const token = getCookie('accessToken');
-    const refreshToken = getCookie('refreshToken');
+
     if (token) {
       config.headers.Authorization = `${token}`;
-      // config.headers['cookie'] = `refreshToken=${refreshToken}`;
     }
   }
 
