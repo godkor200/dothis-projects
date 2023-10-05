56a3a08c06f1e29a77c4816e3953e96e2d5a9e53
mini <jma1020@naver.com>
2023-10-05 16:57:49 +0900

    refactor: 데이터를 가져옴에 따른 수정사항 반영 (데이터 매핑)

--- apps/dashboard-web/src/components/common/ContentCard/Content.tsx
+++ apps/dashboard-web/src/components/common/ContentCard/Content.tsx
@@ -5,9 +5,16 @@ import SvgComp from '@/share/SvgComp';
 
 import ContentSubTitle from './ContentSubTitle/ContentSubTitle';
 
-const Content = () => {
+export interface ContentProps {
+  title: string;
+  category: string;
+  provider: string;
+  date: string;
+}
+
+const Content = ({ title, category, provider, date }: ContentProps) => {
   return (
-    <div className="rounded-10 border-grey500 w-[640px] overflow-hidden border border-solid">
+    <div className="rounded-10 border-grey300 w-[640px] overflow-hidden border border-solid">
       <Image
         src="/RelatedContentMain.png"
         width={640}
@@ -16,13 +23,11 @@ const Content = () => {
         style={{ objectFit: 'cover', layout: 'fill' }}
       />
       <div className="px-[1.875rem] py-10">
-        <h3 className="text-grey700 mb-6  text-[1.5rem] font-bold">
-          충격!!...진짜귀여운동그란고양이실존!!!충격!!...진짜귀여운동그란고양이실존!!!...충격!!...진짜귀여운동그란고
-        </h3>
+        <h3 className="text-grey700 mb-6  text-[1.5rem] font-bold">{title}</h3>
         <ContentSubTitle
-          secondText="경향신문"
-          thirdText="경제>금융_재테크"
-          date="2023. 9. 4"
+          secondText={provider}
+          thirdText={category}
+          date={date}
         />
         <div className="bg-grey200 flex w-full items-center justify-center rounded-lg   py-4 text-center">
           <button className="inline-flex items-center gap-[0.7rem]">
--- apps/dashboard-web/src/components/common/ContentCard/ContentList.tsx
+++ apps/dashboard-web/src/components/common/ContentCard/ContentList.tsx
@@ -1,37 +1,40 @@
 import Image from 'next/image';
 
+import type { ContentProps } from './Content';
 import ContentSubTitle from './ContentSubTitle/ContentSubTitle';
 
-const ContentList = () => {
-  return (
-    <div className="px-5 pt-[0.375rem]">
-      <div className="flex gap-[1rem] pt-[0.875rem]">
-        <div className="overflow-hidden rounded-md">
-          <Image
-            src="/RelatedContentMain.png"
-            width={132}
-            height={74}
-            alt="Picture of the author"
-            style={{ objectFit: 'cover', layout: 'fill' }}
-          />
-        </div>
+interface ContentListProps {
+  contentArray: Array<ContentProps>;
+}
 
-        <div>
-          <p className="text-grey700 mb-2 line-clamp-2 w-[278px] font-medium">
-            인간은 대체로 합리적이지 않다…금융 투자에서는 법률·부동산·금융,
-            얼마나 아... 인간은 대체로 합리적이지 않다…금융 투자에서는
-            법률·부동산·금융, 얼마나 아...인간은 대체로 합리적이지 않다…금융
-            투자에서는 법률·부동산·금융, 얼마나 아...인간은 대체로 합리적이지
-            않다…금융 투자에서는 법률·부동산·금융, 얼마나 아...
-          </p>
-          <ContentSubTitle
-            secondText="경향신문"
-            thirdText="경제>금융_재테크"
-            date="2023. 9. 4"
-            isList={true}
-          />
+const ContentList = ({ contentArray }: ContentListProps) => {
+  return (
+    <div className="px-5 pt-[0.375rem] ">
+      {contentArray.map(({ title, category, date, provider }) => (
+        <div className="flex gap-[1rem] py-[0.875rem]">
+          <div className="overflow-hidden rounded-md">
+            <Image
+              src="/RelatedContentMain.png"
+              width={132}
+              height={74}
+              alt="Picture of the author"
+              style={{ objectFit: 'cover', layout: 'fill' }}
+            />
+          </div>
+
+          <div>
+            <p className="text-grey700 mb-2 line-clamp-2 w-[278px] font-medium">
+              {title}
+            </p>
+            <ContentSubTitle
+              secondText={provider}
+              thirdText={category}
+              date={date}
+              isList={true}
+            />
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
