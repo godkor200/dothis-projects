f92a615b9d76a9648aee2ed50fdfc37db8d87ed1
mini <jma1020@naver.com>
2023-10-05 16:53:45 +0900

    design: chart 페이지 헤더텍스트 디자인 수정사항 반영

--- apps/dashboard-web/src/app/(main)/chart/layout.tsx
+++ apps/dashboard-web/src/app/(main)/chart/layout.tsx
@@ -5,7 +5,9 @@ import SearchBar from '@/components/Chart/SearchBar/Search';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <h2> 키워드로 인사이트를 얻어보세요</h2>
+      <h2 className="text-h3 pb-10 pt-20 text-center font-bold">
+        키워드로 인사이트를 얻어보세요
+      </h2>
       <KeywordSlide />
       <SearchBar />
       <Header />
