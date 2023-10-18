c2904c9867a91d744661b03375afa3993f6014c9
godkor200 <godkor200@gmail.com>
2023-10-18 11:05:51 +0900

    refecter : find-daily-views dtos 수정

--- apps/server/apps/api/src/modules/daily_views/dtos/find-daily-views.dtos.ts
+++ apps/server/apps/api/src/modules/daily_views/dtos/find-daily-views.dtos.ts
@@ -1,4 +1,24 @@
-import { FindDailyViewsQuery } from '@Apps/modules/daily_views/interface/find-daily-views.dto';
+import { IQuery } from '@nestjs/cqrs';
+
+export class FindDailyViewsQuery implements IQuery {
+  readonly clusterNumber: string;
+
+  readonly keyword: string;
+
+  readonly relationKeyword?: string;
+
+  readonly from: Date;
+
+  readonly to: Date;
+
+  constructor(props: FindDailyViewsQuery) {
+    this.clusterNumber = props.clusterNumber;
+    this.keyword = props.keyword;
+    this.relationKeyword = props.relationKeyword;
+    this.from = props.from;
+    this.to = props.to;
+  }
+}
 
 export interface FindDailyViewsDtos
   extends Omit<FindDailyViewsQuery, 'clusterNumber'> {}
