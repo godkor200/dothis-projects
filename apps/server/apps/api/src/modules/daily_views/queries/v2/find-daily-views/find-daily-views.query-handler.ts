52c5c37a60cde9d6d7278596d3e360395c056764
godkor200 <godkor200@gmail.com>
2023-10-18 18:41:51 +0900

    refecter : 예의 처리 추가

--- apps/server/apps/api/src/modules/daily_views/queries/v2/find-daily-views/find-daily-views.query-handler.ts
+++ apps/server/apps/api/src/modules/daily_views/queries/v2/find-daily-views/find-daily-views.query-handler.ts
@@ -2,20 +2,27 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { VIDEO_DI_TOKEN } from '@Apps/modules/video/video.di-token';
 import { FindVideoOsAdapter } from '@Apps/modules/video/interface/find-video.os.adapter';
-import { DailyViewsEntity } from '@Apps/modules/daily_views/repository/entity/daily-views.entity';
 import { FindDailyViewsQuery } from '@Apps/modules/daily_views/dtos/find-daily-views.dtos';
 import { VIDEO_HISTORY_DI_TOKEN } from '@Apps/modules/video_history/video_history.di-token';
 import { FindVideoHistoryOsAdapter } from '@Apps/modules/video_history/interface/find-video-history.os.adapter';
 import { FindVideoHistoryResposne } from '@Apps/modules/video_history/interface/find-video.history.resposne';
+import { VideoNotFoundError } from '@Apps/modules/video/domain/event/video.error';
+import { Err } from 'oxide.ts';
+import { VideoHistoryNotFoundError } from '@Apps/modules/video_history/domain/event/video_history.err';
 
-interface IncreaseData extends FindVideoHistoryResposne {
-  increase_views?: number;
-  increase_likes?: number;
-  increase_comments?: number;
+export interface IncreaseData {
+  date: string;
+  increase_views: number;
+  increase_likes: number;
+  increase_comments: number;
 }
 @QueryHandler(FindDailyViewsQuery)
 export class FindDailyViewsQueryOsHandler
-  implements IQueryHandler<FindDailyViewsQuery, DailyViewsEntity[]>
+  implements
+    IQueryHandler<
+      FindDailyViewsQuery,
+      IncreaseData[] | Err<VideoNotFoundError> | Err<VideoHistoryNotFoundError>
+    >
 {
   constructor(
     @Inject(VIDEO_HISTORY_DI_TOKEN.FIND_HISTORY)
@@ -24,20 +31,21 @@ export class FindDailyViewsQueryOsHandler
     private readonly video: FindVideoOsAdapter,
   ) {}
 
-  async execute(query: FindDailyViewsQuery): Promise<any> {
+  async execute(query: FindDailyViewsQuery) {
     const videos = await this.video.findvideoIdfullScanAndVideos(query);
+    if (!videos) return Err(new VideoNotFoundError());
     const videoHistories = await this.videoHistory.findVideoHistory(
       videos,
       query.from.toString(),
       query.to.toString(),
       query.clusterNumber,
     );
-
+    if (!videoHistories) return Err(new VideoHistoryNotFoundError());
     return this.calculateIncrease(videoHistories);
   }
 
   /**
-   * 일일 조회 수: 각 동영상들의 현재 데이터
+   * 일일 조회 수: 각 동영상들의 날짜별 증감에 따른 합계
    * @param videoData
    * @private
    */
