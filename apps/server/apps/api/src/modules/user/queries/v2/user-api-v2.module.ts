ce3f09916389ed00a22672349660d9edb32c2a12
godkor200 <godkor200@gmail.com>
2023-10-18 16:55:42 +0900

    refecter : 레포지토리수정

--- apps/server/apps/api/src/modules/user/queries/v2/user-api-v2.module.ts
+++ apps/server/apps/api/src/modules/user/queries/v2/user-api-v2.module.ts
@@ -4,13 +4,18 @@ import { UserEntityModule } from '@Apps/modules/user/domain/user.entity.module';
 import { MembershipEntityModule } from '@Apps/modules/membership/domain/membership.entity.module';
 import { UserRepository } from '@Apps/modules/user/database/user.repository';
 import { USER_REPOSITORY } from '@Apps/modules/user/user.di-token';
-import { CHANNEL_DATA_REPOSITORY_BY_OS } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
+import {
+  CHANNEL_DATA_REPOSITORY,
+  CHANNEL_DATA_REPOSITORY_BY_OS,
+} from '@Apps/modules/channel/constants/channel-data.di-token.constants';
 import { ChannelQueryHandler } from '@Apps/modules/channel/repository/repository/channel.query-handler';
 import { GetKeywordByUserHttpController } from '@Apps/modules/user/queries/v2/get-keyword-byUser/get-keyword-byUser.http.controller';
 import { GetUserV2CommandHandler } from '@Apps/modules/user/queries/v2/get-keyword-byUser/get-keyword-byUser.service';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { PutAgreePromotionHttpController } from '@Apps/modules/user/command/v1/put-agree-promotion/put-agree-promotion.http.controller';
 import { PutAgreePromotionCommandHandler } from '@Apps/modules/user/command/v1/put-agree-promotion/put-agree-promotion.command-handler';
+import { ChannelDataRepository } from '@Apps/modules/channel/repository/db/channel-data.repository';
+import { ChannelEntity } from '@Apps/modules/channel/repository/entity/channel.entity';
 
 const httpControllers = [
   GetKeywordByUserHttpController,
@@ -19,6 +24,7 @@ const httpControllers = [
 
 const repositories: Provider[] = [
   { provide: USER_REPOSITORY, useClass: UserRepository },
+  { provide: CHANNEL_DATA_REPOSITORY, useClass: ChannelDataRepository },
   { provide: CHANNEL_DATA_REPOSITORY_BY_OS, useClass: ChannelQueryHandler },
 ];
 
@@ -29,7 +35,13 @@ const commandHandlers: Provider[] = [
 
 const queryHandlers: Provider[] = [];
 @Module({
-  imports: [CqrsModule, UserEntityModule, MembershipEntityModule, AwsModule],
+  imports: [
+    CqrsModule,
+    UserEntityModule,
+    MembershipEntityModule,
+    AwsModule,
+    ChannelEntity,
+  ],
   controllers: [...httpControllers],
   providers: [...repositories, ...commandHandlers, ...queryHandlers],
 })
