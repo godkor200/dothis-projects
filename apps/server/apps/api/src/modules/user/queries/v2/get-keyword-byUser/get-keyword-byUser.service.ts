159d075073521c72a809642757c3685665db3f53
godkor200 <godkor200@gmail.com>
2023-10-18 16:31:32 +0900

    refecter : sql에서 받아오는 것으로 수정

--- apps/server/apps/api/src/modules/user/queries/v2/get-keyword-byUser/get-keyword-byUser.service.ts
+++ apps/server/apps/api/src/modules/user/queries/v2/get-keyword-byUser/get-keyword-byUser.service.ts
@@ -1,11 +1,9 @@
-import { Inject, NotFoundException } from '@nestjs/common';
-import { UserRepositoryPort } from '@Apps/modules/user/database/user.repository.port';
-import { USER_REPOSITORY } from '@Apps/modules/user/user.di-token';
+import { Inject } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { CHANNEL_DATA_REPOSITORY_BY_OS } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
-import { ChannelAdapter } from '@Apps/modules/channel/interface/channel.adapter';
+import { CHANNEL_DATA_REPOSITORY } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
 import { Err, Ok } from 'oxide.ts';
 import { UserNotFoundError } from '@Apps/common/auth/domain/event/auth.error';
+import { ChannelDataRepositoryPort } from '@Apps/modules/channel/repository/db/channel-data.repository.port';
 
 export class FindKeywordTagByUserCommand {
   public readonly userId: string;
@@ -21,17 +19,18 @@ export class GetUserV2CommandHandler
   implements ICommandHandler<FindKeywordTagByUserCommand>
 {
   constructor(
-    @Inject(USER_REPOSITORY)
-    protected readonly userRepo: UserRepositoryPort,
-
-    @Inject(CHANNEL_DATA_REPOSITORY_BY_OS)
-    protected readonly channelOpenSearchRepo: ChannelAdapter,
+    @Inject(CHANNEL_DATA_REPOSITORY)
+    protected readonly channelDataRepo: ChannelDataRepositoryPort,
   ) {}
   async execute(command: FindKeywordTagByUserCommand) {
-    const res = await this.channelOpenSearchRepo.findChannelTagOrKeyword(
+    const res = await this.channelDataRepo.findOneByChannelId(
       command.channelId,
     );
     if (!res) return Err(new UserNotFoundError());
-    return Ok(res);
+    const { keyword, tag } = res;
+    return Ok({
+      channel_keywords: keyword,
+      channel_tags: tag,
+    });
   }
 }
