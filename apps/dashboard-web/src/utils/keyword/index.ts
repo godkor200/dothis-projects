176de93de942496bbd77703db65c517fd40867a2
mini <jma1020@naver.com>
2023-10-20 02:05:33 +0900

    fix: keyword 관련 util파일 keyword 폴더로 이동

--- apps/dashboard-web/src/utils/keyword/index.ts
+++ apps/dashboard-web/src/utils/keyword/index.ts
@@ -1,3 +1,9 @@
+/**
+ *  선택한 키워드가 존재하는지 확인하는 함수
+ * @param arr array 형태의 키워드 리스트를 보내줘야함
+ * @returns 선택된 키워드가 하나라도 있으면 true를 반환
+ */
+
 export function isHashKeyword(arr: Array<string> | null | undefined) {
   if (arr === null || arr === undefined) {
     return false;
@@ -9,6 +15,13 @@ export function isHashKeyword(arr: Array<string> | null | undefined) {
   }
   return false; // 모든 문자열에서 '#'을 찾지 못하면 false 반환
 }
+
+/**
+ * api -> getUserKeyword에 response형식에 맞춰서 params를 받고 두개의 리스트를 합친 array를 반환하는 함수
+ * @param arr1 채널 키워드 or 태그 response가 들어옴 (ex: '먹방, 와인, 쥬스' string형식)
+ * @param arr2 채널 키워드 or 태그 response가 들어옴
+ * @returns 두개의 리스트를 합친 array를 반환
+ */
 
 export const combinedKeywordsAndTags = (
   arr1: string | undefined | null,
