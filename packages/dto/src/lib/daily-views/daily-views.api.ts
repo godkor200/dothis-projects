1c2d9561010dda0e8950e2d66bf8185096bcc7c7
godkor200 <godkor200@gmail.com>
2023-10-30 11:01:24 +0900

    refacter : dec 수정

--- packages/dto/src/lib/daily-views/daily-views.api.ts
+++ packages/dto/src/lib/daily-views/daily-views.api.ts
@@ -21,8 +21,7 @@ export const dailyViewApi = c.router({
       401: 'Not Found',
       500: '서버에 문제가 있으면 리턴한다.',
     },
-    summary: '데일리 뷰를 가져옵니다',
-    description:
-      'params relationKeyword video를 찾아 옵니다. 와서 video history 를 출력합니다. 아직 많이 느립니다. 평균 30초-40초대입니다. 구현하실떄 더미 데이터로 만 사용하세요.',
+    summary: '일일 조회수를 가져옵니다',
+    description: 'params,relationKeyword, 날짜로 일일 조회수 를 출력합니다.',
   },
 });
