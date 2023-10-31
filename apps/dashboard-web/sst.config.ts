8adc3b2ca91a902fd241cd55389d013b77139642
mini <jma1020@naver.com>
2023-10-31 18:38:34 +0900

    feat: sst 커스텀 도메인 설정 추가

--- apps/dashboard-web/sst.config.ts
+++ apps/dashboard-web/sst.config.ts
@@ -10,7 +10,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, 'site', {});
+      const site = new NextjsSite(stack, 'site', {
+        customDomain: {
+          domainName: 'www.dothis.kr',
+          hostedZone: 'dothis.kr',
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
