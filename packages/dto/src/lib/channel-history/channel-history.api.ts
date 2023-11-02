b77316ac90075657a58b304fc276af4df9dd99f7
godkor200 <godkor200@gmail.com>
2023-11-02 17:54:59 +0900

    refacter : 설명 수정

--- packages/dto/src/lib/channel-history/channel-history.api.ts
+++ packages/dto/src/lib/channel-history/channel-history.api.ts
@@ -22,6 +22,7 @@ export const expectedViewsApi = c.router({
       500: '서버에 문제가 있으면 리턴한다.',
     },
     summary: '기대 조회수를 가져옵니다',
-    description: 'params,relationKeyword, 날짜로 기대 조회수 를 출력합니다.',
+    description:
+      '클러스터 번호(clusterNumber),탐색어(keyword),연관어(relationKeyword), 날짜(from,to)로 기대 조회수 를 출력합니다.',
   },
 });
