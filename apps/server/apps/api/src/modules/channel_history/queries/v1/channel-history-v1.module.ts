904bc8b3315c3aa12fd106b7fbfa9677ba84649a
godkor200 <godkor200@gmail.com>
2023-11-02 14:54:43 +0900

    refacter : 채널 히스토리 레포지토리 모듈 추가

--- apps/server/apps/api/src/modules/channel_history/queries/v1/channel-history-v1.module.ts
+++ apps/server/apps/api/src/modules/channel_history/queries/v1/channel-history-v1.module.ts
@@ -4,12 +4,18 @@ import { ExpectedViewsQueryHandler } from '@Apps/modules/channel_history/queries
 import { ExpectedViewsHttpController } from '@Apps/modules/channel_history/queries/v1/exprected-views/expected-views.http.controller';
 import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
 import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
+import { VIDEO_HISTORY_OS_DI_TOKEN } from '@Apps/modules/video_history/video_history.di-token';
+import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/database/channel-history.query-handler';
 
 const controllers = [ExpectedViewsHttpController];
 const repositories: Provider[] = [
   {
     provide: VIDEO_OS_DI_TOKEN,
     useClass: VideoQueryHandler,
+  },
+  {
+    provide: VIDEO_HISTORY_OS_DI_TOKEN,
+    useClass: ChannelHistoryQueryHandler,
   },
 ];
 @Module({
