5053fa2cfcbd2e9482effb681109ae8870b511cb
godkor200 <godkor200@gmail.com>
2023-11-02 14:08:21 +0900

    feat : 풀스캔하는 거는 메소드 뒤에 fullscan을 붙이도록 수정

--- apps/server/apps/api/src/modules/video_history/database/video_history.query-handler.port.ts
+++ apps/server/apps/api/src/modules/video_history/database/video_history.query-handler.port.ts
@@ -1,11 +1,10 @@
 import { IFindVideoHistoryResposne } from '@Apps/modules/video_history/interface/find-video.history.resposne';
-import { OsRes } from '@Apps/common/aws/interface/os.res.interface';
 
 export interface VideoHistoryQueryHandlerPort {
-  findVideoHistory(
+  findVideoHistoryFullscan(
     videoId: string[],
     from: string,
     to: string,
     clusterNumber: string,
-  ): Promise<OsRes<IFindVideoHistoryResposne>[]>;
+  ): Promise<IFindVideoHistoryResposne[]>;
 }
