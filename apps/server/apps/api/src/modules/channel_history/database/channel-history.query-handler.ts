cc3bc1d5cc06502b90992a26e16c430c2c59423f
godkor200 <godkor200@gmail.com>
2023-11-03 22:22:11 +0900

    refacter : repository 추가

--- apps/server/apps/api/src/modules/channel_history/database/channel-history.query-handler.ts
+++ apps/server/apps/api/src/modules/channel_history/database/channel-history.query-handler.ts
@@ -2,6 +2,7 @@ import { AwsOpensearchConnetionService } from '@Apps/common/aws/service/aws.open
 import { ChannelHistoryOutboundPort } from '@Apps/modules/channel_history/database/channel-history.outbound.port';
 import { IChannelHistoryRes } from '@Apps/modules/channel_history/dtos/expected-views.res';
 import { IFindVideoHistoryResposne } from '@Apps/modules/video_history/interface/find-video.history.resposne';
+import { from, lastValueFrom, map } from 'rxjs';
 
 export class ChannelHistoryQueryHandler
   extends AwsOpensearchConnetionService
@@ -49,5 +50,56 @@ export class ChannelHistoryQueryHandler
       },
     };
     return await this.fullScan<IChannelHistoryRes>(searchQuery);
+  }
+
+  async findChannelHistoryByLimit(
+    channelIds: string[],
+    size: number,
+    order: 'desc' | 'asc',
+  ): Promise<IChannelHistoryRes[]> {
+    let searchQuery = {
+      index: `new_channel_history`,
+      scroll: '10s',
+      size,
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                terms: {
+                  channel_id: channelIds,
+                },
+              },
+              /**
+               * 지금 new-channel-history 인덱스를 사용하기 때문에 날짜가 의미가 없음
+               *
+               *
+               *
+               *
+               {
+               range: {
+               crawled_date: {
+               gte: fromDate + ' 00:00:00', // 시작 날짜 (greater than or equal)
+               lte: toDate + ' 00:00:00', // 종료 날짜 (less than or equal)
+               },
+               },
+               }
+               */
+            ],
+          },
+        },
+        sort: [
+          {
+            crawled_date: {
+              order,
+            },
+          },
+        ],
+      },
+    };
+    const observable$ = from(
+      this.client.search(searchQuery).then((res) => res.body.hits.hits),
+    ).pipe(map((hits) => hits.map((hit) => hit._source)));
+    return await lastValueFrom(observable$);
   }
 }
