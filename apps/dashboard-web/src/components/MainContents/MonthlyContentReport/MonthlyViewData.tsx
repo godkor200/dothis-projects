ba4028400d7e46990c7d9eb7f3b5fe0e5aa94146
YunCow <skypnal12@gmail.com>
2023-11-11 21:25:52 +0900

    feat: 월별 데이터 섹션 완료(클러스터 데이터 없는 경우 때문에 더미 데이터로 적용)

--- apps/dashboard-web/src/components/MainContents/MonthlyContentReport/MonthlyViewData.tsx
+++ apps/dashboard-web/src/components/MainContents/MonthlyContentReport/MonthlyViewData.tsx
@@ -3,17 +3,23 @@
 import { ResponsiveRadar } from '@nivo/radar';
 import { useState } from 'react';
 
-import SvgComp from '@/components/common/SvgComp';
-import useGetDailyView from '@/hooks/react-query/query/useGetDailyView';
-import useGetVideoData from '@/hooks/react-query/query/useGetVideoData';
-import {
-  DUMMY_VIEW_DATA,
-  MONTHLY_VIEW_DUMMY_DATA,
-} from '@/mocks/monthlyReport/monthlyViewDummyData';
+import { clustersCategories } from '@/constants/clusterCategories';
+import useGetRelWords from '@/hooks/react-query/query/useGetRelWords';
+import { DUMMY_VIEW_DATA } from '@/mocks/monthlyReport/monthlyViewDummyData';
+import getMaxValues from '@/utils/contents/getMaxValues';
 
-import SummaryItem from '../AnalysisWidgetItem';
+import MonthlyDataGraphToolTip from './MonthlyDataGraphToolTip';
+
+/**
+ * 데이터 안정화 되면 custom hooks 주석 풀고 더미 데이터 제거 예정입니다.
+ */
 
 type TitleType = (typeof TITLE_BUTTON)[number]['type'];
+
+export interface ViewData {
+  views: number;
+  videoTotalCounts: number;
+}
 
 const TITLE_BUTTON = [
   { label: '주요 카테고리', type: 'category' },
@@ -22,34 +28,60 @@ const TITLE_BUTTON = [
 
 const MonthlyViewData = () => {
   const [selectedType, setSelectedType] = useState<TitleType>('category');
-  const { data: viewData, isLoading: isViewLoading } = useGetDailyView();
-  const { data: videoData, isLoading: isVideoLoading } = useGetVideoData();
-  // const isLoading = isViewLoading || isVideoLoading;
+  const { data, isLoading: isWordLoading } = useGetRelWords();
+  // const { data: viewData, isLoading: isViewLoading } = useGetDailyView();
+  // const { data: videoData, isLoading: isVideoLoading } = useGetVideoData();
 
-  // if (isLoading) return null;
+  // const isLoading = isViewLoading || isVideoLoading || isWordLoading;
 
-  // const categoryViewData = viewData.map((item, idx) => {
-  //   const result = item?.reduce((acc: Record<string, string | number>, cur) => {
-  //     acc.views = ((acc.views as number) || 0) + cur.increase_views;
+  if (isWordLoading) return null;
 
-  //     return acc;
-  //   }, {});
+  // const categoryViewData: ViewData[] = viewData.map((item, idx) => {
+  //   const result: ViewData = (item ?? []).reduce(
+  //     (acc: ViewData, cur) => {
+  //       acc.views = ((acc.views as number) || 0) + cur.increase_views;
+  //       return acc;
+  //     },
+  //     { views: 0, videoTotalCounts: 0 },
+  //   );
 
-  //   if (result) {
-  //     result.views = result.views || 200;
-  //     result.videoTotalCounts = videoData[idx]?.total.value as number;
-  //   }
+  //   result.views = result.views || 200;
+  //   result.videoTotalCounts = (videoData[idx]?.total.value as number) ?? 200;
 
   //   return result;
   // });
+
+  const { maxViews, maxVideoTotalCounts, viewAndVideoMaxValue } =
+    getMaxValues(DUMMY_VIEW_DATA);
+  const clusterData: number[] = data && JSON.parse(data.cluster);
+
+  const convertedDatas = DUMMY_VIEW_DATA.map(
+    ({ views, videoTotalCounts }, idx) => {
+      return {
+        views: (views / maxViews) * viewAndVideoMaxValue,
+        videoTotalCounts:
+          (videoTotalCounts / maxVideoTotalCounts) * viewAndVideoMaxValue,
+        category:
+          clustersCategories[
+            clusterData[idx] as keyof typeof clustersCategories
+          ],
+      };
+    },
+  );
 
   const onClickTitle = (type: TitleType) => {
     setSelectedType(type);
   };
 
+  const toolTipProps = {
+    maxViews,
+    maxVideoTotalCounts,
+    viewAndVideoMaxValue,
+  };
+
   return (
     <>
-      <h3 className="typo-t2 mt-10 flex items-center gap-[4px]">
+      {/* <h3 className="typo-t2 mt-10 flex items-center gap-[4px]">
         월간 View
         <SvgComp icon="Question" size={18} />
       </h3>
@@ -57,7 +89,7 @@ const MonthlyViewData = () => {
         {MONTHLY_VIEW_DUMMY_DATA.map(({ title, content }) => (
           <SummaryItem key={title} title={title} content={content} />
         ))}
-      </ul>
+      </ul> */}
       <div className="rounded-8 border-grey400 mt-10 flex flex-col border border-solid px-[30px] py-[40px] ">
         <div className="text-t2 text-grey400 flex items-center gap-[10px] font-bold">
           {TITLE_BUTTON.map((item, idx) => (
@@ -79,9 +111,9 @@ const MonthlyViewData = () => {
         <div className="h-[315px] w-[406px] self-center">
           {selectedType === 'category' ? (
             <ResponsiveRadar
-              data={DUMMY_VIEW_DATA}
+              data={convertedDatas}
               keys={['views', 'videoTotalCounts']}
-              indexBy="videoTotalCounts"
+              indexBy="category"
               valueFormat=">-.2f"
               margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
               borderColor={{ from: 'color' }}
@@ -92,6 +124,13 @@ const MonthlyViewData = () => {
               colors={{ scheme: 'nivo' }}
               blendMode="multiply"
               motionConfig="wobbly"
+              sliceTooltip={({ index, data }) => (
+                <MonthlyDataGraphToolTip
+                  data={data}
+                  label={index}
+                  {...toolTipProps}
+                />
+              )}
               legends={[
                 {
                   anchor: 'top-left',
