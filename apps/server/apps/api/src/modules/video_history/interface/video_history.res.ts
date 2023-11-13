c26db40f590fb95a0cf3a24b8bd1e2eb180f235c
godkor200 <godkor200@gmail.com>
2023-11-13 17:06:33 +0900

    refacter : 데이터 enum 추가로 템플릿으로 받아 올수 있게끔 수정

---
+++ apps/server/apps/api/src/modules/video_history/interface/video_history.res.ts
@@ -0,0 +1,8 @@
+export enum VIDEO_HISTORY_DATA {
+  'VIDEO_ID' = 'video_id',
+  'VIDEO_VIEWS' = 'video_views',
+  'VIDEO_LIKES' = 'video_likes',
+  'VIDEO_COMMENTS' = 'video_comments',
+  'CRAWLED_DATE' = 'crawled_date',
+  'PERFORMANCE' = 'performance',
+}
