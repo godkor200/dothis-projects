650f9b506a96eb81487d8e5023d8a599351ac9de
mini <jma1020@naver.com>
2023-11-16 03:47:43 +0900

    feat: middleware  단 랜덤키워드 쿼리 생성  (임시 주석)

--- apps/dashboard-web/src/middleware.ts
+++ apps/dashboard-web/src/middleware.ts
@@ -9,52 +9,55 @@ import { apiServer } from './utils/api/apiServer';
 export async function middleware(request: NextRequest) {
   // if (request.nextUrl.pathname.startsWith('/contents')) {
   //   const requestHeaders = new Headers(request.headers);
-
   //   requestHeaders.set(
   //     'Authorization',
   //     request.cookies.get('accessToken')?.value as string,
   //   );
-
   //   const data = await fetch('https://api.dothis.kr/v2/user/keyword', {
   //     // headers: requestHeaders,
   //   });
-
   //   const responseData = await data.json();
   //   if (responseData.statusCode === 401) {
   //     const auth = await fetch('https://api.dothis.kr/v1/auth/verify-token', {
   //       credentials: 'include',
   //     });
-
   //     console.log(auth);
   //     const authResponse = await auth.json();
-
   //     console.log(authResponse);
   //   }
-
   //   const response = NextResponse.redirect(
   //     new URL('/about?search=dashboard', request.nextUrl),
   //   );
   //   response.cookies.set('test', 'test');
   //   return response;
   // }
-
-  let count = 0;
-  if (request.nextUrl.pathname.startsWith('/contents')) {
-    const test = ['먹방', '도라에몽', '이태원', '엽떡'];
-
-    console.log(request);
-
-    return NextResponse.redirect(
-      new URL(
-        `/contents?guestKeyword=${test[Math.floor(Math.random() * 2)]}`,
-        request.nextUrl,
-      ),
-    );
-  }
-
-  // const response = NextResponse.next();
-
-  // return response;
+  // 랜덤으로 게스트 키워드 생성해주는 코드 (아래)
+  // if (
+  //   request.nextUrl.pathname.startsWith('/contents') &&
+  //   !request.nextUrl.searchParams.has('guestKeyword')
+  // ) {
+  //   const test = ['check', 'phone', 'rice', 'paper', 'news', 'date'];
+  //   const requestHeaders = new Headers(request.headers);
+  //   requestHeaders.set(
+  //     'Authorization',
+  //     request.cookies.get('accessToken')?.value as string,
+  //   );
+  //   const data = await fetch('https://api.dothis.kr/v1/auth/own-info', {
+  //     headers: {
+  //       Authorization: request.cookies.get('accessToken')?.value as string,
+  //     },
+  //   });
+  //   const responseData = await data.json();
+  //   // console.log(responseData.data.personalizationTag);
+  //   return NextResponse.redirect(
+  //     new URL(
+  //       `/contents?guestKeyword=${test[Math.floor(Math.random() * 6)]}`,
+  //       request.nextUrl,
+  //     ),
+  //   );
+  // }
+  const response = NextResponse.next();
+  return response;
 }
 
 export const config = {
