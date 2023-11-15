0a13d38dd4d9ba0643fe3a42c3454811603719d6
mini <jma1020@naver.com>
2023-11-16 04:04:22 +0900

    feat: contents template 수정

--- apps/dashboard-web/src/app/(main)/contents/template.tsx
+++ apps/dashboard-web/src/app/(main)/contents/template.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { getCookie, setCookie } from 'cookies-next';
+import type { Route } from 'next';
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import type { ChangeEvent } from 'react';
 import { type PropsWithChildren, useEffect } from 'react';
 
 import KeywordSearchResult from '@/components/MainContents/KeywordSearch/KeywordSearchResult';
@@ -21,6 +26,32 @@ const MainContentTemplate = ({ children }: PropsWithChildren) => {
   useEffect(() => {
     setRandomIndex(Math.floor(Math.random() * 3));
   }, []);
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const onSelect = () => {
+    // now you got a read/write object
+    const current = new URLSearchParams(Array.from(searchParams!.entries())); // -> has to use this form
+
+    // update as necessary
+
+    current.set('guestKeyword', 'my');
+
+    // cast to string
+    const search = current.toString();
+    // or const query = `${'?'.repeat(search.length && 1)}${search}`;
+    const query = search ? `?${search}` : '';
+
+    router.replace(`${pathname}${query}` as Route);
+  };
+
+  // 새로고침 trigger
+  useEffect(() => {
+    router.replace('/contents');
+  }, [router]);
+
   return (
     <>
       <KeywordSlide />
