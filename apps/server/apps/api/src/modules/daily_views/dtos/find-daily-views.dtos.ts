419003523c93836651243ab19cc8454e0991a0c0
godkor200 <godkor200@gmail.com>
2023-11-16 16:01:18 +0900

    refacter : 데일리뷰 v3 추가

--- apps/server/apps/api/src/modules/daily_views/dtos/find-daily-views.dtos.ts
+++ apps/server/apps/api/src/modules/daily_views/dtos/find-daily-views.dtos.ts
@@ -22,3 +22,26 @@ export class FindDailyViewsQuery implements IQuery {
 
 export interface FindDailyViewsDtos
   extends Omit<FindDailyViewsQuery, 'clusterNumber'> {}
+
+export class FindDailyViewsV3Query implements IQuery {
+  readonly clusterNumber: string;
+
+  readonly keyword: string;
+
+  readonly relationKeyword?: string;
+
+  readonly from: Date;
+
+  readonly to: Date;
+
+  constructor(props: FindDailyViewsV3Query) {
+    this.clusterNumber = props.clusterNumber;
+    this.keyword = props.keyword;
+    this.relationKeyword = props.relationKeyword;
+    this.from = props.from;
+    this.to = props.to;
+  }
+}
+
+export interface FindDailyViewsV3Dtos
+  extends Omit<FindDailyViewsV3Query, 'clusterNumber'> {}
