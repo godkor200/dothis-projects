51dab77411b345c15cf393b9659ac35670fb876d
godkor200 <godkor200@gmail.com>
2023-11-16 15:51:48 +0900

    refacter : 데일리뷰 v3 추가

--- apps/server/apps/api/src/modules/daily_views/queries/v2/daily-views-api-v2.module.ts
+++ apps/server/apps/api/src/modules/daily_views/queries/v2/daily-views-api-v2.module.ts
@@ -1,10 +1,10 @@
 import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { FindDailyViewsOsHttpController } from '@Apps/modules/daily_views/queries/v2/find-daily-views/find-daily-views.http.controller';
+import { FindDailyViewsOsHttpController } from '@Apps/modules/daily_views/queries/v2/find-daily-views/find-daily-views.v2.http.controller';
 
 import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
 import { AwsModule } from '@Apps/common/aws/aws.module';
-import { FindDailyViewsQueryOsHandler } from '@Apps/modules/daily_views/queries/v2/find-daily-views/find-daily-views.query-handler';
+import { FindDailyViewsQueryOsHandler } from '@Apps/modules/daily_views/queries/v2/find-daily-views/find-daily-views.v2.query-handler';
 import { VIDEO_HISTORY_OS_DI_TOKEN } from '@Apps/modules/video_history/video_history.di-token';
 import { VideoHistoryQueryHandler } from '@Apps/modules/video_history/database/video_history.query-handler';
 import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
