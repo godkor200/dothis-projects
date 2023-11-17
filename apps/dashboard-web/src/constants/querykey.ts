07e3871aad6a176a334b5f874f3ec8d33e4bdf4f
mini <jma1020@naver.com>
2023-11-17 13:56:07 +0900

    feat: querykey 리터럴(상수) 추가

---
+++ apps/dashboard-web/src/constants/querykey.ts
@@ -0,0 +1,8 @@
+import { createQueryKeys } from '@/utils/querykey';
+
+export const USER_KEY = createQueryKeys('user');
+export const RELATIONWORD_KEY = createQueryKeys('relationword');
+export const DAILYVIEW_KEY = createQueryKeys('dailyview');
+export const EXPECTEDVIEW_KEY = createQueryKeys('expectedview');
+export const AUTO_COMPLETEWORD_KEY = createQueryKeys('autocompleteword');
+export const VIDEODATA_KEY = createQueryKeys('videodata');
