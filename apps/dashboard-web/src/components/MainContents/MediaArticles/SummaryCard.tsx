e2d1a252d363b4b2c950c8c63db0fd9866b2012d
YunCow <skypnal12@gmail.com>
2023-11-20 22:38:11 +0900

    feat: SummaryCard 컴포넌트 추가

---
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/SummaryCard.tsx
@@ -0,0 +1,25 @@
+import type { PropsWithChildren } from 'react';
+import React from 'react';
+
+interface SummaryCardProps extends PropsWithChildren {
+  title: string;
+  marginTop?: 'mt-10' | 'mt-5';
+}
+
+const SummaryCard = ({
+  children,
+  title,
+  marginTop = 'mt-10',
+}: SummaryCardProps) => {
+  console.log(marginTop);
+  return (
+    <div
+      className={`border-grey300 rounded-8 ${marginTop} border border-solid px-[30px] py-10`}
+    >
+      <h3 className="text-t2 text-grey700 mb-5 font-bold">{title}</h3>
+      {children}
+    </div>
+  );
+};
+
+export default SummaryCard;
