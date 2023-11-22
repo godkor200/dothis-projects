23cd25b79dd6237f6795272d8791c6584487e19d
mini <jma1020@naver.com>
2023-11-22 11:37:39 +0900

    feat: type 선언

--- apps/dashboard-web/src/constants/convertText.ts
+++ apps/dashboard-web/src/constants/convertText.ts
@@ -5,3 +5,20 @@ export const CONVERT_SUBSCRIBERANGE = {
   '100000~500000': '구독자 10만 이상 50만명 미만의 채널',
   '500000이상': '구독자 50만명 이상의 채널',
 };
+
+export type VideoCount =
+  | '1000~10000'
+  | '10000~50000'
+  | '50000~100000'
+  | '100000~500000'
+  | '500000이상';
+
+interface ResponseItem {
+  id: string;
+  label: string;
+  value: number;
+}
+
+export type ResponseType = {
+  [key in VideoCount]: ResponseItem;
+};
