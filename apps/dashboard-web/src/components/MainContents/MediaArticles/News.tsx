bf5fad2d84ae7cdccfd4282ddd503e72a8d5c3a8
mini <jma1020@naver.com>
2023-11-24 13:58:30 +0900

    feat: News api 연관어로 기사를 가져올 수 있도록 수정

--- apps/dashboard-web/src/components/MainContents/MediaArticles/News.tsx
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/News.tsx
@@ -4,6 +4,7 @@ import { useMemo, useState } from 'react';
 
 import ParserContent from '@/components/common/ParserContent';
 import useKeyword from '@/hooks/user/useKeyword';
+import { useSelectedRelWord } from '@/store/selectedRelWordStore';
 import { externaImageLoader, getMainImage } from '@/utils/imagesUtil';
 
 import ArticleList from './ArticleList';
@@ -19,11 +20,13 @@ const News = () => {
 
   const { hashKeywordList } = useKeyword();
 
+  const selectedRelWord = useSelectedRelWord();
+
   const retrievePosts = async () => {
     const obj = {
       access_key: 'eb75ee2e-b1f6-4ada-a964-9bf94c5a2f26',
       argument: {
-        query: hashKeywordList[0],
+        query: selectedRelWord,
 
         published_at: {
           from: '2023-09-01',
@@ -58,11 +61,11 @@ const News = () => {
     return data;
   };
 
-  const { data: original } = useQuery(
-    ['뉴스', hashKeywordList[0]],
+  const { data: original, isLoading } = useQuery(
+    ['뉴스', selectedRelWord],
     retrievePosts,
     {
-      enabled: !!hashKeywordList[0],
+      enabled: !!hashKeywordList[0] && !!selectedRelWord,
     },
   );
 
@@ -82,11 +85,21 @@ const News = () => {
     [original],
   );
 
-  if (!returnData) {
+  if (isLoading) {
     return (
       <div className="mt-10 flex gap-[1.25rem]">
         <CurrentArticle.skeleton />
         <ArticleList.skeleton />
+      </div>
+    );
+  }
+
+  if (returnData.length === 0) {
+    return (
+      <div className="mt-10 flex flex-wrap gap-[1.25rem]">
+        <p className="text-t2 flex h-60 w-full items-center justify-center text-center font-bold">
+          해당 키워드에 대한 뉴스기사가 없습니다
+        </p>
       </div>
     );
   }
