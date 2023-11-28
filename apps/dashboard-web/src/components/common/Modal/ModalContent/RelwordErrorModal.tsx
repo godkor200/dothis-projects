13d6dc3077f102e4566560679f28a12ed5d3b31a
mini <jma1020@naver.com>
2023-11-28 15:37:25 +0900

    docs: 주석 추가-

--- apps/dashboard-web/src/components/common/Modal/ModalContent/RelwordErrorModal.tsx
+++ apps/dashboard-web/src/components/common/Modal/ModalContent/RelwordErrorModal.tsx
@@ -2,6 +2,11 @@ import { Button } from 'dashboard-storybook/src/components/Button/Button';
 
 import SvgComp from '../../SvgComp';
 
+/**
+ * 기획단에서 논의가 필요해서 callback은 달아주지 못하였습니다.
+ * @param dismissCallback 해당 컴포넌트를 닫기를 눌렀을 때 처리되는 callback
+ * @returns
+ */
 const RelwordErrorModal = ({
   dismissCallback,
 }: {
