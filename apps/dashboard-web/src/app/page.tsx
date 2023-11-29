02d80c2e3951141f62ad999f254e6a3fad44c8ae
mini <jma1020@naver.com>
2023-11-29 18:53:18 +0900

    feat: 랜딩페이지 Footer삽입

--- apps/dashboard-web/src/app/page.tsx
+++ apps/dashboard-web/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from '@/components/common/Layout/Footer';
 import Landing from '@/components/Landing';
 
 export default async function RootPage() {
   return (
     <>
       <Landing />
+      <Footer />
     </>
   );
 }
