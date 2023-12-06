c33d51334f9dedf4a621d484af7de663e7135d85
godkor200 <godkor200@gmail.com>
2023-12-06 19:26:23 +0900

    refacter : 채널 히스토리 agg 모듈화

--- apps/server/apps/api/src/modules/channel_history/service/channel-history.service.module.ts
+++ apps/server/apps/api/src/modules/channel_history/service/channel-history.service.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChannelHistoryDataService } from '@Apps/modules/channel_history/service/channel-history-data.service';
+import { ChannelHistoryAggregateService } from '@Apps/modules/channel_history/service/channel-history.aggregate.service';
 
 @Module({
-  providers: [ChannelHistoryDataService],
-  exports: [ChannelHistoryDataService],
+  providers: [ChannelHistoryDataService, ChannelHistoryAggregateService],
+  exports: [ChannelHistoryDataService, ChannelHistoryAggregateService],
 })
 export class ChannelHistoryServiceModule {}
