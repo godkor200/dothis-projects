393ecd9b1b3559bbb1f9c242c544b95fe8628fd8
godkor200 <godkor200@gmail.com>
2023-12-06 19:29:51 +0900

    refacter : 타입명 수정

--- apps/server/apps/api/src/modules/rel-words/interface/find-rel.adapter.ts
+++ apps/server/apps/api/src/modules/rel-words/interface/find-rel.adapter.ts
@@ -1,7 +1,7 @@
-import { KeywordRes, RelwordsRes } from '@Libs/commons/src/types/dto.types';
+import { KeywordRes, RelWordsEntity } from '@Libs/commons/src/types/dto.types';
 
 export interface FindRelAdapter {
-  findOneByKeyword: (option: string) => Promise<RelwordsRes>;
+  findOneByKeyword: (option: string) => Promise<RelWordsEntity>;
 
   findAllKeyword: () => Promise<KeywordRes[]>;
 }
