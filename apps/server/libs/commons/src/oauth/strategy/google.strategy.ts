e71c244784c066e5529e80acd0f081bd73476258
godkor200 <godkor200@gmail.com>
2023-12-08 11:46:24 +0900

    refacter : 구글 유튜브 권한 변경

--- apps/server/libs/commons/src/oauth/strategy/google.strategy.ts
+++ apps/server/libs/commons/src/oauth/strategy/google.strategy.ts
@@ -35,7 +35,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         'email',
         'profile',
         'https://www.googleapis.com/auth/youtube.readonly',
-        'https://www.googleapis.com/auth/youtubepartner',
       ],
     });
   }
