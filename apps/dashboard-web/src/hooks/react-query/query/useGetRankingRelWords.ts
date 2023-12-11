28d6f3f77418bc92bcf39495b6d00266a85de937
mini <jma1020@naver.com>
2023-12-11 16:50:56 +0900

    feat:  useGetRankingRelWords 쿼리 훅 생성

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetRankingRelWords.ts
@@ -0,0 +1,25 @@
+import type { apiRouter } from '@dothis/dto/src/lib/apiRouter';
+import type { UseQueryOptions } from '@ts-rest/react-query';
+
+import { AUTO_COMPLETEWORD_KEY } from '@/constants/querykey';
+import { apiClient } from '@/utils/api/apiClient';
+
+const useGetRankingRelWords = (
+  keyword: string,
+  queryOptions?: UseQueryOptions<typeof apiRouter.relwords.rankRel>,
+) => {
+  const queryResult = apiClient(1).relwords.rankRel.useQuery(
+    ['rank', keyword],
+    { params: { keyword } },
+    { enabled: !!keyword, ...queryOptions },
+  );
+
+  return {
+    ...queryResult,
+    data: queryResult.data?.body.data
+      .sort((a, b) => b.expectedViews - a.expectedViews)
+      .map((item) => item.word),
+  };
+};
+
+export default useGetRankingRelWords;
