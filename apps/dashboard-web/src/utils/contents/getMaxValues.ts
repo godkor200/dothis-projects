b13ff01dd7316aebf109b318abfb07c1bf73dc41
mini <jma1020@naver.com>
2023-12-11 15:33:43 +0900

    fix: radar 차트 계산식에서 NaN으로 출력되는 이슈로 인한 default value 1로 수정

--- apps/dashboard-web/src/utils/contents/getMaxValues.ts
+++ apps/dashboard-web/src/utils/contents/getMaxValues.ts
@@ -1,8 +1,8 @@
 import type { ViewData } from '@/components/MainContents/MonthlyContentReport/MonthlyViewData';
 
 const getMaxValues = (data: ViewData[]) => {
-  let maxViews = 0;
-  let maxVideoTotalCounts = 0;
+  let maxViews = 1;
+  let maxVideoTotalCounts = 1;
 
   data.forEach((item) => {
     maxViews = Math.max(maxViews, item.views);
