fec51920e6efc7a7993e398968eb96a53b151af1
godkor200 <godkor200@gmail.com>
2023-12-12 16:10:12 +0900

    refacter : channel_name 가로채서 담는 로직 구현

--- apps/server/apps/api/src/modules/video/queries/v1/find-video-paging/find-video-page.query-handler.ts
+++ apps/server/apps/api/src/modules/video/queries/v1/find-video-paging/find-video-page.query-handler.ts
@@ -6,6 +6,8 @@ import { VideoServicePort } from '@Apps/modules/video/database/video.service.por
 import { VideoNotFoundError } from '@Apps/modules/video/domain/event/video.error';
 import { Inject } from '@nestjs/common';
 import { Ok, Result, Err } from 'oxide.ts';
+import { CHANNEL_OS_DI_TOKEN } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
+import { ChannelQueryHandlerPort } from '@Apps/modules/channel/database/channel.query-handler.port';
 @QueryHandler(FindVideoPageQuery)
 export class FindVideoPageQueryHandler
   implements IQueryHandler<FindVideoPageQuery, Result<IPagingRes, Error>>
@@ -13,12 +15,35 @@ export class FindVideoPageQueryHandler
   constructor(
     @Inject(VIDEO_OS_DI_TOKEN)
     protected readonly video: VideoServicePort,
+
+    @Inject(CHANNEL_OS_DI_TOKEN)
+    protected readonly channelData: ChannelQueryHandlerPort,
   ) {}
   async execute(
     arg: FindVideoPageQuery,
   ): Promise<Result<IPagingRes, VideoNotFoundError>> {
     const res = await this.video.findVideoPaging(arg);
     if (!res.total.value) return Err(new VideoNotFoundError());
-    return Ok(res);
+
+    const videos = await Promise.all(
+      res.data.map(async (video) => {
+        const channel_name = await this.channelData.findChannelName(
+          video._source.channel_id,
+        );
+        return {
+          ...video,
+          _source: {
+            ...video._source,
+            channel_name,
+          },
+        };
+      }),
+    );
+    const updatedRes = {
+      ...res,
+      data: videos,
+    };
+
+    return Ok(updatedRes);
   }
 }
