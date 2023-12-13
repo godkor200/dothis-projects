850b246fb49f4bbd5d358382afa14ba006706f90
mini <jma1020@naver.com>
2023-12-14 03:51:26 +0900

    refactor: ts-rest error response 정의

--- apps/dashboard-web/src/utils/api/apiClient.ts
+++ apps/dashboard-web/src/utils/api/apiClient.ts
@@ -1,7 +1,7 @@
 import { apiRouter } from '@dothis/dto/src/lib/apiRouter';
 import type { ApiRouteResponse } from '@ts-rest/core';
 import { initQueryClient } from '@ts-rest/react-query';
-import type { Method } from 'axios';
+import type { AxiosError, AxiosResponse, Method } from 'axios';
 
 import { serverApiBaseUrl } from '@/constants/dev';
 
@@ -17,13 +17,22 @@ export const apiClient = (version: number) => {
     },
     credentials: 'include',
     api: async ({ path, method, headers, body }) => {
-      const result = await apiInstance.request({
-        headers,
-        method: method as Method,
-        url: path,
-        data: body,
-      });
-      return { status: result.status, body: result.data };
+      try {
+        const result = await apiInstance.request({
+          headers,
+          method: method as Method,
+          url: path,
+          data: body,
+        });
+        return { status: result.status, body: result.data };
+      } catch (e: Error | AxiosError | any) {
+        const error = e as AxiosError;
+        const response = error.response as AxiosResponse;
+        return {
+          status: response.status,
+          body: response.data,
+        };
+      }
     },
   });
 };
