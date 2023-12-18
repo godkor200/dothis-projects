372079dd47c76a1ada285ad47c29080b527a13c2
mini <jma1020@naver.com>
2023-12-18 17:53:23 +0900

    feat: 전체탭 테이블 api 및 util 연결

--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationWord.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationWord.tsx
@@ -1,5 +1,18 @@
+import { useMemo } from 'react';
+
+import type { ResponseType, VideoCount } from '@/constants/convertText';
+import { CONVERT_SUBSCRIBERANGE } from '@/constants/convertText';
+import useGetDailyView from '@/hooks/react-query/query/useGetDailyView';
+import useGetExpectedView from '@/hooks/react-query/query/useGetExpectedView';
 import useGetRelWords from '@/hooks/react-query/query/useGetRelWords';
+import useGetVideoCount from '@/hooks/react-query/query/useGetVideoCount';
+import { useEndDate, useStartDate } from '@/store/dateStore';
 import { cn } from '@/utils/cn';
+import {
+  averageViews,
+  formatToLineGraph,
+  sumViews,
+} from '@/utils/contents/dailyview';
 
 interface Props {
   keyword: string;
@@ -13,6 +26,87 @@ interface Props {
 
 const RelationWord = ({ keyword, relword, index, arr }: Props) => {
   // const { data: relword } = useGetRelWords(keyword);
+
+  const { data: dailyViewData } = useGetDailyView({
+    keyword: keyword,
+    relword: relword,
+  });
+
+  const startDate = useStartDate();
+  const endDate = useEndDate();
+
+  const dailyViewChartData = useMemo(
+    () =>
+      formatToLineGraph(
+        sumViews(dailyViewData.flat(), { startDate, endDate }),
+        '일일 조회 수',
+      ),
+    [dailyViewData],
+  );
+
+  const lastDailyView = dailyViewChartData[0].data.at(-1)?.y;
+
+  const { data: expectedViewData } = useGetExpectedView({
+    keyword: keyword,
+    relword: relword,
+  });
+
+  const expectedViewChartData = useMemo(
+    () =>
+      formatToLineGraph(
+        averageViews(expectedViewData.flat(), { startDate, endDate }),
+        '기대 조회 수',
+      ),
+    [expectedViewData],
+  );
+
+  const lastExpectedView = expectedViewChartData[0].data.at(-1)?.y;
+
+  const { data: videoCountData } = useGetVideoCount({
+    keyword: keyword,
+    relword: relword,
+  });
+
+  const { totalCount, videoCountViewChartData } = useMemo(
+    () =>
+      videoCountData.reduce<{
+        totalCount: number;
+        videoCountViewChartData: ResponseType;
+      }>(
+        (acc, dataItem) => {
+          acc.totalCount += dataItem?.videoTotal || 0;
+          dataItem?.section.forEach((sectionItem) => {
+            const key = sectionItem.section;
+
+            if (key in CONVERT_SUBSCRIBERANGE) {
+              const existingRange = CONVERT_SUBSCRIBERANGE[key as VideoCount];
+              const existingItem =
+                acc.videoCountViewChartData[key as VideoCount];
+
+              if (existingItem) {
+                existingItem.value += sectionItem.number;
+              } else {
+                acc.videoCountViewChartData[key as VideoCount] = {
+                  id: existingRange,
+                  label: existingRange,
+                  value: sectionItem.number,
+                };
+              }
+            }
+          });
+
+          return acc;
+        },
+        { totalCount: 0, videoCountViewChartData: {} } as {
+          totalCount: number;
+          videoCountViewChartData: ResponseType;
+        },
+      ),
+    [videoCountData],
+  );
+
+  console.log(totalCount);
+  console.log(videoCountViewChartData);
 
   return (
     <div
@@ -31,10 +125,10 @@ const RelationWord = ({ keyword, relword, index, arr }: Props) => {
         {keyword}
       </div>
       <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-        기대 조회 수(배)
+        {lastExpectedView}
       </div>
       <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-        일일 조회 수
+        {lastDailyView}
       </div>
       <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
         누적 영상 수
