6f1d802a27c30e660f3c7c0ddd56edf1b64d343a
godkor200 <godkor200@gmail.com>
2023-12-18 15:52:51 +0900

    feat : membership 엔티티

--- apps/server/apps/api/src/modules/membership/domain/membership.entity.ts
+++ apps/server/apps/api/src/modules/membership/domain/membership.entity.ts
@@ -3,8 +3,8 @@ import {
   Column,
   Entity,
   PrimaryGeneratedColumn,
-  ManyToOne,
   JoinColumn,
+  OneToOne,
 } from 'typeorm';
 
 @Entity({ name: 'membership' })
@@ -33,7 +33,7 @@ export class Membership {
   @Column({ name: 'created_at' })
   createdAt: Date;
 
-  // @ManyToOne((type) => User, (user) => user.UserChannelData)
-  // @JoinColumn({ name: 'userId' })
-  // User: User;
+  @OneToOne((type) => User, (user) => user.id)
+  @JoinColumn({ name: 'userId' })
+  User: User;
 }
