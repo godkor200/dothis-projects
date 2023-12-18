3af35a864a51ef69fac929f0985330ed5ed8d9a4
mini <jma1020@naver.com>
2023-12-18 17:08:37 +0900

    feat: RelationWord Item 컴포넌트 분리

--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import useGetRankingRelWords from '@/hooks/react-query/query/useGetRankingRelWords';
 import useGetRankingWordList from '@/hooks/react-query/query/useGetRankingWordList';
-import useGetRelWords from '@/hooks/react-query/query/useGetRelWords';
 import useKeyword from '@/hooks/user/useKeyword';
 import { cn } from '@/utils/cn';
 
+import RelationWord from './RelationWord';
+
 const Container = () => {
-  const { data: relword } = useGetRelWords();
   const { hashKeywordList } = useKeyword();
-  const { data: rank } = useGetRankingWordList(hashKeywordList);
+
+  const { data: rank, isLoading } = useGetRankingWordList(hashKeywordList, {
+    onError: (data) => {},
+  });
 
   return (
     <div className="bg-grey00 rounded-8 shadow-[inset_0_0_0_2px_rgb(228,228,231)]">
@@ -38,41 +40,15 @@ const Container = () => {
         </div>
       </div>
       <ul>
+        {/* props드릴링이 생기는 것보다는 각기 다르게 호출해주자. */}
         {rank?.map((item, index, arr) => (
-          <div
-            key={item.word + index}
-            className={cn(
-              'grid grid-cols-[minmax(250px,1fr)_140px_110px_110px_100px_100px_minmax(150px,1fr)_minmax(150px,1fr)] items-center gap-[12px] ',
-              {
-                'shadow-[inset_0_-2px_0_0_#f4f4f5]': index !== arr.length - 1,
-              },
-            )}
-          >
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              {item.word}
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              {item.keyword}
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              기대 조회 수(배)
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              일일 조회 수
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              누적 영상 수
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              경쟁강도
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              구독자 10만 이상 채널
-            </div>
-            <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
-              구독자 수 비슷한 채널
-            </div>
-          </div>
+          <RelationWord
+            key={index + item.relword}
+            arr={arr}
+            index={index}
+            keyword={item.keyword}
+            relword={item.relword}
+          />
         ))}
       </ul>
     </div>
--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationWord.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationWord.tsx
@@ -1,5 +1,55 @@
-const RelationWord = () => {
-  return <div></div>;
+import useGetRelWords from '@/hooks/react-query/query/useGetRelWords';
+import { cn } from '@/utils/cn';
+
+interface Props {
+  keyword: string;
+  relword: string;
+  index: number;
+  arr: {
+    relword: string;
+    keyword: string;
+  }[];
+}
+
+const RelationWord = ({ keyword, relword, index, arr }: Props) => {
+  // const { data: relword } = useGetRelWords(keyword);
+
+  return (
+    <div
+      key={relword + index}
+      className={cn(
+        'grid grid-cols-[minmax(250px,1fr)_140px_110px_110px_100px_100px_minmax(150px,1fr)_minmax(150px,1fr)] items-center gap-[12px] ',
+        {
+          'shadow-[inset_0_-2px_0_0_#f4f4f5]': index !== arr.length - 1,
+        },
+      )}
+    >
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        {relword}
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        {keyword}
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        기대 조회 수(배)
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        일일 조회 수
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        누적 영상 수
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        경쟁강도
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        구독자 10만 이상 채널
+      </div>
+      <div className="text-grey700 py-[26px] pl-[8px] text-[14px] font-bold ">
+        구독자 수 비슷한 채널
+      </div>
+    </div>
+  );
 };
 
 export default RelationWord;
