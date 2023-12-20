163fc89a566bc403aa7bc4dd22f41b1d4c10b672
mini <jma1020@naver.com>
2023-12-20 18:33:32 +0900

    feat: 분리한 컴포넌트 생성

--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
@@ -1,53 +1,54 @@
 'use client';
 
+import { ResponsiveLine } from '@nivo/line';
+import dayjs from 'dayjs';
+import { useCallback, useState } from 'react';
+
+import CustomTooltip from '@/components/MainContents/InfoChartAndRanking/CustomTooltip';
+import { VIEWCHART_LABEL } from '@/components/MainContents/InfoChartAndRanking/KeywordAnalyticsView';
+import { NIVO_CHART_COLOR } from '@/constants/nivoChartColor';
 import useGetRankingWordList from '@/hooks/react-query/query/useGetRankingWordList';
 import useKeyword from '@/hooks/user/useKeyword';
 import { cn } from '@/utils/cn';
 
+import RelationGraph from './RelationGraph';
+import RelationTable from './RelationTable';
 import RelationWord from './RelationWord';
 
-const Container = () => {
-  const { hashKeywordList } = useKeyword();
+export type NivoLineChart = Array<{
+  id: string;
+  color?: string;
+  data: Array<{ x: string; y: number }>;
+}>;
 
-  const { data: rank, isLoading } = useGetRankingWordList(hashKeywordList, {
-    onError: (data) => {},
-  });
+const Container = () => {
+  const [dailyViewChartDataList, setDailyViewChartDataList] =
+    useState<NivoLineChart>([]);
+
+  const [expectedViewChartDataList, setExpectedViewChartDataList] =
+    useState<NivoLineChart>([]);
+
+  const setDaily = useCallback(() => {
+    return setDailyViewChartDataList;
+  }, []);
+
+  const setExpected = useCallback(() => {
+    return setExpectedViewChartDataList;
+  }, []);
 
   return (
-    <div className="bg-grey00 rounded-8 shadow-[inset_0_0_0_2px_rgb(228,228,231)]">
-      <div className="grid grid-cols-[minmax(250px,1fr)_140px_150px_150px_150px_100px_minmax(150px,1fr)] items-center gap-[12px] py-[30px] shadow-[inset_0_-1px_0_0_#d4d4d8]">
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">소재</div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          키워드
-        </div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          기대 조회 수(배)
-        </div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          일일 조회 수
-        </div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          누적 영상 수
-        </div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          경쟁강도
-        </div>
-        <div className="text-grey500 pl-[8px] text-[14px] font-bold">
-          구독자 10만 이상 채널
-        </div>
-      </div>
-      <ul>
-        {/* props드릴링이 생기는 것보다는 각기 다르게 호출해주자. */}
-        {rank?.map((item, index, arr) => (
-          <RelationWord
-            key={index + item.relword}
-            arr={arr}
-            index={index}
-            keyword={item.keyword}
-            relword={item.relword}
-          />
-        ))}
-      </ul>
+    <div className="flex flex-col gap-[20px]">
+      {dailyViewChartDataList.length === 0 ? null : (
+        <RelationGraph
+          dailyViewChartDataList={dailyViewChartDataList}
+          expectedViewChartDataList={expectedViewChartDataList}
+        />
+      )}
+
+      <RelationTable
+        setDailyViewChartDataList={setDaily}
+        setExpectedViewChartDataList={setExpected}
+      />
     </div>
   );
 };
