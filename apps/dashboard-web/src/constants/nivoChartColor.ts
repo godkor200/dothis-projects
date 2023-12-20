5b4b719b2844805ecc192196068ceef6444b8b13
mini <jma1020@naver.com>
2023-12-20 16:16:22 +0900

    feat: NIVO_CHART_COLOR 상수 array 생성

---
+++ apps/dashboard-web/src/constants/nivoChartColor.ts
@@ -0,0 +1,12 @@
+export const NIVO_CHART_COLOR = [
+  'hsl(0, 100%, 50%)', // Red
+  'hsl(240, 100%, 50%)', // Blue
+  'hsl(120, 100%, 50%)', // Green
+  'hsl(60, 100%, 50%)', // Yellow
+  'hsl(300, 100%, 50%)', // Magenta
+  'hsl(180, 100%, 50%)', // Cyan
+  'hsl(0, 100%, 25%)', // Dark Red
+  'hsl(120, 100%, 25%)', // Dark Green
+
+  'hsl(0, 0%, 50%)', // Light Gray
+];
