8e7f6f34b3b30c08a30619ac34f7facfb741f787
mini <jma1020@naver.com>
2023-12-27 11:20:51 +0900

    remove: 사용하지않는 import 제거

--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/Container.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { ResponsiveLine } from '@nivo/line';
-import dayjs from 'dayjs';
-import { useCallback, useState } from 'react';
-
-import CustomTooltip from '@/components/MainContents/InfoChartAndRanking/CustomTooltip';
-import { VIEWCHART_LABEL } from '@/components/MainContents/InfoChartAndRanking/KeywordAnalyticsView';
-import { NIVO_CHART_COLOR } from '@/constants/nivoChartColor';
-import useGetRankingWordList from '@/hooks/react-query/query/useGetRankingWordList';
-import useKeyword from '@/hooks/user/useKeyword';
-import { cn } from '@/utils/cn';
+import { useState } from 'react';
 
 import RelationGraph from './RelationGraph';
 import RelationTable from './RelationTable';
-import RelationWord from './RelationWord';
 
 export type NivoLineChart = Array<{
   id: string;
