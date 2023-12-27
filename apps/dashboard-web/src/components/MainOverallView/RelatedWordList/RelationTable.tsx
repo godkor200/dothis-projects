4d09eaa295640de77d477b9957c8347df26aa96c
mini <jma1020@naver.com>
2023-12-27 10:40:56 +0900

    feat: Props 추가 및 keyword에 따른 setDailyViewChartDataList 필터링 로직 추가

--- apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationTable.tsx
+++ apps/dashboard-web/src/components/MainOverallView/RelatedWordList/RelationTable.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import useGetRankingWordList from '@/hooks/react-query/query/useGetRankingWordList';
 import useKeyword from '@/hooks/user/useKeyword';
 
@@ -5,15 +7,19 @@ import type { NivoLineChart } from './Container';
 import RelationWord from './RelationWord';
 
 interface Props {
-  setDailyViewChartDataList: () => React.Dispatch<
+  dailyViewChartDataList: NivoLineChart;
+  expectedViewChartDataList: NivoLineChart;
+  setDailyViewChartDataList: React.Dispatch<
     React.SetStateAction<NivoLineChart>
   >;
-  setExpectedViewChartDataList: () => React.Dispatch<
+  setExpectedViewChartDataList: React.Dispatch<
     React.SetStateAction<NivoLineChart>
   >;
 }
 
 const RelationTable = ({
+  dailyViewChartDataList,
+  expectedViewChartDataList,
   setDailyViewChartDataList,
   setExpectedViewChartDataList,
 }: Props) => {
@@ -22,6 +28,26 @@ const RelationTable = ({
     onError: (data) => {},
   });
 
+  /**
+   * hashKeywordList에 변동이 있을 경우 (어떤 키워드를 제거할 경우) 제거한 키워드에 연관어를 지우는 useEffect
+   */
+  useEffect(() => {
+    setDailyViewChartDataList((prev) =>
+      prev.filter((item) =>
+        rank.some((rankItem) => rankItem.relword === item.id),
+      ),
+    );
+
+    setExpectedViewChartDataList((prev) =>
+      prev.filter((item) =>
+        rank.some((rankItem) => rankItem.relword === item.id),
+      ),
+    );
+  }, [JSON.stringify(hashKeywordList)]);
+
+  if (isLoading) {
+    return null;
+  }
   return (
     <div className="bg-grey00 rounded-8 shadow-[inset_0_0_0_2px_rgb(228,228,231)]">
       <div className="grid grid-cols-[minmax(250px,1fr)_140px_150px_150px_150px_100px_minmax(150px,1fr)] items-center gap-[12px] py-[30px] shadow-[inset_0_-1px_0_0_#d4d4d8]">
@@ -54,6 +80,8 @@ const RelationTable = ({
             index={index}
             keyword={item.keyword}
             relword={item.relword}
+            dailyViewChartDataList={dailyViewChartDataList}
+            expectedViewChartDataList={expectedViewChartDataList}
             setDailyViewChartDataList={setDailyViewChartDataList}
             setExpectedViewChartDataList={setExpectedViewChartDataList}
 
