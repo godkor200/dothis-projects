09153a3d388c0b5dec11be3a1feb14f59bf5ae88
mini <jma1020@naver.com>
2023-12-28 16:58:23 +0900

    feat: TabNav에 CATEGORY_TABNAV_DATA 추가

--- apps/dashboard-web/src/constants/TabNav.ts
+++ apps/dashboard-web/src/constants/TabNav.ts
@@ -13,3 +13,11 @@ export const MYPAGE_TABNAV_DATA = [
   { title: '내 이용권 관리', category: 'manage' },
   { title: 'FAQ', category: 'faq' },
 ] as const;
+
+export const CATEGORY_TABNAV_DATA = [
+  { title: '주요 카테고리', category: 'category' },
+  { title: '채널 비교', category: 'channel' },
+];
+
+export type CategoryTabNavDataCategoryType =
+  (typeof CATEGORY_TABNAV_DATA)[number]['category'];
