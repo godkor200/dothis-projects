bebe68909e2cb82c7a2ddeab71102809141ea3a3
godkor200 <godkor200@gmail.com>
2024-01-01 23:53:32 +0900

    refacter : 개별 영상 api

---
+++ apps/server/apps/api/src/modules/video/queries/v1/find-individual-video-info/find-individual-video-info.http.controller.ts
@@ -0,0 +1,63 @@
+import { QueryBus } from '@nestjs/cqrs';
+import { Controller, NotFoundException, Param } from '@nestjs/common';
+import { IRes } from '@Libs/commons/src/types/res.types';
+import { FindIndividualVideoInfoV1Dto } from '@Apps/modules/video/dtos/find-individual-video-info.dto';
+import { match, Result } from 'oxide.ts';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import { apiRouter, VideoDetailsModel } from '@dothis/dto';
+import { nestControllerContract, TsRest } from '@ts-rest/nest';
+import { VideoNotFoundError } from '@Apps/modules/video/domain/event/video.error';
+import { VideoInfoRes } from '@Libs/commons/src/types/dto.types';
+const c = nestControllerContract(apiRouter.video);
+const { summary, responses, description } = c.getIndividualVideo;
+
+@ApiTags('영상')
+@Controller()
+export class FindIndividualVideoInfoHttpController {
+  constructor(private readonly queryBus: QueryBus) {}
+  @ApiParam({
+    name: 'videoId',
+    description: '찾을 비디오의 id 값을 입력받습니다.',
+    example: 'Mm-8O8iFmao',
+  })
+  @ApiParam({
+    name: 'clusterNumber',
+    description: '찾을 비디오의 클러스터 번호 값을 입력받습니다.',
+    example: '0',
+  })
+  @TsRest(c.getIndividualVideo)
+  @ApiInternalServerErrorResponse()
+  @ApiOkResponse({ type: VideoInfoRes })
+  @ApiNotFoundResponse({
+    description: VideoNotFoundError.message,
+  })
+  @ApiOperation({
+    summary,
+    description,
+  })
+  async execute(
+    @Param() param: { videoId: string; clusterNumber: string },
+  ): Promise<IRes<VideoDetailsModel>> {
+    const arg = new FindIndividualVideoInfoV1Dto(param);
+    const result: Result<VideoDetailsModel, NotFoundException> =
+      await this.queryBus.execute(arg);
+    return match(result, {
+      Ok: (result) => ({ success: true, data: result }),
+      Err: (err: Error) => {
+        if (err instanceof VideoNotFoundError) {
+          throw new NotFoundException(err.message);
+        }
+        throw err;
+      },
+    });
+  }
+}
---
+++ apps/server/apps/api/src/modules/video/queries/v1/find-individual-video-info/find-individual-video-info.query-handler.ts
@@ -0,0 +1,79 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { FindIndividualVideoInfoV1Dto } from '@Apps/modules/video/dtos/find-individual-video-info.dto';
+import { Inject } from '@nestjs/common';
+import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
+import { VideoServicePort } from '@Apps/modules/video/database/video.service.port';
+import { VideoDetailsModel } from '@dothis/dto';
+import { Err, Ok, Result } from 'oxide.ts';
+import { VideoNotFoundError } from '@Apps/modules/video/domain/event/video.error';
+import { VideoAggregateService } from '@Apps/modules/video/service/video.aggregate.service';
+import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
+import { ChannelHistoryOutboundPort } from '@Apps/modules/channel_history/database/channel-history.outbound.port';
+
+@QueryHandler(FindIndividualVideoInfoV1Dto)
+export class FindIndividualVideoInfoQueryHandler
+  implements
+    IQueryHandler<
+      FindIndividualVideoInfoV1Dto,
+      Result<VideoDetailsModel, VideoNotFoundError>
+    >
+{
+  constructor(
+    @Inject(VIDEO_OS_DI_TOKEN)
+    private readonly video: VideoServicePort,
+
+    @Inject(CHANNEL_HISTORY_OS_DI_TOKEN)
+    private readonly channelHistory: ChannelHistoryOutboundPort,
+
+    private readonly videoAggregateService: VideoAggregateService,
+  ) {}
+
+  async execute(
+    query: FindIndividualVideoInfoV1Dto,
+  ): Promise<Result<VideoDetailsModel, VideoNotFoundError>> {
+    //영상 태그 불러오기
+    const video = await this.video.findVideoInfo(
+      query.clusterNumber,
+      query.videoId,
+    );
+    const channel = await this.channelHistory.findChannelHistoryInfo(
+      video._source.channel_id,
+    );
+
+    if (!video.found) return Err(new VideoNotFoundError());
+
+    const videoData = video._source;
+    const videoTags = videoData.video_tags;
+    const videoHistory = videoData.video_history;
+    const channelAverageViews = channel.channel_average_views;
+
+    const dailyViewAggregate =
+      this.videoAggregateService.calculateIncreaseBySource(videoHistory);
+    const lastHistory = videoHistory[videoHistory.length - 1];
+    const lastViews = lastHistory.video_views;
+    const lastComments = lastHistory.video_comments;
+    const lastLikes = lastHistory.video_likes;
+    /**
+     * 기대조회수 expectedViews
+     */
+    const expectedViews = lastViews / channelAverageViews;
+    /**
+     * 참여도 participationRate
+     * */
+    const participationRate = (lastComments + lastLikes) / lastViews;
+
+    const videoPublishedDate = videoData.video_published;
+    const videoPrediction = this.videoAggregateService.getVideoPrediction(
+      videoPublishedDate,
+      dailyViewAggregate,
+    );
+    const subscribers = channel.channel_subscribers;
+
+    return Ok({
+      videoTags,
+      videoPerformance: { expectedViews, participationRate },
+      videoPrediction,
+      channelPerformance: { subscribers, averageViews: channelAverageViews },
+    });
+  }
+}
