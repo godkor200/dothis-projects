94db920498764b9ac0e2d2bc416344dbb466ba7f
mini <jma1020@naver.com>
2024-01-03 11:06:30 +0900

    feat:useGetVideoPagination 쿼리 훅 생성 (video fetch 에서 사용)

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetVideoPagination.ts
@@ -0,0 +1,62 @@
+import type { apiRouter } from '@dothis/dto/src/lib/apiRouter';
+import type { UseQueryOptions } from '@ts-rest/react-query';
+
+import { VIDEODATA_KEY } from '@/constants/querykey';
+import { apiClient } from '@/utils/api/apiClient';
+
+import useGetRelWords from './useGetRelWords';
+
+export const videoKeys = {
+  video: ['video'],
+};
+
+const useGetVideoPagination = (
+  page: number,
+  {
+    keyword,
+    relword,
+  }: {
+    keyword: string | null;
+    relword: string | null;
+  },
+  lastIndex_ID?: string,
+  queryOptions?: UseQueryOptions<typeof apiRouter.video.getVideoPageV2>,
+) => {
+  const { data } = useGetRelWords(keyword);
+
+  let clusters: string[] = [];
+
+  console.log(lastIndex_ID);
+
+  if (data && data.cluster) {
+    clusters = JSON.parse(data.cluster);
+  }
+
+  const queryResults = apiClient(2).video.getVideoPageV2.useQuery(
+    VIDEODATA_KEY.list([
+      {
+        relword: relword,
+        keyword: keyword,
+        page: page,
+      },
+    ]),
+    {
+      query: {
+        last: lastIndex_ID ? lastIndex_ID : undefined,
+        limit: 5,
+        related: relword!,
+        search: keyword!,
+        cluster: clusters.join(','),
+      },
+    },
+    { ...queryOptions, enabled: !!data && !!relword },
+  );
+
+  queryResults.isPreviousData;
+  return {
+    ...queryResults,
+    data: queryResults.data?.body,
+  };
+};
+
+export default useGetVideoPagination;
