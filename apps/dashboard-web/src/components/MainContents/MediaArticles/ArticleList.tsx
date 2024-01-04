51df63c07f93df8a7d8a825e9e61307f3749e82b
mini <jma1020@naver.com>
2024-01-04 17:29:21 +0900

    design: 스크롤 상시 활성화로 인한 오른쪽 padding 수정

--- apps/dashboard-web/src/components/MainContents/MediaArticles/ArticleList.tsx
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/ArticleList.tsx
@@ -20,7 +20,8 @@ const ArticleList = ({
   onChange,
 }: ArticleListProps) => {
   return (
-    <div className="accodient-box h-[630px] cursor-pointer  overflow-hidden px-5  pt-[0.375rem] hover:overflow-auto ">
+    // hover:overflow-auto
+    <div className="accodient-box h-[630px] cursor-pointer  overflow-auto pl-5 pr-3  pt-[0.375rem]">
       <InView onChange={onChange} threshold={0.5}>
         {articleListData.map(
           ({ title, category, date, provider, image }, index) => {
