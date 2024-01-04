a4e1fbe39f13f63b7f038e6e94effd91fe553180
mini <jma1020@naver.com>
2024-01-04 12:05:22 +0900

    fix: AnalysisWidgetItem Tooltip 컴포넌트 추가

--- apps/dashboard-web/src/components/MainContents/AnalysisWidgetItem.tsx
+++ apps/dashboard-web/src/components/MainContents/AnalysisWidgetItem.tsx
@@ -1,14 +1,30 @@
+import TooltipComponent from '../common/tooltips/Tooltip';
+
 export interface AnalysisWidgetItemProps {
   title: string;
   content: string;
+  hasTooltip: boolean;
+  tooltipText: string;
 }
 
-const AnalysisWidgetItem = ({ title, content }: AnalysisWidgetItemProps) => {
+const AnalysisWidgetItem = ({
+  title,
+  content,
+  hasTooltip,
+  tooltipText,
+}: AnalysisWidgetItemProps) => {
   return (
     <li className="rounded-8 bg-primary50 w-[18.1rem] grow py-5">
       <div className="border-primary500 border-l-2 pl-[25px] font-bold">
         <div className="text-primary500 text-[20px]">{content}</div>
-        <div className="text-grey600 mb-1">{title}</div>
+        <div className="text-grey600 mb-1 flex gap-[4px]">
+          <p> {title}</p>
+          {hasTooltip && (
+            <div className="flex items-center ">
+              <TooltipComponent title={tooltipText} />
+            </div>
+          )}
+        </div>
       </div>
     </li>
   );
