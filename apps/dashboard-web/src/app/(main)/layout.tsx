50e549ede6b5059a0ec69326a2a4e02cda15fc66
mini <jma1020@naver.com>
2024-01-08 16:55:11 +0900

    design: 메인 : w1440 이상부터는 bg-grey100 bg 컬러 가로값에 꽉 채워져야 함 -> QA 수정으로 인한 구조 추가 및 디자인 수정

--- apps/dashboard-web/src/app/(main)/layout.tsx
+++ apps/dashboard-web/src/app/(main)/layout.tsx
@@ -10,13 +10,15 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="border-grey400 z-index-1 absolute box-border h-[5.5rem] w-full border-b border-solid p-5" />
 
       {/* SideBar 영역 width 박스 */}
-      <div className="w-[80px]" />
+      <div className=" expandSideBar:w-[16.625rem]  w-[80px] flex-shrink-0" />
 
-      <div className=" mx-auto  w-[1342px]">
-        <div className="">
+      <div className=" flex-1">
+        <div className=" mx-auto  w-[1342px]">
           <GNB />
-          <>{children}</>
-          {/* Footer infinity border */}
+        </div>
+        <>{children}</>
+        {/* Footer infinity border */}
+        <div className=" mx-auto  w-[1342px]">
           <div className="border-grey400 z-index-1 absolute left-0 box-border h-[1px] w-full border-b border-solid" />
           <Footer />
         </div>
