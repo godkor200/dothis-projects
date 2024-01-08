11ec3fb35d691a17bc39887d64ed807a11b3212a
mini <jma1020@naver.com>
2024-01-08 11:50:44 +0900

    feat: Blog 페이지 임시 생성

---
+++ apps/dashboard-web/src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import BlogDetail from '@/components/Blog/BlogDetail';
+
+const BlogPage = () => {
+  return (
+    <ul>
+      <BlogDetail title="dd" content="232" date="232" />
+    </ul>
+  );
+};
+
+export default BlogPage;
