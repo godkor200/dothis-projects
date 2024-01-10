fd225028f9080ec36c5d75c783b14414ec03a2e5
godkor200 <godkor200@gmail.com>
2024-01-10 20:48:29 +0900

    refacter : 디렉토리 변경

--- apps/server/apps/api/src/modules/video/queries/v2/video-api-v2.module.ts
+++ apps/server/apps/api/src/modules/video/queries/v2/video-api-v2.module.ts
@@ -2,7 +2,7 @@ import { Module, Provider } from '@nestjs/common';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
-import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/database/channel-history.query-handler';
+import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/repository/database/channel-history.query-handler';
 import { FindAccumulateVideosV2QueryHandler } from '@Apps/modules/video/queries/v2/find-accumulate-videos/find-accumulate-videos.query-handler';
 import { ChannelHistoryAggregateService } from '@Apps/modules/channel_history/service/channel-history.aggregate.service';
 import { FindAccumulateVideosV2HttpController } from '@Apps/modules/video/queries/v2/find-accumulate-videos/find-accumulate-videos.http.controller';
