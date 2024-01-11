783ea15539034eb1296f6fa5f78f538f9ff696c0
mini <jma1020@naver.com>
2024-01-11 13:32:33 +0900

    fix: Guest 키워드 수정

--- apps/dashboard-web/src/constants/guest.ts
+++ apps/dashboard-web/src/constants/guest.ts
@@ -1,9 +1,3 @@
-export const GUEST_KEYWORD = [
-  '먹방',
-  '키즈카페',
-  '고양이',
-  '미국주식',
-  '강아지',
-];
+export const GUEST_KEYWORD = ['먹방', '아이돌', '고양이', '게임', '강아지'];
 
 export const GUEST_AVERAGEVIEW = Math.floor(Math.random() * (1101 - 900) + 900);
