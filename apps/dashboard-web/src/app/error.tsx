5dc8fc4d8407f7895db3a925f5de1adb2903eb28
mini <jma1020@naver.com>
2024-01-11 15:25:30 +0900

    feat: error , 404 페이지 생성 및 워딩 수정

--- apps/dashboard-web/src/app/error.tsx
+++ apps/dashboard-web/src/app/error.tsx
@@ -1,20 +1,35 @@
+'use client'; // Error components must be Client Components
+
 import { Button } from 'dashboard-storybook/src/components/Button/Button';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
-export default function NotFound() {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  console.log(error.name);
+  console.log(error.message);
+
   return (
     <div className="absolute flex h-full w-full flex-col items-center justify-center">
       <h2 className="text-grey500 border-grey500 mb-[1rem] flex h-20  w-20 items-center justify-center rounded-full border-4 text-[48px] font-bold">
         !
       </h2>
-      <h2 className="mb-[1rem] text-[48px] font-bold ">404</h2>
       <p className="mb-[0.5rem] text-[26px] font-bold">
-        원하시는 페이지를 찾을 수 없습니다.
+        서비스에 접속할 수 없습니다.
       </p>
       <div className="text-grey600 mb-[3rem] text-center text-[16px]">
-        <p>찾으려는 페이지의 주소가 잘못 입력되었거나,</p>
-        <p>주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.</p>
-        <p>입력하시 페이지의 주소가 정확한지 다시 한번 확인해 주세요.</p>
+        <p>죄송합니다. 기술적인 문제로 일시적으로 접속되지 않았습니다.</p>
+        <p>잠시 후 다시 확인해주세요.</p>
       </div>
       <Link href="/contents">
         <Button size="L" theme="contained">
