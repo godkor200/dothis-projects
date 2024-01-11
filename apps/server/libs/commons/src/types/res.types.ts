c20193768356d777135ad8a38cc9a490aa190d29
godkor200 <godkor200@gmail.com>
2024-01-11 15:00:44 +0900

    refacter : res type 폴더 합침

--- apps/server/libs/commons/src/types/res.types.ts
+++ apps/server/libs/commons/src/types/res.types.ts
@@ -1,4 +1,51 @@
+import { createZodDto } from '@anatine/zod-nestjs';
+import { extendApi } from '@anatine/zod-openapi';
+import {
+  WeeklyKeywordsListSourceSchema,
+  zChannelAnalysis,
+  zDailyViewData,
+  zExpectedViewsData,
+  zKeywords,
+  zRankRes,
+  zResWordsPickData,
+  zTokenExpired,
+  zVideoDetails,
+  zVideoHistory,
+  zVideoModel,
+  zWeeklyKeywordsList,
+} from '@dothis/dto';
+import { UserDto } from '@Libs/commons/src/types/dto.types';
+
 export interface IRes<T> {
   success: boolean;
   data?: T | any;
 }
+
+export class UserRes extends UserDto {}
+export class VideoHistoryRes extends createZodDto(extendApi(zVideoHistory)) {}
+
+export class VideoRes extends createZodDto(extendApi(zVideoModel)) {}
+
+export class RelWordsEntity extends createZodDto(
+  extendApi(zResWordsPickData),
+) {}
+export class RelWordsRankingRes extends createZodDto(extendApi(zRankRes)) {}
+export class KeywordRes extends createZodDto(extendApi(zKeywords)) {}
+export class TokenExpired extends createZodDto(extendApi(zTokenExpired)) {}
+
+export class ExpectedViewsData extends createZodDto(
+  extendApi(zExpectedViewsData),
+) {}
+
+export class IncreaseData extends createZodDto(extendApi(zDailyViewData)) {}
+export class WeeklyData extends createZodDto(
+  extendApi(WeeklyKeywordsListSourceSchema),
+) {}
+export class VideoInfoRes extends createZodDto(extendApi(zVideoDetails)) {}
+
+export class ChannelAnalysisRes extends createZodDto(
+  extendApi(zChannelAnalysis),
+) {}
+export class WeeklyKeywordsRes extends createZodDto(
+  extendApi(zWeeklyKeywordsList),
+) {}
