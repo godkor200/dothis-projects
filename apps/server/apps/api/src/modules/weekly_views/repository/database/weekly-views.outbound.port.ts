1d41a9c56819921729c5938a607c0ab1ac2aa9e3
godkor200 <godkor200@gmail.com>
2024-01-12 15:03:50 +0900

    refacter : 위클리 레포지토리에 타입 선언

--- apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.outbound.port.ts
+++ apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.outbound.port.ts
@@ -1,5 +1,9 @@
 import { GetWeeklyViewsQuery } from '@Apps/modules/weekly_views/dtos/get-weekly-views-list.dto';
+import { WeeklyKeywordsRes } from '@Libs/commons/src/types/res.types';
+import { WeeklyViewsError } from '@Apps/modules/weekly_views/domain/event/weekly-views.error';
 
 export interface WeeklyViewsOutboundPort {
-  getPaginatedWeeklyViewsByKeyword(arg: GetWeeklyViewsQuery): Promise<any>;
+  getPaginatedWeeklyViewsByKeyword(
+    arg: GetWeeklyViewsQuery,
+  ): Promise<WeeklyKeywordsRes | WeeklyViewsError>;
 }
--- apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.query-handler.ts
+++ apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.query-handler.ts
@@ -2,9 +2,10 @@ import { WeeklyViewsOutboundPort } from '@Apps/modules/weekly_views/repository/d
 import { AwsOpenSearchConnectionService } from '@Apps/common/aws/service/aws.opensearch.service';
 import { GetWeeklyViewsQuery } from '@Apps/modules/weekly_views/dtos/get-weekly-views-list.dto';
 import { from, lastValueFrom } from 'rxjs';
-import { query } from 'express';
-import { Err } from 'oxide.ts';
+import { Err, Ok, Result } from 'oxide.ts';
 import { WeeklyViewsError } from '@Apps/modules/weekly_views/domain/event/weekly-views.error';
+import { WeeklyKeywordsRes } from '@Libs/commons/src/types/res.types';
+
 export class SearchQueryBuilder {
   static WeeklyViews(index: string, limit: number, last?: string) {
     let searchQuery = {
@@ -23,14 +24,19 @@ export class WeeklyViewsQueryHandler
 {
   async getPaginatedWeeklyViewsByKeyword(
     arg: GetWeeklyViewsQuery,
-  ): Promise<any> {
+  ): Promise<WeeklyKeywordsRes | WeeklyViewsError> {
     const { from: DateForm, limit, last } = arg;
     const alias = 'weekly-views-' + DateForm;
     const indexList = await this.getIndices(alias);
     const index = indexList[0].index;
+    if (!indexList && !index) {
+      return new WeeklyViewsError(
+        WeeklyViewsError.message + 'The date must be set from 01/08/2024.',
+      );
+    }
     const searchQuery = SearchQueryBuilder.WeeklyViews(index, limit, last);
     const totalDoc = await this.countDocuments(searchQuery);
-    if (!totalDoc) return Err(new WeeklyViewsError());
+    if (!totalDoc) return new WeeklyViewsError();
     const observable$ = from(
       this.client.search(searchQuery).then((res) => ({
         total: totalDoc,
