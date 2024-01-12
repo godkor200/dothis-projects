138accba139c63b6ea94798d0f45fa6eeb3a9738
godkor200 <godkor200@gmail.com>
2024-01-12 18:04:00 +0900

    refacter : 타입 수정

--- apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.outbound.port.ts
+++ apps/server/apps/api/src/modules/weekly_views/repository/database/weekly-views.outbound.port.ts
@@ -1,9 +1,9 @@
-import { GetWeeklyViewsQuery } from '@Apps/modules/weekly_views/dtos/get-weekly-views-list.dto';
+import { GetWeeklyViewsDto } from '@Apps/modules/weekly_views/dtos/get-weekly-views-list.dto';
 import { WeeklyKeywordsRes } from '@Libs/commons/src/types/res.types';
 import { WeeklyViewsError } from '@Apps/modules/weekly_views/domain/event/weekly-views.error';
 
 export interface WeeklyViewsOutboundPort {
   getPaginatedWeeklyViewsByKeyword(
-    arg: GetWeeklyViewsQuery,
+    arg: GetWeeklyViewsDto,
   ): Promise<WeeklyKeywordsRes | WeeklyViewsError>;
 }
