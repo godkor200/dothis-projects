3c955226fa7d1cdc8f287ddab93146389f031bb8
godkor200 <godkor200@gmail.com>
2024-01-12 14:58:44 +0900

    refacter : 이름 변경

--- apps/server/apps/api/src/modules/weekly_views/dtos/get-weekly-views-list.dto.ts
+++ apps/server/apps/api/src/modules/weekly_views/dtos/get-weekly-views-list.dto.ts
@@ -1,10 +1,16 @@
+import { createZodDto } from '@anatine/zod-nestjs';
+import { extendApi } from '@anatine/zod-openapi';
+import { zGetWeeklyViewsQuery } from '@dothis/dto';
 import { IPickDateFromLimitLast } from '@Apps/modules/daily_views/dtos/find-daily-views.dtos';
 
-export class GetWeeklyViewsQuery implements IPickDateFromLimitLast {
-  readonly from: Date;
+export class GetWeeklyViewsQuery extends createZodDto(
+  extendApi(zGetWeeklyViewsQuery),
+) {}
+export class GetWeeklyViewsDto implements IPickDateFromLimitLast {
+  readonly from: string;
   readonly limit: number;
   readonly last?: string;
-  constructor(props: GetWeeklyViewsQuery) {
+  constructor(props: GetWeeklyViewsDto) {
     Object.assign(this, props);
   }
 }
