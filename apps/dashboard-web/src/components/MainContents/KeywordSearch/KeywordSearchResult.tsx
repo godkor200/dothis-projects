4e2d0ee15d3fc8186afd912349158f58392587b4
mini <jma1020@naver.com>
2024-01-15 15:11:30 +0900

    design: SearchBar min height 주입에 따른 디자인 수정

--- apps/dashboard-web/src/components/MainContents/KeywordSearch/KeywordSearchResult.tsx
+++ apps/dashboard-web/src/components/MainContents/KeywordSearch/KeywordSearchResult.tsx
@@ -18,7 +18,7 @@ const KeywordSearchResult = () => {
   // 해당 로직은 백엔드에서 여러 키워드로 연관어를 뽑는 작업이 마무리되면 수정이 필요한 페이지입니다.
   // h1 mt class에 calc (24px은 KeywordSlide컴포넌트의 margin collapse현상, 52px은 SearchInput 컴포넌트의 abolute화로 인한 영역만큼 크기)
   return (
-    <h1 className="bg-primary50 mx-auto mb-[3.1rem] mt-[calc(24px+52px+60px)] flex max-w-[680px] flex-col rounded-2xl py-[18px] text-center text-[20px] font-bold">
+    <h1 className="bg-primary50 mx-auto mb-[3.1rem] mt-[calc(24px+60px)] flex max-w-[680px] flex-col rounded-2xl py-[18px] text-center text-[20px] font-bold">
       <span className="mb-1">
         <span>{hashKeywordList[0]}</span>{' '}
         {hashKeywordList.length > 1 && `외 ${hashKeywordList.length - 1}개`}{' '}
