b379acf53040348b0d6cc5c7704d91c502f29913
mini <jma1020@naver.com>
2024-01-16 10:35:42 +0900

    feat: 주간 날짜 슬라이드 제한 추가 (24년 1월부터 조회)

--- apps/dashboard-web/src/components/Trending/TrendingFilter/WeekDateSlider.tsx
+++ apps/dashboard-web/src/components/Trending/TrendingFilter/WeekDateSlider.tsx
@@ -21,28 +21,36 @@ const WeekDateSlider = ({ startDate, setStartDate }: Props) => {
   };
 
   const handleDecreaseDate = () => {
+    if (startDate.subtract(1, 'week').isBefore('2024-01-01', 'day')) {
+      return;
+    }
     setStartDate((prev) => prev.subtract(1, 'week'));
   };
 
   return (
-    <div className="flex items-center justify-between">
-      <ArrowLeftButton onClick={handleDecreaseDate}>
-        <SvgComp icon="KeywordLeftArrow" size="20px" />
-      </ArrowLeftButton>
+    <>
+      <div className="flex items-center justify-between">
+        <ArrowLeftButton onClick={handleDecreaseDate}>
+          <SvgComp icon="KeywordLeftArrow" size="20px" />
+        </ArrowLeftButton>
 
-      <div className="border-grey400 rounded-8 flex items-center border px-4 py-2">
-        <div className="">
-          <SvgComp icon="Calendar" size="20px" />
+        <div className="border-grey400 rounded-8 flex items-center border px-4 py-2">
+          <div className="">
+            <SvgComp icon="Calendar" size="20px" />
+          </div>
+
+          <p className="text-grey600 px-2">{`${startDate.format(
+            'YYYY-MM-DD',
+          )} ~ ${startDate.add(6, 'day').format('YYYY-MM-DD')}`}</p>
         </div>
-
-        <p className="text-grey600 px-2">{`${startDate.format(
-          'YYYY-MM-DD',
-        )} ~ ${startDate.add(6, 'day').format('YYYY-MM-DD')}`}</p>
+        <ArrowRightButton onClick={handleIncreaseDate}>
+          <SvgComp icon="KeywordRightArrow" size="20px" />
+        </ArrowRightButton>
       </div>
-      <ArrowRightButton onClick={handleIncreaseDate}>
-        <SvgComp icon="KeywordRightArrow" size="20px" />
-      </ArrowRightButton>
-    </div>
+      {/* <p className="text-grey500 mt-[20px] text-[14px]">
+        * 2024년 데이터부터 조회할 수 있습니다.
+      </p> */}
+    </>
   );
 };
 
