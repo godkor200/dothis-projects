fd20f992d7edee797a924f858e0279ec5535b418
mini <jma1020@naver.com>
2024-01-16 11:10:43 +0900

    feat: sort 기능 및 sort UI 추가

--- apps/dashboard-web/src/app/(main)/trending/page.tsx
+++ apps/dashboard-web/src/app/(main)/trending/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import { useState } from 'react';
 
@@ -10,18 +11,40 @@ import useGetTrendingKeywords from '@/hooks/react-query/query/useGetTrendingKeyw
 import { cn } from '@/utils/cn';
 import { convertCompetitionScoreFormat } from '@/utils/contents/competitionScore';
 
-const TrendingPage = () => {
-  const trendingTableHeaders = [
-    { label: '순위', key: 'rank' },
-    { label: '키워드', key: 'keyword' },
-    { label: '대표 카테고리', key: 'category' },
-    { label: '주간 조회수', key: 'views' },
-    { label: '영상 수', key: 'count' },
-    { label: '경쟁강도', key: 'competitive' },
-    { label: '구독자 10만 이상 채널', key: 'megachannel' },
-  ];
+export type TrendingQuery = {
+  selectOptions: { value: number; label: string }[];
+  keywordList: string[];
+  startDate: Dayjs;
+};
 
-  const { data, total } = useGetTrendingKeywords({ date: '2024-01-08' });
+export type SortingQuery = {
+  sort: (typeof trendingTableHeaders)[number]['key'];
+  order: 'asc' | 'desc';
+};
+
+export const trendingTableHeaders = [
+  { label: '순위', key: 'rank' },
+  { label: '키워드', key: 'keyword' },
+  { label: '대표 카테고리', key: 'category' },
+  { label: '주간 조회수', key: 'weekly_views' },
+  { label: '영상 수', key: 'video_count' },
+  { label: '경쟁강도', key: 'competitive' },
+  { label: '구독자 10만 이상 채널', key: 'mega_channel' },
+] as const;
+
+const TrendingPage = () => {
+  const [trendingQueryOption, setTrendingQueryOption] = useState<TrendingQuery>(
+    {
+      selectOptions: [],
+      keywordList: [],
+      startDate: dayjs().startOf('week').subtract(1, 'week').add(1, 'day'),
+    },
+  );
+
+  const [sortingParams, setSortingParams] = useState<SortingQuery>({
+    sort: 'weekly_views',
+    order: 'desc',
+  });
 
   const [openFilter, setOpenFilter] = useState<boolean>(false);
 
@@ -34,6 +57,28 @@ const TrendingPage = () => {
   const [startDate, setStartDate] = useState(
     dayjs().startOf('week').subtract(1, 'week').add(1, 'day'),
   );
+
+  const { data, total } = useGetTrendingKeywords({
+    selectOptions: trendingQueryOption.selectOptions,
+    keywordList: trendingQueryOption.keywordList,
+    startDate: trendingQueryOption.startDate,
+    order: sortingParams.order,
+    sort: sortingParams.sort,
+  });
+
+  const sortByTableKey = (
+    key: (typeof trendingTableHeaders)[number]['key'],
+  ) => {
+    if (key === 'rank' || key === 'category') {
+      return;
+    }
+    setSortingParams((prev) => {
+      if (prev.sort === key) {
+        return { ...prev, order: prev.order === 'asc' ? 'desc' : 'asc' };
+      }
+      return { ...prev, sort: key };
+    });
+  };
 
   return (
     <div className=" mx-auto max-w-[1100px] ">
@@ -50,9 +95,26 @@ const TrendingPage = () => {
         />
       </div>
       <div className="bg-grey00 rounded-8 shadow-[inset_0_0_0_2px_rgb(228,228,231)]">
-        <div className="grid grid-cols-[40px_140px_140px_140px_140px_140px_minmax(150px,1fr)] items-center gap-[12px] py-[30px] pl-[18px] shadow-[inset_0_-1px_0_0_#d4d4d8]">
+        <div className="grid grid-cols-[40px_140px_140px_140px_140px_140px_minmax(150px,1fr)] gap-[12px] py-[30px] pl-[18px] shadow-[inset_0_-1px_0_0_#d4d4d8]">
           {trendingTableHeaders.map(({ label, key }) => (
-            <div key={key} className="text-grey500 text-[14px] font-bold">
+            <div
+              key={key}
+              className={cn(
+                'text-grey500 before:bg-grey00 relative mx-auto w-fit cursor-pointer text-center text-[14px] font-bold ',
+                {
+                  ' before:absolute before:right-[-12px] before:top-1/2  before:mt-[-9px]  before:block before:h-0 before:w-0 before:border-4 before:border-transparent before:border-b-[#bfbfbf] after:absolute after:right-[-12px] after:top-1/2 after:mt-[1px] after:block after:h-0 after:w-0 after:border-4 after:border-transparent after:border-t-[#bfbfbf]':
+                    key !== 'rank' && key !== 'category',
+                  'before:border-b-[#000]':
+                    sortingParams.sort === key && sortingParams.order === 'asc',
+                  'after:border-t-[#000]':
+                    sortingParams.sort === key &&
+                    sortingParams.order === 'desc',
+                },
+              )}
+              onClick={() => {
+                sortByTableKey(key);
+              }}
+            >
               {label}
             </div>
           ))}
@@ -68,15 +130,15 @@ const TrendingPage = () => {
                 },
               )}
             >
-              <div className="flex items-center gap-[10px]">
-                <div className="text-grey700 py-[26px] text-[14px] font-bold ">
+              <div className=" items-center gap-[10px]">
+                <div className="text-grey700 py-[26px]  text-center text-[14px] font-bold ">
                   {index + 1}
                 </div>
               </div>
-              <div className="text-grey700 py-[26px]text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px]text-[14px] text-center font-bold ">
                 {item._source.keyword}
               </div>
-              <div className="text-grey700 py-[26px]  text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px]  text-center text-[14px]  font-bold">
                 {
                   clustersCategories[
                     JSON.parse(
@@ -85,16 +147,16 @@ const TrendingPage = () => {
                   ]
                 }
               </div>
-              <div className="text-grey700 py-[26px]  text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px]  text-center text-[14px]  font-bold">
                 {item._source.weekly_views?.toLocaleString('ko-kr')}
               </div>
-              <div className="text-grey700 py-[26px]  text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px]  text-center text-[14px] font-bold ">
                 {item._source.video_count?.toLocaleString('ko-kr')}
               </div>
-              <div className="text-grey700 py-[26px] text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px] text-center text-[14px]  font-bold">
                 {convertCompetitionScoreFormat(item._source.competitive)}
               </div>
-              <div className="text-grey700 py-[26px] text-[14px] font-bold ">
+              <div className="text-grey700 py-[26px] text-center text-[14px] font-bold ">
                 {item._source.mega_channel?.toLocaleString('ko-kr')}
               </div>
             </div>
@@ -110,6 +172,8 @@ const TrendingPage = () => {
           setKeywordList={setKeywordList}
           startDate={startDate}
           setStartDate={setStartDate}
+          setTrendingQueryOption={setTrendingQueryOption}
+          setOpenFilter={setOpenFilter}
         />
       )}
     </div>
