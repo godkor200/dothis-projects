276ad461bb95f57b86669791765036e747171421
godkor200 <godkor200@gmail.com>
2024-01-17 15:01:21 +0900

    refacter : searchkeyword 정리

--- packages/dto/src/lib/common.model.ts
+++ packages/dto/src/lib/common.model.ts
@@ -1,14 +1,28 @@
 import { z, ZodTypeAny } from 'zod';
 
 /**
+ * 탐색어 연관어
+ */
+export const zSearchKeyword = z.object({
+  keyword: z.string().describe('탐색어'),
+  related: z.string().describe('연관어').optional(),
+});
+/**
  * 기본 날짜 쿼리
  */
-export const dateQuery = z.object({ from: z.string(), to: z.string() });
+export const zDateQuery = z.object({ from: z.string(), to: z.string() });
+
+/**
+ * 페이지네이션 쿼리
+ */
 export const zPaginatedQuery = z.object({
   limit: z.number().describe('Specifies a limit of returned records'),
   last: z.string().describe('Last index returned').optional(),
 });
-
+/**
+ * 정렬 쿼리
+ * @param enumElement
+ */
 export const zSortQuery = (enumElement: Array<string>) => {
   if (!enumElement.length) {
     return z.object(undefined);
