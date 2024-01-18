958b09f886253830b666d871b3ff526d41f89c30
godkor200 <godkor200@gmail.com>
2024-01-18 13:37:39 +0900

    refacter : add example keyword

--- packages/dto/src/lib/common.model.ts
+++ packages/dto/src/lib/common.model.ts
@@ -4,20 +4,23 @@ import { z, ZodTypeAny } from 'zod';
  * 탐색어 연관어
  */
 export const zSearchKeyword = z.object({
-  keyword: z.string().describe('탐색어'),
-  related: z.string().describe('연관어').optional(),
+  keyword: z.string().describe('탐색어').default('먹방'),
+  related: z.string().describe('연관어').default('돼지고기').optional(),
 });
 /**
  * 기본 날짜 쿼리
  */
-export const zDateQuery = z.object({ from: z.string(), to: z.string() });
+export const zDateQuery = z.object({
+  from: z.string().describe('언제부터 날짜').default('2023-11-23'),
+  to: z.string().describe('까지 날짜').default('2023-11-30'),
+});
 
 /**
  * 페이지네이션 쿼리
  */
 export const zPaginatedQuery = z.object({
-  limit: z.number().describe('Specifies a limit of returned records'),
-  last: z.string().describe('Last index returned').optional(),
+  limit: z.number().describe('리턴되는 레코드의 숫자').default(5),
+  last: z.string().describe('마지막 인덱스').optional(),
 });
 /**
  * 정렬 쿼리
