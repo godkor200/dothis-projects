a18bd239bd1df3ab4dbfbda6bb1ba82547cb91e2
godkor200 <godkor200@gmail.com>
2024-01-22 13:16:17 +0900

    refacter : router v1

--- apps/server/apps/api/src/modules/story_board/domain/story-board.api.module.ts
+++ apps/server/apps/api/src/modules/story_board/domain/story-board.api.module.ts
@@ -1,5 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StoryBoardEntityModule } from '@Apps/modules/story_board/domain/entities/story-board.entity.module';
+import { StoryBoardV1Module } from '@Apps/modules/story_board/controllers/v1/story-board.v1.module';
+import { RouterModule } from '@nestjs/core';
 
-@Module({ imports: [StoryBoardEntityModule] })
+@Module({
+  imports: [
+    StoryBoardV1Module,
+    RouterModule.register([{ path: 'v1', module: StoryBoardV1Module }]),
+  ],
+})
 export class StoryBoardApiModule {}
