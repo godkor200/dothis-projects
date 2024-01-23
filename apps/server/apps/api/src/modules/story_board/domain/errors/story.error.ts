7d5a34089bcf321ee2fd8ae64a3aa291811b1e4b
godkor200 <godkor200@gmail.com>
2024-01-23 13:17:26 +0900

    Refactor: add story not exists error

--- apps/server/apps/api/src/modules/story_board/domain/errors/story.error.ts
+++ apps/server/apps/api/src/modules/story_board/domain/errors/story.error.ts
@@ -1,9 +1,9 @@
 import { ExceptionBase } from '@Libs/commons/src/exceptions/exception.base';
 
 export class StoryNotExistsError extends ExceptionBase {
-  static readonly message = 'story not exists';
+  static readonly message = 'The story does not exist';
 
-  public readonly code = 'story.ALREADY_EXISTS';
+  public readonly code = 'STORY.ALREADY_EXISTS';
 
   constructor(cause?: Error, metadata?: unknown) {
     super(StoryNotExistsError.message, cause, metadata);
