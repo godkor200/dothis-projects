b8e8abf98b939dd8ca51937df2217f85091fca64
godkor200 <godkor200@gmail.com>
2024-01-23 22:20:38 +0900

    feat :add create-story-board

--- apps/server/apps/api/src/modules/story_board/application/service/create-recent-story-board.command.ts
+++ apps/server/apps/api/src/modules/story_board/application/service/create-recent-story-board.command.ts
@@ -5,6 +5,8 @@ import { Inject, InternalServerErrorException } from '@nestjs/common';
 import { RECENT_STORY_BOARD_DI_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/recent-story-board.di-token.constant';
 import { RecentStoryBoardEntity } from '@Apps/modules/story_board/domain/entities/recent-story-board.entity';
 import { Ok, Result } from 'oxide.ts';
+import { STORY_BOARD_DETAIL_DO_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/story-board-details.di-token.constant';
+import { StoryBoardDetailOutboundPort } from '@Apps/modules/story_board/domain/ports/outbound/story-board-details.outbound';
 
 @CommandHandler(RecentStoryBoardCreateDto)
 export class CreateRecentStoryBoardCommand
@@ -17,10 +19,16 @@ export class CreateRecentStoryBoardCommand
   constructor(
     @Inject(RECENT_STORY_BOARD_DI_TOKEN_CONSTANT)
     private readonly recentStoryBoard: RecentStoryBoardOutboundPort,
+
+    @Inject(STORY_BOARD_DETAIL_DO_TOKEN_CONSTANT)
+    private readonly storyBoardDetail: StoryBoardDetailOutboundPort,
   ) {}
   async execute(
     command: RecentStoryBoardCreateDto,
   ): Promise<Result<RecentStoryBoardEntity, InternalServerErrorException>> {
-    return Ok(await this.recentStoryBoard.create(command.id));
+    const storyBoardResult = await this.recentStoryBoard.create(command.id);
+    const storyBoardId = storyBoardResult.id;
+    await this.storyBoardDetail.create(storyBoardId);
+    return Ok(storyBoardResult);
   }
 }
