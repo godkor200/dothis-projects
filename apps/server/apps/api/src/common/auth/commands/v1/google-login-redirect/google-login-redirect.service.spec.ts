6ebe8677287fddc6b19d4a8e6a5d51f894a4dea9
godkor200 <godkor200@gmail.com>
2024-01-23 13:25:03 +0900

    Refactor: Change ID type to number

--- apps/server/apps/api/src/common/auth/commands/v1/google-login-redirect/google-login-redirect.service.spec.ts
+++ apps/server/apps/api/src/common/auth/commands/v1/google-login-redirect/google-login-redirect.service.spec.ts
@@ -32,7 +32,7 @@ describe('GoogleLoginRedirectCommandHandler 예외 처리', () => {
    */
   it('checkUser가 없을 경우 server err를 띄웁니다.', async () => {
     const arg: UserInfoCommandDto = {
-      id: '1234567890',
+      id: 1234567890,
       channelId: 'UCX6OQ3DkcsbYNE6H8uQQuVA',
       userEmail: 'godkor200@gmail.com',
       tokenRefresh: 'aaaaa',
