d07d09d35dd822e4eacfb11865f84ab306113324
godkor200 <godkor200@gmail.com>
2024-01-26 18:11:34 +0900

    refacter : renamed

--- apps/server/apps/api/src/modules/story_board/application/service/create-story-board.command.ts
+++ apps/server/apps/api/src/modules/story_board/application/service/create-story-board.command.ts
@@ -1,19 +1,19 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { RecentStoryBoardCreateDto } from '@Apps/modules/story_board/interfaces/dtos/recent-story-board.dto';
-import { RecentStoryBoardOutboundPort } from '@Apps/modules/story_board/domain/ports/outbound/recent-story-board.outbound';
+import { RecentStoryBoardCreateDto } from '@Apps/modules/story_board/interfaces/dtos/story-board.dto';
+import { RecentStoryBoardOutboundPort } from '@Apps/modules/story_board/domain/ports/outbound/story-board.outbound';
 import { Inject, InternalServerErrorException } from '@nestjs/common';
 import { RECENT_STORY_BOARD_DI_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/recent-story-board.di-token.constant';
-import { RecentStoryBoardEntity } from '@Apps/modules/story_board/domain/entities/recent-story-board.entity';
+import { StoryBoardEntity } from '@Apps/modules/story_board/domain/entities/story-board.entity';
 import { Ok, Result } from 'oxide.ts';
 import { STORY_BOARD_DETAIL_DO_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/story-board-details.di-token.constant';
 import { StoryBoardDetailOutboundPort } from '@Apps/modules/story_board/domain/ports/outbound/story-board-details.outbound';
 
 @CommandHandler(RecentStoryBoardCreateDto)
-export class CreateRecentStoryBoardCommand
+export class CreateStoryBoardCommand
   implements
     ICommandHandler<
       RecentStoryBoardCreateDto,
-      Result<RecentStoryBoardEntity, InternalServerErrorException>
+      Result<StoryBoardEntity, InternalServerErrorException>
     >
 {
   constructor(
@@ -25,7 +25,7 @@ export class CreateRecentStoryBoardCommand
   ) {}
   async execute(
     command: RecentStoryBoardCreateDto,
-  ): Promise<Result<RecentStoryBoardEntity, InternalServerErrorException>> {
+  ): Promise<Result<StoryBoardEntity, InternalServerErrorException>> {
     const storyBoardResult = await this.recentStoryBoard.create(command.id);
     const storyBoardId = storyBoardResult.id;
     await this.storyBoardDetail.create(storyBoardId);
