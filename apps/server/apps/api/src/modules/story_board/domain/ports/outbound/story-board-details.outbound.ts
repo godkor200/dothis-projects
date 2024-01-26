d55fe78fa02ea61a9cb514503b4f5fb8500062c1
godkor200 <godkor200@gmail.com>
2024-01-26 19:11:54 +0900

    refacter : renamed

--- apps/server/apps/api/src/modules/story_board/domain/ports/outbound/story-board-details.outbound.ts
+++ apps/server/apps/api/src/modules/story_board/domain/ports/outbound/story-board-details.outbound.ts
@@ -1,8 +1,8 @@
 import { RepositoryPort } from '@Libs/commons/src';
-import { RecentStoryBoardEntity } from '@Apps/modules/story_board/domain/entities/recent-story-board.entity';
-import { StoryBoardDetailEntity } from '@Apps/modules/story_board/domain/entities/story-board-detail.entity';
+import { StoryBoardEntity } from '@Apps/modules/story_board/domain/entities/story-board.entity';
+import { StoryBoardOverviewEntity } from '@Apps/modules/story_board/domain/entities/story-board-overview.entity';
 
 export interface StoryBoardDetailOutboundPort
-  extends RepositoryPort<StoryBoardDetailEntity> {
-  create(boardId: number): Promise<StoryBoardDetailEntity>;
+  extends RepositoryPort<StoryBoardOverviewEntity> {
+  create(boardId: number): Promise<StoryBoardOverviewEntity>;
 }
