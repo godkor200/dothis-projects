ef77e270be30ba9e6bfb0fbe5b5fd9799f595370
godkor200 <godkor200@gmail.com>
2024-01-26 18:57:31 +0900

    refacter : renamed

--- apps/server/apps/api/src/modules/story_board/interfaces/dtos/index.ts
+++ apps/server/apps/api/src/modules/story_board/interfaces/dtos/index.ts
@@ -1,4 +1,4 @@
 export * from './story-board-memo.dto';
 export * from './story-board-reference.dto';
-export * from './recent-story-board.dto';
-export * from './story-board-detail.dto';
+export * from './story-board.dto';
+export * from './story-board-overview.dto';
