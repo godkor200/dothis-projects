c556c304d76bd8082893aa5f47904f825e818ff3
godkor200 <godkor200@gmail.com>
2024-01-26 17:50:03 +0900

    refacter : moved to folder

--- apps/server/libs/commons/src/interfaces/types/dto.types.ts
+++ apps/server/libs/commons/src/interfaces/types/dto.types.ts
@@ -1,7 +1,14 @@
 import { extendApi } from '@anatine/zod-openapi';
 import { createZodDto } from '@anatine/zod-nestjs';
 import { zUserModel } from '@dothis/dto';
-
+import { zPaginatedSqlQueryParams } from '@dothis/dto';
+export class PaginatedSqlQueryParams extends createZodDto(
+  extendApi(zPaginatedSqlQueryParams),
+) {
+  constructor(props: PaginatedSqlQueryParams) {
+    super();
+  }
+}
 export class UserDto extends createZodDto(extendApi(zUserModel)) {}
 
 export type OrderEnum = 'asc' | 'desc';
