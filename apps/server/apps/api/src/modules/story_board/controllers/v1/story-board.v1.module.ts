99bd21883b9bdf9a4d9f0db7c44f9f7001547dba
godkor200 <godkor200@gmail.com>
2024-01-26 19:10:59 +0900

    refacter : implemented a get-one storyboard

--- apps/server/apps/api/src/modules/story_board/controllers/v1/story-board.v1.module.ts
+++ apps/server/apps/api/src/modules/story_board/controllers/v1/story-board.v1.module.ts
@@ -2,44 +2,47 @@ import { Module, Provider } from '@nestjs/common';
 import { PostStoryBoardHttpV1Controller } from '@Apps/modules/story_board/controllers/v1/commands/post-story-board/post-story-board.http.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { RECENT_STORY_BOARD_DI_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/recent-story-board.di-token.constant';
-import { RecentStoryBoardAdapter } from '@Apps/modules/story_board/infrastructure/adapters/recent-story-board.adapter';
-import { CreateRecentStoryBoardCommand } from '@Apps/modules/story_board/application/service/create-recent-story-board.command';
+import { StoryBoardAdapter } from '@Apps/modules/story_board/infrastructure/adapters/story-board.adapter';
+import { CreateStoryBoardCommand } from '@Apps/modules/story_board/application/service/create-story-board.command';
 import { StoryBoardEntityModule } from '@Apps/modules/story_board/domain/entities/story-board.entity.module';
 import { PostStoryBoardTitleCommand } from '@Apps/modules/story_board/application/service/post-story-board-title.command';
 import { PostStoryBoardDraftCommand } from '@Apps/modules/story_board/application/service/post-story-board-draft.command';
-import { PostStoryBoardDetailCommand } from '@Apps/modules/story_board/application/service/post-story-board-detail.command';
+import { PostStoryBoardOverviewCommand } from '@Apps/modules/story_board/application/service/post-story-board-overview.command';
 import { STORY_BOARD_DETAIL_DO_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/story-board-details.di-token.constant';
-import { StoryBoardDetailAdapter } from '@Apps/modules/story_board/infrastructure/adapters/story-board-detail.adapter';
-import { GetStoryBoardHttpV1Controller } from '@Apps/modules/story_board/controllers/v1/queries/get-story-board/get-story-board.http.controller';
-import { GetStoryBoardQuery } from '@Apps/modules/story_board/application/service/get-story-board.query';
+import { StoryBoardOverviewAdapter } from '@Apps/modules/story_board/infrastructure/adapters/story-board-overview.adapter';
+import { GetOneStoryBoardHttpV1Controller } from '@Apps/modules/story_board/controllers/v1/queries/get-one-story-board/get-one-story-board.http.controller';
+import { GetOneStoryBoardQuery } from '@Apps/modules/story_board/application/service/get-one-story-board.query';
 import { PostReferenceCommand } from '@Apps/modules/story_board/application/service/post-reference.command';
 import { REFERENCE_DI_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/reference.di-token.constant';
 import { ReferenceAdapter } from '@Apps/modules/story_board/infrastructure/adapters/reference.adapter';
 import { MemoAdapter } from '@Apps/modules/story_board/infrastructure/adapters/memo.adapter';
 import { MEMO_DI_TOKEN_CONSTANT } from '@Apps/modules/story_board/constants/memo.di-token.constant';
 import { PostMemoCommand } from '@Apps/modules/story_board/application/service/post-memo.command';
+import { GetManyStoryBoardHttpController } from '@Apps/modules/story_board/controllers/v1/queries/get-many-story-board/get-many-story-board.http.controller';
+import { GetManyStoryBoardQuery } from '@Apps/modules/story_board/application/service/get-many-story-board.query';
 const controllers = [
   PostStoryBoardHttpV1Controller,
-  GetStoryBoardHttpV1Controller,
+  GetOneStoryBoardHttpV1Controller,
+  GetManyStoryBoardHttpController,
 ];
-const queries: Provider[] = [GetStoryBoardQuery];
+const queries: Provider[] = [GetOneStoryBoardQuery, GetManyStoryBoardQuery];
 
 const commands: Provider[] = [
-  CreateRecentStoryBoardCommand,
+  CreateStoryBoardCommand,
   PostStoryBoardTitleCommand,
   PostStoryBoardDraftCommand,
-  PostStoryBoardDetailCommand,
+  PostStoryBoardOverviewCommand,
   PostReferenceCommand,
   PostMemoCommand,
 ];
 const providers: Provider[] = [
   {
     provide: RECENT_STORY_BOARD_DI_TOKEN_CONSTANT,
-    useClass: RecentStoryBoardAdapter,
+    useClass: StoryBoardAdapter,
   },
   {
     provide: STORY_BOARD_DETAIL_DO_TOKEN_CONSTANT,
-    useClass: StoryBoardDetailAdapter,
+    useClass: StoryBoardOverviewAdapter,
   },
   {
     provide: REFERENCE_DI_TOKEN_CONSTANT,
