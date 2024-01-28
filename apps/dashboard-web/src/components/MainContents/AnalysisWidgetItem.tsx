65a1303d816c466793f5a0fc822348cc5e202725
mini <jma1020@naver.com>
2024-01-29 02:25:17 +0900

    design: AnalysisWidgetItem style 수정

--- apps/dashboard-web/src/components/MainContents/AnalysisWidgetItem.tsx
+++ apps/dashboard-web/src/components/MainContents/AnalysisWidgetItem.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import TooltipComponent from '../common/Tooltip/Tooltip';
 
 export interface AnalysisWidgetItemProps {
@@ -14,11 +16,14 @@ const AnalysisWidgetItem = ({
   tooltipText,
 }: AnalysisWidgetItemProps) => {
   return (
-    <li className="rounded-8 bg-primary50 w-[18.1rem] grow py-5" key={title}>
+    <li
+      className="rounded-8 bg-primary50 min-h-[100px] min-w-[18.1rem] grow  py-5"
+      key={title}
+    >
       <div className="border-primary500 border-l-2 pl-[25px] font-bold">
         <div className="text-primary500 text-[20px]">{content}</div>
         <div className="text-grey600 mb-1 flex gap-[4px]">
-          <p> {title}</p>
+          <p className="whitespace-pre-line">{title}</p>
           {hasTooltip && (
             <div className="flex items-center ">
               <TooltipComponent title={tooltipText} />
