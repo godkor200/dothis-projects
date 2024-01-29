b42283fff1733827f1c7b67989dc8826288af253
mini <jma1020@naver.com>
2024-01-29 16:17:13 +0900

    feat: Youtube탭도 미리 데이터 fetch가 가능하도록 수정 및 주석 추가

--- apps/dashboard-web/src/components/MainContents/MediaArticles/MediaArticlesContainer.tsx
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/MediaArticlesContainer.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react';
 
 import type { MedialTabNavDataCategoryType } from '@/constants/TabNav';
 import useGetNewsInfiniteQuery from '@/hooks/react-query/query/useGetNewsInfiniteQuery';
+import useGetVideoDataInfinityQuery from '@/hooks/react-query/query/useGetVideoDataInfinityQuery';
 import { useGptOptionAction } from '@/store/gptOptionStore';
+import { useSelectedWord } from '@/store/selectedWordStore';
 
 import News from './News';
 import YouTube from './Youtube';
@@ -17,38 +19,44 @@ interface MediaArticlesContainerProps {
 const MediaArticlesContainer = ({
   selectedArticle,
 }: MediaArticlesContainerProps) => {
-  const [pageIndex, setPageIndex] = useState(4);
+  const seletedWord = useSelectedWord();
 
-  const [contentIndex, setContentIndex] = useState(0);
+  /**
+   * 기존에는 Tab을 클릭하면 해당 컴포넌트가 mount되어서 data도 mount에 동시에 fetch되어서 가져오는 형식이였지만, openAI 섹션이 추가되면서 Tab클릭이 아닌 Background에서도 해당 데이터가 필요한 경우가 발생.
+   * 그로인해서 부모컴포넌트인 여기서 data fetch와 동시에 gptOption setter 함수 실행
+   */
+  const { setRelatedNews, setRelatedVideo } = useGptOptionAction();
 
-  const handleSetContentIndex = (index: number) => {
-    setContentIndex(index);
-  };
+  const { data: newsData, isLoading: newsIsLoading } =
+    useGetNewsInfiniteQuery();
 
-  const { setRelatedNews } = useGptOptionAction();
-
-  const {
-    data: scrollData,
-    fetchNextPage,
-    hasNextPage,
-    isLoading,
-    isFetching,
-  } = useGetNewsInfiniteQuery();
-
+  /**
+   * setter를 초기화와 동시에 상위 3가지 컬럼을 가져와서 저장한다.
+   */
   useEffect(() => {
-    if (!isLoading) {
-      const relatedNewsData = scrollData?.pages[0].return_object.documents
+    if (!newsIsLoading) {
+      const relatedNewsData = newsData?.pages[0].return_object.documents
         .filter((item, index) => index < 3)
         .map((item) => item.title);
 
       setRelatedNews((prev) => [...relatedNewsData!]);
-      // scrollData?.pages[0].return_object.documents.map((item, index) => {
-      //   if (index < 3) {
-      //     setRelatedNews((prev) => [...prev, item.title]);
-      //   }
-      // });
     }
-  }, [isLoading, JSON.stringify(scrollData)]);
+  }, [newsIsLoading]);
+
+  const { data: videoData, isLoading: videoIsLoading } =
+    useGetVideoDataInfinityQuery(seletedWord);
+
+  /**
+   * setter를 초기화와 동시에 상위 3가지 컬럼을 가져와서 저장한다.
+   */
+  useEffect(() => {
+    if (!videoIsLoading) {
+      const relatedVideoData = videoData
+        ?.filter((item, index) => index < 3)
+        .map((item) => item._source.video_title);
+      setRelatedVideo((prev) => [...relatedVideoData!]);
+    }
+  }, [videoIsLoading]);
 
   return (
     <>
