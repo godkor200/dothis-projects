78cd3dfb7b7ee63d3024550cd17c7b62fe619353
mini <jma1020@naver.com>
2024-01-29 22:16:44 +0900

    chore: build 에러로 인한 주석 처리

--- apps/dashboard-web/src/components/OpenAI/CompletionAI.tsx
+++ apps/dashboard-web/src/components/OpenAI/CompletionAI.tsx
@@ -1,32 +1,32 @@
-'use client';
+// 'use client';
 
-import { useCompletion } from 'ai/react';
-import { useCallback, useState } from 'react';
+// import { useCompletion } from 'ai/react';
+// import { useCallback, useState } from 'react';
 
-export default function Completion() {
-  // Locally store our blog posts content
-  const [content, setContent] = useState('');
-  const { complete } = useCompletion({
-    api: '/api/completion',
-  });
+// export default function Completion() {
+//   // Locally store our blog posts content
+//   const [content, setContent] = useState('');
+//   const { complete } = useCompletion({
+//     api: '/api/completion',
+//   });
 
-  const checkAndPublish = useCallback(
-    async (c: string) => {
-      const completion = await complete(c);
-      if (!completion) throw new Error('Failed to check typos');
-      const typos = JSON.parse(completion);
-      // you should add more validation here to make sure the response is valid
-      if (typos?.length && !window.confirm('Typos found… continue?')) return;
-      else alert('Post published');
-    },
-    [complete],
-  );
+//   const checkAndPublish = useCallback(
+//     async (c: string) => {
+//       const completion = await complete(c);
+//       if (!completion) throw new Error('Failed to check typos');
+//       const typos = JSON.parse(completion);
+//       // you should add more validation here to make sure the response is valid
+//       if (typos?.length && !window.confirm('Typos found… continue?')) return;
+//       else alert('Post published');
+//     },
+//     [complete],
+//   );
 
-  return (
-    <div>
-      <h1>Post Editor</h1>
-      <textarea value={content} onChange={(e) => setContent(e.target.value)} />
-      <button onClick={() => checkAndPublish(content)}>Publish</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Post Editor</h1>
+//       <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+//       <button onClick={() => checkAndPublish(content)}>Publish</button>
+//     </div>
+//   );
+// }
