2a4950b3cee29e9b539882424f08c082805b00de
godkor200 <godkor200@gmail.com>
2024-01-31 11:18:21 +0900

    refacter : move to constants folder

--- apps/server/apps/api/src/modules/video/queries/v1/find-individual-video-info/find-individual-video-info.query-handler.ts
+++ apps/server/apps/api/src/modules/video/queries/v1/find-individual-video-info/find-individual-video-info.query-handler.ts
@@ -1,7 +1,7 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { FindIndividualVideoInfoV1Dto } from '@Apps/modules/video/dtos/find-individual-video-info.dto';
 import { Inject } from '@nestjs/common';
-import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
+import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/constants/video.di-token';
 import { VideoQueryHandlerOutboundPort } from '@Apps/modules/video/database/video.query-handler.outbound.port';
 import { VideoDetailsModel } from '@dothis/dto';
 import { Err, Ok, Result } from 'oxide.ts';
