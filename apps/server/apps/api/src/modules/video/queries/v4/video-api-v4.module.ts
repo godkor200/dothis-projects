3bd1de1efa24e1c28e830584c367d4ade9271c11
godkor200 <godkor200@gmail.com>
2024-01-31 11:25:01 +0900

    refacter : change folder name

--- apps/server/apps/api/src/modules/video/queries/v4/video-api-v4.module.ts
+++ apps/server/apps/api/src/modules/video/queries/v4/video-api-v4.module.ts
@@ -3,7 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { FindAccumulateVideosV4HttpController } from '@Apps/modules/video/queries/v4/find-accumulate-videos/find-accumulate-videos.http.controller';
 import { FindAccumulateVideosV4QueryHandler } from '@Apps/modules/video/queries/v4/find-accumulate-videos/find-accumulate-videos.query-handler';
-import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
+import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/constants/video.di-token';
 import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
 
 const controllers = [FindAccumulateVideosV4HttpController];
