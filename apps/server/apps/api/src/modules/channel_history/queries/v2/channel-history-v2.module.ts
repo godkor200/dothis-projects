999ebcdb9b8fe1d7b3cb87eb8eda9c82a67a0118
godkor200 <godkor200@gmail.com>
2024-01-31 11:12:31 +0900

    refacter : change folder name

--- apps/server/apps/api/src/modules/channel_history/queries/v2/channel-history-v2.module.ts
+++ apps/server/apps/api/src/modules/channel_history/queries/v2/channel-history-v2.module.ts
@@ -2,7 +2,7 @@ import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ExpectedViewsV2QueryHandler } from '@Apps/modules/channel_history/queries/v2/exprected-views/expected-views.v2.query-handler';
 import { ExpectedViewsV2HttpController } from '@Apps/modules/channel_history/queries/v2/exprected-views/expected-views.v2.http.controller';
-import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
+import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/constants/video.di-token';
 import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
 import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/repository/database/channel-history.query-handler';
 import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
