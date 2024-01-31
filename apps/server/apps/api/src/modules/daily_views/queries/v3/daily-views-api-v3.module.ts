bd7a636e7727c4187410cac9629c6da08c8343e1
godkor200 <godkor200@gmail.com>
2024-01-31 11:12:41 +0900

    refacter : moved file

--- apps/server/apps/api/src/modules/daily_views/queries/v3/daily-views-api-v3.module.ts
+++ apps/server/apps/api/src/modules/daily_views/queries/v3/daily-views-api-v3.module.ts
@@ -3,7 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { FindDailyViewsOsV3HttpController } from '@Apps/modules/daily_views/queries/v3/find-daily-views/find-daily-views.v3.http.controller';
 import { FindDailyViewsQueryOsV3Handler } from '@Apps/modules/daily_views/queries/v3/find-daily-views/find-daily-views.v3.query-handler';
-import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
+import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/constants/video.di-token';
 import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
 import { VideoAggregateService } from '@Apps/modules/video/service/video.aggregate.service';
 import { VideoDataService } from '@Apps/modules/video/service/video-data.service';
