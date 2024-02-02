ddc91aed1a7f687c11cd78a3bd22711484d6b130
godkor200 <godkor200@gmail.com>
2024-02-02 10:30:18 +0900

    refactor : add daily-view module

--- apps/server/apps/api/src/modules/daily-view/daily-views-api.module.ts
+++ apps/server/apps/api/src/modules/daily-view/daily-views-api.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
-import { DailyViewsApiV3Module } from '@Apps/modules/daily-view/queries/v3/daily-views-api-v3.module';
+import { DailyViewsQueryV3Module } from '@Apps/modules/daily-view/http/controller/queries/v3/daily-views-query-v3.module';
+import { DailyViewQueryV1Module } from '@Apps/modules/daily-view/http/controller/queries/v1/daily-view-query-v1.module';
 
 @Module({
   imports: [
-    DailyViewsApiV3Module,
-    RouterModule.register([{ path: 'v3', module: DailyViewsApiV3Module }]),
+    DailyViewQueryV1Module,
+    DailyViewsQueryV3Module,
+    RouterModule.register([{ path: 'v1', module: DailyViewQueryV1Module }]),
+    RouterModule.register([{ path: 'v3', module: DailyViewsQueryV3Module }]),
   ],
 })
 export class DailyViewsApiModule {}
---
+++ apps/server/apps/api/src/modules/daily-view/http/controller/queries/v1/daily-view-query-v1.module.ts
@@ -0,0 +1,13 @@
+import { CqrsModule } from '@nestjs/cqrs';
+import { Module, Provider } from '@nestjs/common';
+import { FindDailyViewV1QueryHandler } from '@Apps/modules/daily-view/application/commands/find-daily-view.v1.query-handler';
+import { FindDailyViewV1HttpController } from '@Apps/modules/daily-view/http/controller/queries/v1/find-daily-view/find-daily-view.v1.http.controller';
+const commands: Provider[] = [];
+const queries: Provider[] = [FindDailyViewV1QueryHandler];
+const controllers = [FindDailyViewV1HttpController];
+@Module({
+  imports: [CqrsModule],
+  controllers,
+  providers: [...commands, ...queries],
+})
+export class DailyViewQueryV1Module {}
