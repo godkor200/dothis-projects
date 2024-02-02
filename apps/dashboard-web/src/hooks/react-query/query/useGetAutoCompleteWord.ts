168a91c4452067cc2280bf27a3b3d8f88672212c
mini <jma1020@naver.com>
2024-02-02 16:51:03 +0900

    docs: query hook에 대한 주석 추가

--- apps/dashboard-web/src/hooks/react-query/query/useGetAutoCompleteWord.ts
+++ apps/dashboard-web/src/hooks/react-query/query/useGetAutoCompleteWord.ts
@@ -5,6 +5,12 @@ import type { DeepRequired } from 'react-hook-form';
 import { AUTO_COMPLETEWORD_KEY } from '@/constants/querykey';
 import { apiClient } from '@/utils/api/apiClient';
 
+/**
+ * 해당 query는 검색창 인풋에서 자동완성 키워드를 가져올 때 사용하는 hook입니다.
+ * @param word 검색할 단어 파라미터입니다.
+ * @param queryOptions
+ * @returns
+ */
 const useGetAutoCompleteWord = (
   word: string,
   queryOptions?: UseQueryOptions<
--- apps/dashboard-web/src/hooks/react-query/query/useGetVideoInformation.ts
+++ apps/dashboard-web/src/hooks/react-query/query/useGetVideoInformation.ts
@@ -5,6 +5,12 @@ import type { DeepRequired } from 'react-hook-form';
 import { VIDEODATA_KEY } from '@/constants/querykey';
 import { apiClient } from '@/utils/api/apiClient';
 
+/**
+ * videoId를 가지고 해당 video의 자세한 정보를 조회할 수 있는 hook입니다.
+ * @param @videoId 조회할 videoId @clusterNumber video가 존재하는 cluster
+ * @param queryOptions
+ * @returns
+ */
 const useGetVideoInformation = (
   {
     videoId,
