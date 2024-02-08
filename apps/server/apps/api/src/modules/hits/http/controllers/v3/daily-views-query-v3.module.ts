7e3f55f3e133c2e765ad5b1be22a3d7350975551
godkor200 <godkor200@gmail.com>
2024-02-08 17:22:11 +0900

    refactor : moved folder

--- apps/server/apps/api/src/modules/hits/http/controllers/v3/daily-views-query-v3.module.ts
+++ apps/server/apps/api/src/modules/hits/http/controllers/v3/daily-views-query-v3.module.ts
@@ -4,7 +4,7 @@ import { AwsModule } from '@Apps/common/aws/aws.module';
 import { FindDailyViewsOsV3HttpController } from '@Apps/modules/hits/http/controllers/v3/find-daily-views/find-daily-views.v3.http.controller';
 import { FindDailyViewsQueryOsV3Handler } from '@Apps/modules/hits/application/queries/find-daily-views.v3.query-handler';
 import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/constants/video.di-token';
-import { VideoQueryHandler } from '@Apps/modules/video/database/video.query-handler';
+import { VideoQueryHandler } from '@Apps/modules/video/infrastructure/adapters/video.query-handler';
 import { VideoAggregateService } from '@Apps/modules/video/application/service/video.aggregate.service';
 import { VideoDataService } from '@Apps/modules/video/application/service/video-data.service';
 const controllers = [FindDailyViewsOsV3HttpController];
