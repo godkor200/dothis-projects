a655dc4159b4e13922db14582babf3f3cd2b7b2b
godkor200 <godkor200@gmail.com>
2024-02-15 19:31:54 +0900

    refactor : fix decorator

--- apps/server/apps/api/src/modules/story-board/interfaces/http/controllers/v1/queries/get-many-story-board/get-many-story-board.http.controller.ts
+++ apps/server/apps/api/src/modules/story-board/interfaces/http/controllers/v1/queries/get-many-story-board/get-many-story-board.http.controller.ts
@@ -1,56 +1,45 @@
 import {
-  ApiBearerAuth,
-  ApiHeaders,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
-  ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
 import { PaginatedSqlQueryParams } from '@Libs/commons/src/interfaces/types/dto.types';
 import { QueryBus } from '@nestjs/cqrs';
-import {
-  nestControllerContract,
-  NestRequestShapes,
-  TsRest,
-  TsRestRequest,
-} from '@ts-rest/nest';
-import { JwtAccessGuard, User } from '@Libs/commons/src';
+import { nestControllerContract, TsRest, tsRestHandler } from '@ts-rest/nest';
+import { JwtAccessGuard, Paginated, User } from '@Libs/commons/src';
 import { UserInfoCommandDto } from '@Apps/common/auth/interfaces/dtos/user-info.dto';
 import { IRes } from '@Libs/commons/src/interfaces/types/res.types';
 import { StoryBoardEntity } from '@Apps/modules/story-board/domain/entities/story-board.entity';
 import { match } from 'oxide.ts';
-import { StoryNotExistsError } from '@Apps/modules/story-board/domain/errors';
+import {
+  NotFoundErr,
+  StoryNotExistsError,
+} from '@Apps/modules/story-board/domain/events/errors';
 import {
   Controller,
+  Headers,
   InternalServerErrorException,
   NotFoundException,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { TGetManyStoryBoardRes } from '@Apps/modules/story-board/application/queries/get-many-story-board.query';
 import { apiRouter } from '@dothis/dto';
-import { getManyStoryBoardDto } from '@Apps/modules/story-board/application/dtos';
+import { GetManyStoryBoardDto } from '@Apps/modules/story-board/application/dtos';
+import { AuthToken } from '@Apps/common/auth/domain/event/auth.event';
+import { TTsRestRes } from '@Apps/modules/hits/interfaces/http/controllers/v1/find-daily-view/find-daily-view.v1.http.controller';
+import { InternalServerErr } from '@Apps/common/auth/domain/event/auth.error';
 const c = nestControllerContract(apiRouter.storyBoard);
 const { getManyStoryBoard } = c;
-type RequestShapes = NestRequestShapes<typeof c>;
 const { summary, description, responses } = getManyStoryBoard;
 
 @Controller()
 @ApiTags('스토리 보드')
 export class GetManyStoryBoardHttpController {
   constructor(private readonly queryBus: QueryBus) {}
-  @ApiQuery({
-    type: PaginatedSqlQueryParams,
-  })
-  @ApiBearerAuth('Authorization')
-  @ApiHeaders([
-    {
-      name: 'Authorization',
-      description:
-        "우리 사이트 accessToken(ex:'Bearer ~~~~~~') 상단에 Authorize 눌러 토큰을 저장하고 사용하세요",
-    },
-  ])
+
   @ApiOkResponse({
     schema: {
       type: 'object',
@@ -72,27 +61,38 @@ export class GetManyStoryBoardHttpController {
     description,
   })
   @TsRest(getManyStoryBoard)
-  @ApiNotFoundResponse({ description: StoryNotExistsError.message })
-  // @ApiInternalServerErrorResponse({
-  //   description: responses['internalServerError'][500],
-  // })
+  @ApiNotFoundResponse({ type: NotFoundErr })
+  @ApiInternalServerErrorResponse({
+    type: InternalServerErr,
+  })
   @UseGuards(JwtAccessGuard)
   async execute(
-    @TsRestRequest()
-    { query }: RequestShapes['getManyStoryBoard'],
     @User() userInfo: UserInfoCommandDto,
-  ): Promise<IRes<StoryBoardEntity[]>> {
-    const arg = new getManyStoryBoardDto({ ...query, userInfo });
-    const result: TGetManyStoryBoardRes = await this.queryBus.execute(arg);
+    @Query() query: PaginatedSqlQueryParams,
+    @Headers() headers: AuthToken,
+  ) {
+    return tsRestHandler(getManyStoryBoard, async ({ query }) => {
+      const arg = new GetManyStoryBoardDto({ ...query, userInfo });
+      const result: TGetManyStoryBoardRes = await this.queryBus.execute(arg);
 
-    return match(result, {
-      Ok: (result) => ({ success: true, data: result }),
-      Err: (err: Error) => {
-        if (err instanceof StoryNotExistsError) {
-          throw new NotFoundException(err.message);
-        }
-        throw new InternalServerErrorException(err.message);
-      },
+      return match<
+        TGetManyStoryBoardRes,
+        TTsRestRes<IRes<Paginated<StoryBoardEntity>>>
+      >(result, {
+        Ok: (result) => ({
+          status: 200,
+          body: {
+            success: true,
+            data: result,
+          },
+        }),
+        Err: (err: Error) => {
+          if (err instanceof StoryNotExistsError) {
+            throw new NotFoundException(err.message);
+          }
+          throw new InternalServerErrorException(err.message);
+        },
+      });
     });
   }
 }
