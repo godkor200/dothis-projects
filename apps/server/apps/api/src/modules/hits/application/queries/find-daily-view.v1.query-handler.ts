44023be2d1b94ff0fe90298bb0d2cdfa0eff5693
godkor200 <godkor200@gmail.com>
2024-02-15 19:31:42 +0900

    refactor : fix return type

--- apps/server/apps/api/src/modules/hits/application/queries/find-daily-view.v1.query-handler.ts
+++ apps/server/apps/api/src/modules/hits/application/queries/find-daily-view.v1.query-handler.ts
@@ -7,12 +7,12 @@ import { VIDEO_SERVICE_DI_TOKEN } from '@Apps/modules/video/video.di-token';
 import { IRes } from '@Libs/commons/src/interfaces/types/res.types';
 import { Inject } from '@nestjs/common';
 import { Result } from 'oxide.ts';
-import { IIncreaseData } from '@Apps/modules/hits/application/dtos/find-daily-views.dtos';
 import { FindDailyViewsV1Dto } from '@Apps/modules/hits/application/queries/find-daily-view.v1.query';
+import { IIncreaseHitsData } from '@Apps/modules/video/application/service/video.aggregate.service';
 const IgniteClient = require('apache-ignite-client');
 const IllegalStateError = IgniteClient.Errors.IllegalStateError;
 export type TFindDailyView = Result<
-  IRes<IIncreaseData[]>,
+  IRes<IIncreaseHitsData[]>,
   VideoNotFoundError | VideoHistoryNotFoundError | typeof IllegalStateError
 >;
 @QueryHandler(FindDailyViewsV1Dto)
