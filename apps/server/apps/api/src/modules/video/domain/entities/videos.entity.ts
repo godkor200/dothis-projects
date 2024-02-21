4863b43571503f1062efee72293c445d7d222485
godkor200 <godkor200@gmail.com>
2024-02-21 21:25:03 +0900

    refactor : rename

--- apps/server/apps/api/src/modules/video/domain/entities/videos.entity.ts
+++ apps/server/apps/api/src/modules/video/domain/entities/videos.entity.ts
@@ -8,7 +8,7 @@ import {
 } from 'typeorm';
 
 import { ChannelEntity } from '@Apps/modules/channel/repository/entity/channel.entity';
-import { DailyViewsEntity } from '@Apps/modules/hits/domain/repository/entity/daily-views.entity';
+import { DailyViewsEntity } from '@Apps/modules/hits/domain/entities/daily-views.entity';
 @Entity({ name: 'video' })
 export class VideoEntity {
   @PrimaryGeneratedColumn({ name: 'video_id' })
