47ddd653f8b81567fb7fa999c37e3f53b57a4c21
godkor200 <godkor200@gmail.com>
2024-02-27 23:22:12 +0900

    refactor : no changed

--- apps/server/apps/api/src/modules/channel/queries/v1/analyze-channel/analyze-channel.query-handler.ts
+++ apps/server/apps/api/src/modules/channel/queries/v1/analyze-channel/analyze-channel.query-handler.ts
@@ -4,7 +4,7 @@ import { ChannelAnalysisRes } from '@dothis/dto';
 import { ChannelNotFoundError } from '@Apps/modules/channel/domain/event/channel.errors';
 import { Err, Ok, Result } from 'oxide.ts';
 import { Inject } from '@nestjs/common';
-import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
+import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/channel-history.di-token.constants';
 import { ChannelHistoryOutboundPort } from '@Apps/modules/channel_history/repository/database/channel-history.outbound.port';
 import { CHANNEL_OS_DI_TOKEN } from '@Apps/modules/channel/constants/channel-data.di-token.constants';
 import { ChannelQueryHandlerPort } from '@Apps/modules/channel/database/channel.query-handler.port';
