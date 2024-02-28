702f09b103ce4f334c13f6010be7efb96850e324
godkor200 <godkor200@gmail.com>
2024-02-28 11:02:55 +0900

    refactor : delete os adapter

--- apps/server/apps/api/src/modules/channel_history/queries/v2/channel-history-v2.module.ts
+++ apps/server/apps/api/src/modules/channel_history/queries/v2/channel-history-v2.module.ts
@@ -4,8 +4,6 @@ import { ExpectedViewsV2QueryHandler } from '@Apps/modules/channel_history/queri
 import { ExpectedViewsV2HttpController } from '@Apps/modules/channel_history/queries/v2/exprected-views/expected-views.v2.http.controller';
 import { VIDEO_OS_DI_TOKEN } from '@Apps/modules/video/video.di-token';
 import { VideoQueryHandler } from '@Apps/modules/video/infrastructure/adapters/video.query-handler';
-import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/repository/database/channel-history.query-handler';
-import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { VideoDataService } from '@Apps/modules/video/application/service/video-data.service';
 import { ChannelHistoryServiceModule } from '@Apps/modules/channel_history/service/channel-history.service.module';
@@ -17,10 +15,6 @@ const repositories: Provider[] = [
   {
     provide: VIDEO_OS_DI_TOKEN,
     useClass: VideoQueryHandler,
-  },
-  {
-    provide: CHANNEL_HISTORY_OS_DI_TOKEN,
-    useClass: ChannelHistoryQueryHandler,
   },
 ];
 @Module({
