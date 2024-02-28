3ff933222c958def2b871b0950f46177d8db54cf
godkor200 <godkor200@gmail.com>
2024-02-28 10:47:05 +0900

    refactor : no changed

--- apps/server/apps/api/src/modules/channel_history/repository/database/channel-history.database.module.ts
+++ apps/server/apps/api/src/modules/channel_history/repository/database/channel-history.database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AwsModule } from '@Apps/common/aws/aws.module';
 import { ChannelHistoryQueryHandler } from '@Apps/modules/channel_history/repository/database/channel-history.query-handler';
 import { ChannelHistoryDataService } from '@Apps/modules/channel_history/service/channel-history-data.service';
-import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/constants/channel-history.di-token.constants';
+import { CHANNEL_HISTORY_OS_DI_TOKEN } from '@Apps/modules/channel_history/channel-history.di-token.constants';
 
 @Module({
   imports: [AwsModule],
