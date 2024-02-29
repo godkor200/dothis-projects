8b1499ad274598f26ab3951e58ef591af1750a53
godkor200 <godkor200@gmail.com>
2024-02-29 22:18:27 +0900

    refactor : Implementation of related word ranking, modularization as video adapter becomes larger

---
+++ apps/server/apps/api/src/modules/related-word/interfaces/http/v1/ranking-related-words/get-ranking-related-words.http.controller.ts
@@ -0,0 +1,84 @@
+import { QueryBus } from '@nestjs/cqrs';
+import {
+  apiRouter,
+  TRankRes,
+  zPaginatedIgniteQueryParams,
+  zSearch,
+} from '@dothis/dto';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Controller, NotFoundException, Param } from '@nestjs/common';
+import {
+  nestControllerContract,
+  TsRest,
+  tsRestHandler,
+  TsRestHandler,
+} from '@ts-rest/nest';
+import { match } from 'oxide.ts';
+import { RelwordsNotFoundError } from '@Apps/modules/related-word/domain/errors/relwords.errors';
+import { VideoNotFoundError } from '@Apps/modules/video/domain/events/video.error';
+import {
+  IRes,
+  RelWordsRankingRes,
+  TTsRestRes,
+} from '@Libs/commons/src/interfaces/types/res.types';
+import {
+  GetRankingRelatedWordsDto,
+  GetRankingRelatedWordsParams,
+} from '@Apps/modules/related-word/application/dtos/get-ranking-related-words.dto';
+import { TGetRankingRelatedWordsRes } from '@Apps/modules/related-word/application/service/get-ranking-related-words.service';
+
+const c = nestControllerContract(apiRouter.relatedWords);
+const { rankingRelatedWords } = c;
+const { summary, description } = rankingRelatedWords;
+@ApiTags('관련어')
+@Controller()
+export class GetRankingRelatedWordsHttpController {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @TsRestHandler(rankingRelatedWords)
+  @ApiOperation({
+    summary,
+    description,
+  })
+  @ApiNotFoundResponse({
+    description:
+      RelwordsNotFoundError.message + ' or ' + VideoNotFoundError.message,
+  })
+  @ApiOkResponse({
+    type: RelWordsRankingRes,
+  })
+  async execute(@Param() params: GetRankingRelatedWordsParams) {
+    const arg = new GetRankingRelatedWordsDto(params);
+    console.log(arg, params);
+    return tsRestHandler(rankingRelatedWords, async ({ params }) => {
+      const result = await this.queryBus.execute(arg);
+      return match<TGetRankingRelatedWordsRes, TTsRestRes<IRes<TRankRes>>>(
+        result,
+        {
+          Ok: (result) => ({
+            status: 200,
+            body: {
+              success: true,
+              data: result,
+            },
+          }),
+          Err: (err: Error) => {
+            if (
+              err instanceof RelwordsNotFoundError ||
+              err instanceof VideoNotFoundError
+            )
+              throw new NotFoundException(err.message);
+
+            throw err;
+          },
+        },
+      );
+    });
+  }
+}
