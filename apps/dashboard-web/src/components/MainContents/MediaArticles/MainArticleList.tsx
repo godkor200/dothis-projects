4027340df257b7e5ebb54ba2e5cafcbebb1fb350
mini <jma1020@naver.com>
2024-02-29 15:31:06 +0900

    refactor: MainArticleList card layout 및 리팩토링

--- apps/dashboard-web/src/components/MainContents/MediaArticles/MainArticleList.tsx
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/MainArticleList.tsx
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 
 import useGetNewsInfiniteQuery from '@/hooks/react-query/query/useGetNewsInfiniteQuery';
 import { useSelectedWord } from '@/store/selectedWordStore';
+import { cn } from '@/utils/cn';
 import { externaImageLoader, getMainImage } from '@/utils/imagesUtil';
 
 import CurrentArticle from './CurrentArticle';
@@ -20,17 +21,19 @@ const MainArticleList = () => {
     (item) => item.return_object.documents,
   );
 
-  const returnData = dataObj?.map((item) => {
-    return {
-      title: item.title,
-      category: item.category[0],
-      provider: item.provider,
-      date: dayjs(`${item.dateline}`).format('YYYY.MM.DD'),
-      image: externaImageLoader(getMainImage(item.images)),
-      link: item.provider_link_page,
-      hilight: item.hilight,
-    };
-  });
+  const returnData = dataObj
+    ?.map((item) => {
+      return {
+        title: item.title,
+        category: item.category[0],
+        provider: item.provider,
+        date: dayjs(`${item.dateline}`).format('YYYY.MM.DD'),
+        image: externaImageLoader(getMainImage(item.images)),
+        link: item.provider_link_page,
+        hilight: item.hilight,
+      };
+    })
+    .slice(0, 3);
 
   if (isLoading) {
     return (
@@ -52,35 +55,22 @@ const MainArticleList = () => {
       <h2 className="mb-[40px] text-center text-[20px] font-bold">
         "{selectedWord.keyword}" 키워드에 있었던 특별한 이슈
       </h2>
-      <div className=" mb-[40px]  flex justify-between gap-[120px]">
-        <CurrentArticle
-          title={returnData[0]?.title}
-          category={returnData[0]?.category}
-          provider={returnData[0]?.provider}
-          date={returnData[0]?.date}
-          image={returnData[0]?.image}
-          link={returnData[0]?.link}
-        />
-        {returnData[1] && (
+      <div
+        className={cn('mb-[40px]  flex justify-center gap-[120px]', {
+          'justify-between': returnData.length === 3,
+          'justify-evenly': returnData.length === 2,
+        })}
+      >
+        {returnData.map(({ title, category, provider, date, image, link }) => (
           <CurrentArticle
-            title={returnData[1]?.title}
-            category={returnData[1]?.category}
-            provider={returnData[1]?.provider}
-            date={returnData[1]?.date}
-            image={returnData[1]?.image}
-            link={returnData[1]?.link}
+            title={title}
+            category={category}
+            provider={provider}
+            date={date}
+            image={image}
+            link={link}
           />
-        )}
-        {returnData[2] && (
-          <CurrentArticle
-            title={returnData[2]?.title}
-            category={returnData[2]?.category}
-            provider={returnData[2]?.provider}
-            date={returnData[2]?.date}
-            image={returnData[2]?.image}
-            link={returnData[2]?.link}
-          />
-        )}
+        ))}
       </div>
       <p className="mb-[40px] text-center text-[20px] font-bold">
         "{selectedWord.keyword}" 키워드를 주제로 만든 영상들의 성과를 확인하고,
