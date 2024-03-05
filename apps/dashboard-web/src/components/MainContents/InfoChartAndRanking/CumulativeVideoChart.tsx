aa570154330112884256d6000e0dcbe73fd50c8b
mini <jma1020@naver.com>
2024-03-05 15:19:31 +0900

    feat: 총 영상 수  컴포넌트 apex donutchart 추가 (mock 세팅)

--- apps/dashboard-web/src/components/MainContents/InfoChartAndRanking/CumulativeVideoChart.tsx
+++ apps/dashboard-web/src/components/MainContents/InfoChartAndRanking/CumulativeVideoChart.tsx
@@ -2,6 +2,7 @@
 
 import { colors } from '@dothis/theme/dashboard/colors';
 import { ResponsivePie } from '@nivo/pie';
+import ReactApexChart from 'react-apexcharts';
 
 import type { ResponseType, VideoCount } from '@/constants/convertText';
 
@@ -15,13 +16,57 @@ const CumulativeVideoChart = ({ totalCount, videoCountsBySection }: Props) => {
     <div className="rounded-8 border-grey400 bg-grey00 text-grey600 flex h-[368px] flex-col border p-6 font-bold">
       <div>연간 영상 수</div>
       <div className="relative h-full">
-        <div className="absolute left-2/4 top-[82px] flex translate-x-[-50%] flex-col items-center justify-center">
+        <div className="absolute left-2/4 top-[65px] flex translate-x-[-50%] flex-col items-center justify-center">
           <div>영상 수</div>
           <div className="text-[20px]">
             <span className="text-primary500">{totalCount}</span>개
           </div>
         </div>
-        <ResponsivePie
+        <ReactApexChart
+          options={{
+            chart: {
+              type: 'donut',
+            },
+            plotOptions: {
+              pie: {
+                donut: {
+                  // donut chart bar 사이즈
+                  size: '75',
+
+                  /**
+                   * label => 가운데(dont안에) 레이블의 여부
+                   * label -> total =>  label을 합계 데이터로 보여질지 여부
+                   * total 옵션을 사용하는데 style 커스텀이 어려워서 직접 css로 조정
+                   */
+                },
+              },
+            },
+            labels: ['1만이상', '2만이상', '3만이상', '4만이상', '5만이상'],
+            colors: [
+              colors.primary600,
+              colors.primary400,
+              colors.primary300,
+              colors.primary200,
+              colors.primary100,
+            ],
+            // donut chart bar와의 gap 설정 프로퍼티
+            stroke: { width: 3 },
+
+            // only hover시에만 legend가 보여지도록
+            legend: {
+              show: false,
+            },
+
+            // donut bar에 data 퍼센테이지 표시여부
+            dataLabels: {
+              enabled: false,
+            },
+          }}
+          series={[44, 55, 41, 17, 15]}
+          type="donut"
+          width={'100%'}
+        />
+        {/* <ResponsivePie
           data={videoCountsBySection}
           margin={{ top: 26, bottom: 100 }}
           sortByValue={false}
@@ -80,7 +125,7 @@ const CumulativeVideoChart = ({ totalCount, videoCountsBySection }: Props) => {
               text: { fontSize: 12, fontWeight: 500 },
             },
           }}
-        />
+        /> */}
       </div>
     </div>
   );
