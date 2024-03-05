4035a19ebb1cabbcae7f2ec03620b52e63d3a676
mini <jma1020@naver.com>
2024-03-05 15:18:19 +0900

    feat: AnalyticsSummaryList 컴포넌트 생성

---
+++ apps/dashboard-web/src/components/MainContents/InfoChartAndRanking/AnalyticsSummaryList.tsx
@@ -0,0 +1,29 @@
+import AnalyticsSummaryCard from './AnalyticsSummaryCard';
+
+interface Props {
+  totalView: number;
+  viewCountChange: number;
+  searchVolumeChange: number;
+}
+
+const AnalyticsSummaryList = ({
+  totalView,
+  viewCountChange,
+  searchVolumeChange,
+}: Props) => {
+  const summaryData = [
+    { title: '조회수 합계', value: totalView, unit: '' },
+    { title: '조회수 변동', value: viewCountChange, unit: '%' },
+    { title: '검색량 변동', value: searchVolumeChange, unit: '%' },
+  ];
+
+  return (
+    <ul className="mt-10">
+      {summaryData.map(({ title, value, unit }) => (
+        <AnalyticsSummaryCard title={title} view={value} unit={unit} />
+      ))}
+    </ul>
+  );
+};
+
+export default AnalyticsSummaryList;
