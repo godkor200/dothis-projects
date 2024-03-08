da824f3b8b9f18622a97ccb91e04b744b46d9ab3
mini <jma1020@naver.com>
2024-03-08 17:24:00 +0900

    feat: News 컴포넌트 리팩토링

--- apps/dashboard-web/src/components/MainContents/MediaArticles/News.tsx
+++ apps/dashboard-web/src/components/MainContents/MediaArticles/News.tsx
@@ -1,30 +1,23 @@
 import dayjs from 'dayjs';
 import { useCallback, useState } from 'react';
 
-import ParserContent from '@/components/common/ParserContent';
 import useGetNewsInfiniteQuery from '@/hooks/react-query/query/useGetNewsInfiniteQuery';
+import { useGptOptionAction } from '@/store/gptOptionStore';
 import { externaImageLoader, getMainImage } from '@/utils/imagesUtil';
 
-import ArticleList from './ArticleList';
-import CurrentArticle from './CurrentArticle';
-import SummaryCard from './SummaryCard';
+import type { MediaDigestData } from '.';
+import MediaDigestList from './MediaDigestList';
 
 const News = () => {
-  const [contentIndex, setContentIndex] = useState(0);
-
-  const handleSetContentIndex = (index: number) => {
-    setContentIndex(index);
-  };
-
   const {
-    data: scrollData,
+    data: newsData,
     fetchNextPage,
     hasNextPage,
     isLoading,
     isFetching,
   } = useGetNewsInfiniteQuery();
 
-  const onChange = useCallback(
+  const handleFetchNextPage = useCallback(
     (isInview: boolean) => {
       if (isInview && hasNextPage) {
         fetchNextPage();
@@ -33,55 +26,49 @@ const News = () => {
     [fetchNextPage, hasNextPage],
   );
 
-  const dataObj = scrollData?.pages.flatMap(
+  const flattenNewsData = newsData?.pages.flatMap(
     (item) => item.return_object.documents,
   );
 
-  const returnData = dataObj?.map((item) => {
-    return {
-      title: item.title,
-      category: item.category[0],
-      provider: item.provider,
-      date: dayjs(`${item.dateline}`).format('YYYY.MM.DD'),
-      image: externaImageLoader(getMainImage(item.images)),
-      link: item.provider_link_page,
-      hilight: item.hilight,
-    };
-  });
+  const { setRelatedNews, setRelatedVideo } = useGptOptionAction();
 
   /**
-   * @mediaDataList returnData로 포맷팅을 변환한 Object[] -> 페이지네이션에 맞게끔 포맷팅을 변경합니다! (ex)[Array(5),Array(5),Array(5),Array(5),Array(5)]
-   * @jsx 밑에 jsx는 mediaDataList를 이용해서 prop으로 전달하도록 수정하였습니다.
-   * const mediaDataList = useMemo(() => {
-    return returnData?.reduce(
-      (
-        result: (typeof returnData)[],
-        item: (typeof returnData)[0],
-        index: number,
-      ) => {
-        const chunkIndex: number = Math.floor(index / 5);
-        if (!result[chunkIndex]) {
-          result[chunkIndex] = [];
-        }
-
-        result[chunkIndex].push(item);
-        return result;
-      },
-      [],
-    );
-  }, [data]);
+   * @mediaDigestData api로 받아온 youtubeVideoData의 필요한 프로퍼티만 가져와서 포맷팅을 수정하는 코드
+   * News 컴포넌트와 동일한 컴포넌트를(MediaDigest) 사용하기때문에 추가된 섹션
    */
+  const mediaDigestData: MediaDigestData[] | undefined = flattenNewsData?.map(
+    (item) => {
+      return {
+        title: item.title,
+        provider: item.provider,
+        element: item.category[0],
+        uploadDate: dayjs(`${item.dateline}`).format('YYYY.MM.DD'),
+        image: externaImageLoader(getMainImage(item.images)),
+        link: item.provider_link_page,
+        hilight: item.hilight,
+      };
+    },
+  );
+
+  /**
+   * 이하 jsx전까지의 코드들은 선택된 video의 대한 추가적인 분석정보를 가져와서 Block을 구성했을 디자인 때 존재했던 코드입니다.
+   * 현재는 사용 X
+   */
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleSetSelectedMedia = (index: number) => {
+    setSelectedIndex(index);
+  };
 
   if (isLoading) {
     return (
       <div className="mt-10 flex gap-[1.25rem]">
-        {/* <CurrentArticle.skeleton /> */}
-        <ArticleList.skeleton />
+        <MediaDigestList.skeleton />
       </div>
     );
   }
 
-  if (returnData?.length === 0 || !returnData) {
+  if (mediaDigestData?.length === 0 || !mediaDigestData) {
     return (
       <div className="mt-10 flex flex-wrap gap-[1.25rem]">
         <p className="text-t2 flex h-60 w-full items-center justify-center text-center font-bold">
@@ -94,10 +81,10 @@ const News = () => {
     <>
       <div className="mt-10 flex  gap-[1.25rem] ">
         <div className="flex-1 ">
-          <ArticleList
-            articleListData={returnData}
-            handleSetContentIndex={handleSetContentIndex}
-            onChange={onChange}
+          <MediaDigestList
+            mediaDigestData={mediaDigestData}
+            handleSetSelectedMedia={handleSetSelectedMedia}
+            handleFetchNextPage={handleFetchNextPage}
           />
         </div>
       </div>
