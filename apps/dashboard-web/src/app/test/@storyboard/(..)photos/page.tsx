09f135e18b59d5a6d57cd3574ea5939e8e1dda7b
mini <jma1020@naver.com>
2024-03-15 15:03:34 +0900

    fix: 캘린더 컴포넌트 네이밍 수정

--- apps/dashboard-web/src/app/test/@storyboard/(..)photos/page.tsx
+++ apps/dashboard-web/src/app/test/@storyboard/(..)photos/page.tsx
@@ -3,7 +3,7 @@
 import type { Route } from 'next';
 import Link from 'next/link';
 
-import MiniCalendar from '@/components/common/Calendar/Calendar';
+import Calendar from '@/components/common/Calendar/Calendar';
 
 const PhotoPage = () => {
   return (
@@ -12,11 +12,7 @@ const PhotoPage = () => {
         스토리보드 리스트 페이지(퍼렐로)
       </p>
 
-      <MiniCalendar
-        setOpenDrop={() => {}}
-        type="2024-01-05"
-        setType={() => {}}
-      />
+      <Calendar setOpenDrop={() => {}} type="2024-03-10" setType={() => {}} />
 
       <Link href={`/photos/123`}>id로 이동</Link>
     </div>
