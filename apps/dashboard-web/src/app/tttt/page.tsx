fdcfc3c8043e90ff9a38a45a32b73acf2b4e4deb
mini <jma1020@naver.com>
2024-03-15 15:21:56 +0900

    feat: 임시 캘린더 테스트 페이지 생성

---
+++ apps/dashboard-web/src/app/tttt/page.tsx
@@ -0,0 +1,12 @@
+import CalendarContainer from '@/components/common/Calendar/CalendarContainer';
+
+const TTTT = () => {
+  return (
+    <div>
+      테스트
+      <CalendarContainer />
+    </div>
+  );
+};
+
+export default TTTT;
