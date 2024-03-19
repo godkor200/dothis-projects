33c95df7be2a63d4dcd0aff2495713c12ad883c6
jinhwan7 <rkflql851@gmail.com>
2024-03-19 22:05:46 +0900

    feat : delete-rel-words.dto에 swagger용 클래서 작성

--- apps/server/apps/api/src/modules/related-word/application/dtos/delete-rel-words.dto.ts
+++ apps/server/apps/api/src/modules/related-word/application/dtos/delete-rel-words.dto.ts
@@ -1,3 +1,15 @@
+import { createZodDto } from '@anatine/zod-nestjs';
+import { extendApi } from '@anatine/zod-openapi';
+import { relatedWordsApi, zDeleteRelWords } from '@dothis/dto';
+
+export class DeleteRelWordsBody extends createZodDto(
+  extendApi(relatedWordsApi.deleteRelatedWords.body),
+) {}
+
+export class DeleteRelWordsParams extends createZodDto(
+  extendApi(relatedWordsApi.deleteRelatedWords.pathParams),
+) {}
+
 export class DeleteRelWordsCommandDto {
   constructor(
     public readonly id: string,
