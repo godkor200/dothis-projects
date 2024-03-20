d6b73ec9486e2c61b56f06a8eb0b8292d8e3d1ac
jinhwan7 <rkflql851@gmail.com>
2024-03-20 10:10:44 +0900

    feat : delete-rel-words 컨트롤러에 스웨거 관련 내용 추가

--- apps/server/apps/api/src/modules/related-word/interfaces/http/command/v1/delete-rel-words/delete-rel-words.http.controller.ts
+++ apps/server/apps/api/src/modules/related-word/interfaces/http/command/v1/delete-rel-words/delete-rel-words.http.controller.ts
@@ -1,22 +1,57 @@
-import { Controller, NotFoundException, Param, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { IRes, TTsRestRes } from '@Libs/commons/src/interfaces/types/res.types';
 import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
-import { RelatedWordModel, apiRouter, relatedWordsApi } from '@dothis/dto';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { DeleteRelWordsCommandDto } from '@Apps/modules/related-word/application/dtos/delete-rel-words.dto';
+import { RelatedWordModel, relatedWordsApi } from '@dothis/dto';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
+  DeleteRelWordsBody,
+  DeleteRelWordsCommandDto,
+  DeleteRelWordsParams,
+  DeleteRelWordsSuccessBase,
+  InternalServerErr,
+} from '@Apps/modules/related-word/application/dtos/delete-rel-words.dto';
 import { match } from 'oxide.ts';
 import { RelwordsNotFoundError } from '@Apps/modules/related-word/domain/errors/relwords.errors';
 import { TDeleteRelWordsCommandHandlerRes } from './delete-rel-words.command-handler';
 import { KeywordsNotFoundError } from '@Apps/modules/related-word/domain/errors/keywords.errors';
+import { JwtAccessGuard } from '@Libs/commons/src';
 
 @ApiTags('연관어')
 @Controller()
 export class DeleteRelWordsHttpController {
   constructor(private readonly commandBus: CommandBus) {}
-
+  @UseGuards(JwtAccessGuard)
+  @ApiOperation({
+    summary: relatedWordsApi.deleteRelatedWords.summary,
+    description: relatedWordsApi.deleteRelatedWords.description,
+  })
+  @ApiOkResponse({
+    type: DeleteRelWordsSuccessBase,
+  })
+  @ApiNotFoundResponse({
+    description: '없는 id들어왔거나, 없는 연관어 삭제시에',
+  })
+  @ApiInternalServerErrorResponse({
+    type: InternalServerErr,
+  })
   @TsRestHandler(relatedWordsApi.deleteRelatedWords)
-  async execute() {
+  async execute(
+    @Param() param: DeleteRelWordsParams,
+    @Body() body: DeleteRelWordsBody,
+  ) {
     return tsRestHandler(
       relatedWordsApi.deleteRelatedWords,
       async ({ params, body: reqBody }) => {
