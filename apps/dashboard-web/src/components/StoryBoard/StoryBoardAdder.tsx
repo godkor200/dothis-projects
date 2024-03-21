400d1e4f88c027f669ecfbb9db88db3e2fb345cc
chaejin-jen <jen71324@gmail.com>
2024-03-21 23:21:03 +0900

    fix: createStoryBoard mutate

--- apps/dashboard-web/src/components/StoryBoard/StoryBoardAdder.tsx
+++ apps/dashboard-web/src/components/StoryBoard/StoryBoardAdder.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import type { apiRouter } from '@dothis/dto';
-import type { ClientInferResponseBody } from '@ts-rest/core';
 import { useRouter } from 'next/navigation';
 
 import { useCreateStoryBoardMutation } from '@/hooks/react-query/mutation/useStoryboardMutation';
@@ -15,16 +13,14 @@ const StoryBoardAdder = () => {
     <div
       className="border-1 border-grey500 flex w-[274px] cursor-pointer flex-col items-center justify-center overflow-hidden rounded"
       onClick={() => {
-        createStoryBoard.mutate({
-          onSuccess: (
-            res: ClientInferResponseBody<
-              typeof apiRouter.storyBoard.createStoryBoard,
-              200
-            >,
-          ) => {
-            router.push(`/storyboard/${res.data?.id}`);
+        createStoryBoard.mutate(
+          {},
+          {
+            onSuccess: (res) => {
+              router.push(`/storyboard/${res.body.data?.id}`);
+            },
           },
-        });
+        );
       }}
     >
       + 새 스토리보드
