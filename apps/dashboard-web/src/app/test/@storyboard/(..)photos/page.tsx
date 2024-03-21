bf1773ce651b4deb038bb66d1e6fab22bf71a6ce
mini <jma1020@naver.com>
2024-03-21 10:29:40 +0900

    remove: test 페이지 calendar 제거

--- apps/dashboard-web/src/app/test/@storyboard/(..)photos/page.tsx
+++ apps/dashboard-web/src/app/test/@storyboard/(..)photos/page.tsx
@@ -3,16 +3,12 @@
 import type { Route } from 'next';
 import Link from 'next/link';
 
-import Calendar from '@/components/common/Calendar/Calendar';
-
 const PhotoPage = () => {
   return (
     <div className="text-yellow flex flex-col items-center gap-[40px]">
       <p className="text-[30px] font-bold text-black">
         스토리보드 리스트 페이지(퍼렐로)
       </p>
-
-      <Calendar setOpenDrop={() => {}} type="2024-03-10" setType={() => {}} />
 
       <Link href={`/photos/123`}>id로 이동</Link>
     </div>
