e8a63675adbcecbb3ebaa15f3f9783fb6e06e9bd
godkor200 <godkor200@gmail.com>
2024-03-25 16:12:53 +0900

    refactor : change type element

--- apps/server/apps/api/src/modules/channel-history/domain/ports/channel-history.outbound.port.ts
+++ apps/server/apps/api/src/modules/channel-history/domain/ports/channel-history.outbound.port.ts
@@ -1,15 +1,17 @@
 import { TableNotFoundException } from '@Libs/commons/src/exceptions/exceptions';
 import { Result } from 'oxide.ts';
+import { ChannelHistoryLatestDayTupleRes } from '@Apps/modules/channel-history/infrastructure/daos/channel-history.dao';
 
 export type TChannelHistoryByChannelIdRes = Result<
   IChannelHistoryByChannelIdRes[],
   TableNotFoundException
 >;
 
-export interface IChannelHistoryByChannelIdRes {
-  channelId: string;
-  channelSubscribers: number;
-}
+export interface IChannelHistoryByChannelIdRes
+  extends Pick<
+    ChannelHistoryLatestDayTupleRes,
+    'channelId' | 'channelSubscribers' | 'channelAverageViews'
+  > {}
 
 export interface ChannelHistoryByChannelIdOutboundPort {
   execute(ids: string[]): Promise<TChannelHistoryByChannelIdRes>;
