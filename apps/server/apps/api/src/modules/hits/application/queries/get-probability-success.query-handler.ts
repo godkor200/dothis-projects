91a622bd1355fa61632f604a1a483c8f611c2789
godkor200 <godkor200@gmail.com>
2024-03-25 16:14:25 +0900

    refactor : define probability

---
+++ apps/server/apps/api/src/modules/hits/application/queries/get-probability-success.query-handler.ts
@@ -0,0 +1,34 @@
+import { QueryHandler } from '@nestjs/cqrs';
+import { GetProbabilitySuccessDto } from '@Apps/modules/hits/application/dtos/get-probability-success.dto';
+import { Result } from 'oxide.ts';
+import { GetProbabilitySuccessInboundPort } from '@Apps/modules/hits/domain/ports/get-probability-success.inbound.port';
+
+import { Inject } from '@nestjs/common';
+import { VideoNotFoundError } from '@Apps/modules/video/domain/events/video.error';
+import { IRes } from '@Libs/commons/src/interfaces/types/res.types';
+import { PROBABILITY_SUCCESS_SERVICE_DI_TOKEN } from '@Apps/modules/hits/hits.di-token.contants';
+import {
+  CacheDoesNotFoundException,
+  TableNotFoundException,
+} from '@Libs/commons/src/exceptions/exceptions';
+
+export interface GetProbabilityRes {
+  totalVideoCount: number;
+  countAboveAverage: number;
+}
+
+export type TGetProbabilityRes = Result<
+  IRes<GetProbabilityRes>,
+  VideoNotFoundError | TableNotFoundException | CacheDoesNotFoundException
+>;
+
+@QueryHandler(GetProbabilitySuccessDto)
+export class GetProbabilitySuccessQueryHandler {
+  constructor(
+    @Inject(PROBABILITY_SUCCESS_SERVICE_DI_TOKEN)
+    private readonly getProbabilitySuccessService: GetProbabilitySuccessInboundPort,
+  ) {}
+  async execute(dto: GetProbabilitySuccessDto): Promise<TGetProbabilityRes> {
+    return await this.getProbabilitySuccessService.execute(dto);
+  }
+}
