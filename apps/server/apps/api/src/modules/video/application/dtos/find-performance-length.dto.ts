32440da33c8d0403f107fca4f2f333b3837ef2e8
godkor200 <godkor200@gmail.com>
2024-03-28 16:32:46 +0900

    refactor : define common interface

---
+++ apps/server/apps/api/src/modules/video/application/dtos/find-performance-length.dto.ts
@@ -0,0 +1,21 @@
+import { createZodDto } from '@anatine/zod-nestjs';
+import { extendApi } from '@anatine/zod-openapi';
+import { zClusterNumberMulti, zFindAccumulateQuery } from '@dothis/dto';
+import { IParamsInterface } from '@Libs/commons/src/abstract/applications.abstract';
+export class FindPerformanceLengthQuery extends createZodDto(
+  extendApi(zFindAccumulateQuery),
+) {}
+export class FindPerformanceLengthParams extends createZodDto(
+  extendApi(zClusterNumberMulti),
+) {}
+
+export class FindPerformanceLengthDto
+  extends FindPerformanceLengthQuery
+  implements IParamsInterface
+{
+  readonly clusterNumber: string[];
+  constructor(props: FindPerformanceLengthDto) {
+    super();
+    Object.assign(this, props);
+  }
+}
