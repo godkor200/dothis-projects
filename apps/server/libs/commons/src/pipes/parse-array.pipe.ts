732d961e84276c624f95ca4c88ea081c91849f34
godkor200 <godkor200@gmail.com>
2024-03-28 16:41:34 +0900

    refactor : Implementing pipes that are made into arrays, including space removal, objects, and

---
+++ apps/server/libs/commons/src/pipes/parse-array.pipe.ts
@@ -0,0 +1,24 @@
+import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+
+@Injectable()
+export class ParseArrayPipe
+  implements PipeTransform<Record<string, unknown>, Record<string, unknown>>
+{
+  transform(
+    value: Record<string, unknown>,
+    metadata: ArgumentMetadata,
+  ): Record<string, unknown> {
+    // 객체의 각 속성을 순회하며 처리
+    Object.keys(value).forEach((key) => {
+      const val = value[key];
+      if (typeof val === 'string') {
+        // 문자열 속성에서 쉼표로 구분된 값을 배열로 변환
+        // 각 항목의 앞뒤 공백을 제거
+        value[key] = val.includes(',')
+          ? val.split(',').map((item) => item.trim())
+          : [val.trim()];
+      }
+    });
+    return value;
+  }
+}
