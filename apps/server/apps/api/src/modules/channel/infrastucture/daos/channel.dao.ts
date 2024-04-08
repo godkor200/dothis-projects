346b67d0699b9242d37e69fe97c4158a8dd64f04
godkor200 <godkor200@gmail.com>
2024-04-08 15:36:56 +0900

    refactor : define ChannelProfile adapter api res

---
+++ apps/server/apps/api/src/modules/channel/infrastucture/daos/channel.dao.ts
@@ -0,0 +1,19 @@
+import { Result } from 'oxide.ts';
+import { FindInfluentialListDto } from '@Apps/modules/channel/application/dtos/find-influential-list.dto';
+import { TInfluentialChannelRes } from '@dothis/dto/dist/lib/channel/channel.zod';
+import { NotFoundException } from '@nestjs/common';
+import { TableNotFoundException } from '@Libs/commons/src/exceptions/exceptions';
+
+export type TChannelProfileRes = Result<
+  TInfluentialChannelRes[],
+  NotFoundException | TableNotFoundException
+>;
+
+export class ChannelProfileDao extends FindInfluentialListDto {
+  public readonly relatedCluster: string[];
+
+  constructor(props: FindInfluentialListDto) {
+    super(props);
+    this.relatedCluster = props.clusterNumber;
+  }
+}
