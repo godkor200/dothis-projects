a5ec32f9066ea3e97d2309c0f322e35ba9b0043e
godkor200 <godkor200@gmail.com>
2024-04-08 15:38:01 +0900

    refactor : define ChannelProfile adapter api module, service

--- apps/server/apps/api/src/modules/channel/channel.v1.module.ts
+++ apps/server/apps/api/src/modules/channel/channel.v1.module.ts
@@ -1,1 +1,31 @@
+import { Module, Provider } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { FindInfluentialListHttpController } from '@Apps/modules/channel/interfaces/http/controllers/v1/find-influential-list/find-influential-list.http.controller';
+import {
+  FIND_CHANNEL_PROFILE_IGNITE_DI_TOKEN,
+  FIND_INFLUENTIAL_LIST_SERVICE_DI_TOKEN,
+} from '@Apps/modules/channel/channel-data.di-token.constants';
+import { FindInfluentialListService } from '@Apps/modules/channel/application/service/find-influential-list.service';
+import { ChannelProfileAdapter } from '@Apps/modules/channel/infrastucture/adapters/channel-profile.adapter';
+import { FindInfluentialListQueryHandler } from '@Apps/modules/channel/application/queries/find-influential-list.query-handler';
+const controllers = [FindInfluentialListHttpController];
+
+const service: Provider[] = [
+  {
+    provide: FIND_INFLUENTIAL_LIST_SERVICE_DI_TOKEN,
+    useClass: FindInfluentialListService,
+  },
+];
+const adapter: Provider[] = [
+  {
+    provide: FIND_CHANNEL_PROFILE_IGNITE_DI_TOKEN,
+    useClass: ChannelProfileAdapter,
+  },
+];
+const queries: Provider[] = [FindInfluentialListQueryHandler];
+@Module({
+  imports: [CqrsModule],
+  controllers,
+  providers: [...service, ...adapter, ...queries],
+})
 export class ChannelApiV1Module {}
