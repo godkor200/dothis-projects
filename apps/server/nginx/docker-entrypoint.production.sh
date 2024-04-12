781ee7828502411d99785e9a6f2e8914d5a87feb
godkor200 <godkor200@gmail.com>
2024-04-12 15:01:02 +0900

    refactor : split dev, prod docker build environment

--- apps/server/nginx/docker-entrypoint.production.sh
+++ apps/server/nginx/docker-entrypoint.production.sh
@@ -5,4 +5,4 @@ echo '============================'
 
 chmod +x /wait-for-it.sh
 
-/wait-for-it.sh dev-server:8080 --timeout=80 -- nginx -g 'daemon off;'
+/wait-for-it.sh api-server:8080 --timeout=80 -- nginx -g 'daemon off;'
