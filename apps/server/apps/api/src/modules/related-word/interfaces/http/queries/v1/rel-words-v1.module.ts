a144f2c9f195a6b5dab1c45aa074bc7510b3f515
godkor200 <godkor200@gmail.com>
2024-04-15 17:09:04 +0900

    refactor : fixed delete keyword, relatedword

--- apps/server/apps/api/src/modules/related-word/interfaces/http/queries/v1/rel-words-v1.module.ts
+++ apps/server/apps/api/src/modules/related-word/interfaces/http/queries/v1/rel-words-v1.module.ts
@@ -40,6 +40,9 @@ import { SetDicTermHandler } from '@Apps/modules/related-word/application/servic
 import { ChannelEntityModule } from '@Apps/modules/channel/infrastucture/entities/channel.entity.module';
 import { DeleteRelWordsHttpController } from '@Apps/modules/related-word/interfaces/http/command/v1/delete-rel-words/delete-rel-words.http.controller';
 
+import { DeleteRelWordsCommandHandler } from '../../command/v1/delete-rel-words/delete-rel-words.command-handler';
+import { DeleteKeyWordHttpController } from '@Apps/modules/related-word/interfaces/http/command/v1/delete-keyword/delete-keyword.http.controller';
+import { DeleteKeyWordCommandHandler } from '@Apps/modules/related-word/application/command/delete-keyword.command-handler';
 const controllers = [
   FindRelHttpV1Controller,
   UpdateAutoCompleteWordsHttpController,
@@ -48,6 +51,7 @@ const controllers = [
   FindAutoCompleteHttpController,
   FindSearchTermHttpController,
   DeleteRelWordsHttpController,
+  DeleteKeyWordHttpController,
 ];
 const repositories: Provider[] = [
   {
@@ -93,6 +97,8 @@ const handler = [
   UpdateAutoCompleteWordsCommandHandler,
   GetRankingRelatedWordsService,
   FindSearchKeywordQueryHandler,
+  DeleteRelWordsCommandHandler,
+  DeleteKeyWordCommandHandler,
 ];
 @Module({
   imports: [
