a2b2b3dbc3e88ce226fcca35bcd0bea2edd44538
mini <jma1020@naver.com>
2024-04-18 22:22:54 +0900

    feat:apiRouter ->createApiRouter 생성 (추론되는 타입의 범위를 줄이기위해 생성)

--- packages/dto/src/lib/apiRouter.ts
+++ packages/dto/src/lib/apiRouter.ts
@@ -9,6 +9,37 @@ import { channelHistoryApi } from './channel-history';
 import { channelApi } from './channel';
 import { storyBoardApi } from './story-board';
 
+// 동적으로 key에 맞는 값을 반환하는 함수
+
+const apis = {
+  auth: authApi,
+  user: userApi,
+  hits: hitsApi,
+  channelHistory: channelHistoryApi,
+  channel: channelApi,
+  relatedWords: relatedWordsApi,
+  video: videoApi,
+  storyBoard: storyBoardApi,
+};
+
+function getApiByKey(key: string) {
+  return apis[key];
+}
+
+// apiRouter 생성
+export function createApiRouter<K extends keyof typeof apis>(
+  key: K,
+): Record<K, (typeof apis)[K]> {
+  const api = getApiByKey(key);
+  const router = c.router({
+    [key]: api,
+  } as typeof apis);
+
+  // 해당 어센셜 Type 'RecursivelyApplyOptions<{ [x: string]: ...; }, {}>' is not assignable to type 'Record<K, { auth: RecursivelyApplyOptions<{ getGoogleLogin: { method: "GET"; path: "/auth/google-login"; responses: { 404: ZodObject<{ success: ZodLiteral<false>; statusCode: ZodLiteral<404>; message: ZodLiteral<...>; }, "strip", ZodTypeAny, { ...; }, { ...; }>; 400: ZodObject<...>; 401: ZodObject<...>; 500: ZodObje...'.ts(2322)
+
+  return router;
+}
+
 export const apiRouter = c.router({
   auth: authApi,
   user: userApi,
