ed6389ed3b5930f43ada29cb51dbad5a531a0ff3
godkor200 <godkor200@gmail.com>
2024-04-29 10:46:44 +0900

    refactor : defined type

--- apps/server/apps/api/src/modules/channel/infrastucture/daos/channel.dao.ts
+++ apps/server/apps/api/src/modules/channel/infrastucture/daos/channel.dao.ts
@@ -1,15 +1,22 @@
 import { Result } from 'oxide.ts';
 import { FindInfluentialListDto } from '@Apps/modules/channel/application/dtos/find-influential-list.dto';
-import { TInfluentialChannelRes } from '@dothis/dto';
+import {
+  TAnalyzeMyChannelChannelRes,
+  TInfluentialChannelRes,
+} from '@dothis/dto';
 import { NotFoundException } from '@nestjs/common';
 import { TableNotFoundException } from '@Libs/commons/src/exceptions/exceptions';
-import { ChannelIdDto } from '@Apps/modules/channel/application/dtos/analyze-channel.interface';
+import { GetAnalyzeMyChannel } from '@Apps/modules/channel/application/dtos/analyze-channel.interface';
+import { VideoHistoryNotFoundError } from '@Apps/modules/video-history/domain/events/video_history.err';
 
 export type TChannelProfileRes = Result<
   TInfluentialChannelRes[],
   NotFoundException | TableNotFoundException
 >;
-export type TFindExtendChannelHistoryListRes = Result<any, any>;
+export type TFindExtendChannelHistoryListRes = Result<
+  TAnalyzeMyChannelChannelRes[],
+  TableNotFoundException | VideoHistoryNotFoundError
+>;
 export class ChannelProfileDao extends FindInfluentialListDto {
   public readonly relatedCluster: string[];
 
@@ -19,12 +26,12 @@ export class ChannelProfileDao extends FindInfluentialListDto {
     this.order = props.order;
   }
 }
-export class AnalyzeChannelDao extends ChannelIdDto {
+export class AnalyzeChannelDao extends GetAnalyzeMyChannel {
   constructor(props: AnalyzeChannelDao) {
     super(props);
   }
 }
-export class FindChannelInfoDao extends ChannelIdDto {
+export class FindChannelInfoDao extends GetAnalyzeMyChannel {
   constructor(props: FindChannelInfoDao) {
     super(props);
   }
