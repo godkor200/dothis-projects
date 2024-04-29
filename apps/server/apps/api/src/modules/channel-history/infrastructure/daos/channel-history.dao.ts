3a24ada6db2613023817219a88b77cb83860d0d9
godkor200 <godkor200@gmail.com>
2024-04-29 10:56:31 +0900

    refactor : defined type

--- apps/server/apps/api/src/modules/channel-history/infrastructure/daos/channel-history.dao.ts
+++ apps/server/apps/api/src/modules/channel-history/infrastructure/daos/channel-history.dao.ts
@@ -1,16 +1,8 @@
 import { Result } from 'oxide.ts';
 import { ChannelHistoryNotFoundError } from '@Apps/modules/channel-history/domain/events/channel_history.error';
 import { TableNotFoundException } from '@Libs/commons/src/exceptions/exceptions';
-import { FindChannelInfoDto } from '@Apps/modules/channel-history/application/dtos/find-channel-info.dto';
-import { createZodDto } from '@anatine/zod-nestjs';
-import { zSearchKeyword } from '@dothis/dto';
-import { extendApi } from '@anatine/zod-openapi';
-import { CHANNEL_DATA_KEY } from '@Apps/modules/hits/application/dtos/expected-hits.dtos';
 import { FindAccumulateVideosV1Dto } from '@Apps/modules/video/application/dtos/find-accumulate-videos.dtos';
 
-/**
- * res
- */
 export interface ChannelHistoryLatestDayTupleRes {
   channelId: string;
   channelAverageViews: number;
@@ -27,14 +19,7 @@ export interface IGetRelatedVideoRes
   extends Pick<ChannelHistoryLatestDayTupleRes, 'channelId'> {
   videoId: string;
 }
-export type TChannelHistoryLatestDayTupleArrRes = Result<
-  ChannelHistoryLatestDayTupleRes[],
-  ChannelHistoryNotFoundError | TableNotFoundException
->;
-export type TChannelHistoryLatestDayTupleRes = Result<
-  ChannelHistoryLatestDayTupleRes,
-  ChannelHistoryNotFoundError | TableNotFoundException
->;
+
 export type TGetRelatedVideoRes = Result<
   IGetRelatedVideoRes[],
   ChannelHistoryNotFoundError | TableNotFoundException
