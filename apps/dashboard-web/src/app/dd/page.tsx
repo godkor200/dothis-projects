3e7a1f51af11ea53c848786bdc4dce762de8b66a
mini <jma1020@naver.com>
2024-05-06 12:52:36 +0900

    feat: Naver api apexchart 테스트

--- apps/dashboard-web/src/app/dd/page.tsx
+++ apps/dashboard-web/src/app/dd/page.tsx
@@ -6,7 +6,12 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-import useGetNaverSearchRatio from '@/hooks/react-query/query/useGetNaverSearchRatio';
+import DashboardLineChart from '@/components/common/Charts/DashboardLineChart';
+import { useSearchRatioFormatter } from '@/hooks/contents/useChartFormatter';
+import useGetNaverSearchRatio, {
+  NaverAPI_Results,
+} from '@/hooks/react-query/query/useGetNaverSearchRatio';
+import { handleNaverSearchRatio } from '@/utils/contents/chart';
 
 const request_body = {
   startDate: '2017-01-01',
@@ -31,7 +36,17 @@ const YourComponent = () => {
   const [testUnmounted, setTestUnmounted] = useState(false);
 
   const { data } = useGetNaverSearchRatio({ keyword: '키위', relword: '새' });
-  console.log(data);
+  data?.results;
+  console.log(
+    data &&
+      handleNaverSearchRatio(data.results, {
+        startDate: data.startDate,
+        endDate: data.endDate,
+      }),
+  );
+
+  const search = useSearchRatioFormatter({ keyword: '키위', relword: '새' });
+
   useEffect(() => {
     // async function test() {
     //   const data = await axios.post('api/search', {});
@@ -59,7 +74,7 @@ const YourComponent = () => {
   };
 
   return (
-    <div>
+    <div className="m-10">
       {!testUnmounted && <div id="test">sdsds</div>}
       {testUnmounted && (
         <div id="up" className="up-animation">
@@ -67,6 +82,8 @@ const YourComponent = () => {
         </div>
       )}
       <div onClick={handleTestUnmount}>테스트</div>
+
+      <DashboardLineChart series={[{ ...search }]} />
     </div>
   );
 };
