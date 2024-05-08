fe9834c4b6f6e9f709117cd8d54540cce84d418e
정민킴 <45344418+jma1020@users.noreply.github.com>
2024-05-08 11:05:53 +0900

    Feat/naver api (빌드 에러 수정) (#751)
    * remove:  테스트 페이지 제거
    
    * fix: Route Handler 빌드 에러 수정

--- apps/dashboard-web/src/app/api/search/route.ts
+++ apps/dashboard-web/src/app/api/search/route.ts
@@ -1,3 +1,7 @@
+import { NextResponse } from 'next/server';
+
+import type { NaverAPI_Response } from '@/hooks/react-query/query/useGetNaverSearchRatio';
+
 // Set the runtime to edge for best performance
 export const runtime = 'edge';
 
@@ -33,32 +37,32 @@ const request_body = ({
 export async function POST(req: Request) {
   const { keyword, relword, startDate, endDate } = await req.json();
 
-  try {
-    // 병렬로 처리할 fetch 요청을 배열에 담습니다.
-    // const fetchPromises = [
-    //   fetchWithNaverAPI(['1', '2']),
-    //   fetchWithNaverAPI(['3', '4']),
-    //   fetchWithNaverAPI(['5', '6']),
-    //   fetchWithNaverAPI(['7', '8']),
-    //   fetchWithNaverAPI(['9', '10', '11']),
-    //   // 추가적인 fetch 요청을 필요한 만큼 배열에 추가합니다.
-    // ];
+  // try {
+  // 병렬로 처리할 fetch 요청을 배열에 담습니다.
+  // const fetchPromises = [
+  //   fetchWithNaverAPI(['1', '2']),
+  //   fetchWithNaverAPI(['3', '4']),
+  //   fetchWithNaverAPI(['5', '6']),
+  //   fetchWithNaverAPI(['7', '8']),
+  //   fetchWithNaverAPI(['9', '10', '11']),
+  //   // 추가적인 fetch 요청을 필요한 만큼 배열에 추가합니다.
+  // ];
 
-    // Promise.all을 사용하여 모든 fetch 요청이 완료될 때까지 기다립니다. (해당 기능은 gender 나 age에 따른 조건이 추가될 때 삽입예정)
-    const responseDataArray = await naverTrendKeywordAPI({
-      keyword,
-      relword,
-      startDate,
-      endDate,
-    });
+  // Promise.all을 사용하여 모든 fetch 요청이 완료될 때까지 기다립니다. (해당 기능은 gender 나 age에 따른 조건이 추가될 때 삽입예정)
+  const responseDataArray = await naverTrendKeywordAPI({
+    keyword,
+    relword,
+    startDate,
+    endDate,
+  });
 
-    // 모든 fetch 요청의 결과를 리턴합니다.
-    return Response.json(responseDataArray);
-  } catch (error) {
-    console.error(error);
-    // 에러가 발생한 경우에 대한 처리
-    return Response.json({ error: 'An error occurred' });
-  }
+  // 모든 fetch 요청의 결과를 리턴합니다.
+  return NextResponse.json(responseDataArray);
+  // } catch (error) {
+  //   console.error(error);
+  //   // 에러가 발생한 경우에 대한 처리
+  //   return Response.json({ error: 'An error occurred' });
+  // }
 }
 
 // async function fetchWithNaverAPI(ages: string[], gender?: string) {
@@ -85,7 +89,7 @@ async function naverTrendKeywordAPI({
   relword: string;
   startDate: string;
   endDate: string;
-}) {
+}): Promise<NaverAPI_Response> {
   const response = await fetch('https://openapi.naver.com/v1/datalab/search', {
     method: 'POST',
     headers: {
