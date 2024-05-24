73ab428bb104926475c80f7485c6931d73efc609
mini <jma1020@naver.com>
2024-05-24 17:21:49 +0900

    feat:  MediaText 컴포넌트 생성

---
+++ apps/dashboard-web/src/app/(keyword)/keyword/MediaTextCard.tsx
@@ -0,0 +1,44 @@
+import MediaDigestSummary from '@/components/MainContents/MediaArticles/MediaDigestSummary';
+
+const MediaTextCard = () => {
+  return (
+    <>
+      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
+        <p>
+          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
+          아십니까]
+        </p>
+        <MediaDigestSummary
+          element="안녕"
+          provider="경향신몬"
+          uploadDate="2024.09.05"
+        />
+      </div>
+
+      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
+        <p>
+          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
+          아십니까]
+        </p>
+        <MediaDigestSummary
+          element="안녕"
+          provider="경향신몬"
+          uploadDate="2024.09.05"
+        />
+      </div>
+      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
+        <p>
+          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
+          아십니까]
+        </p>
+        <MediaDigestSummary
+          element="안녕"
+          provider="경향신몬"
+          uploadDate="2024.09.05"
+        />
+      </div>
+    </>
+  );
+};
+
+export default MediaTextCard;
