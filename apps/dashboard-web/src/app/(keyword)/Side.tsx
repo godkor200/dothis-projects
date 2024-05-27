4e3d3aa905d5eaedebad3989b8a86825de96202d
mini <jma1020@naver.com>
2024-05-28 00:05:58 +0900

    feat: Sidebar 트랜지션 추가 및 라우팅 연결

--- apps/dashboard-web/src/app/(keyword)/Side.tsx
+++ apps/dashboard-web/src/app/(keyword)/Side.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import type { Route } from 'next';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import type { SVGType } from '@/components/common/SvgComp';
 import SvgComp from '@/components/common/SvgComp';
+import { cn } from '@/utils/cn';
 
 import * as Style from './styled';
 
@@ -15,18 +17,27 @@ interface Props {
 const Side = ({ isSidebarOpen }: Props) => {
   return (
     <div className=" border-r-1 border-r-grey400">
-      {isSidebarOpen ? (
-        <div className=" flex h-screen w-[240px] flex-shrink-0 flex-col gap-[62px] pt-[40px]">
-          <DashboardList title="대시보드" menuList={SIDE_MENUS} />
+      <div
+        className={cn(
+          'flex h-screen w-[240px] flex-shrink-0 flex-col gap-[62px] pt-[40px] transition-all duration-75',
+          {
+            'w-[79px]': !isSidebarOpen,
+          },
+        )}
+      >
+        {isSidebarOpen ? (
+          <>
+            <DashboardList title="대시보드" menuList={SIDE_MENUS} />
 
-          <DashboardList title="서비스" menuList={SIDE_MENUS2} />
-        </div>
-      ) : (
-        <div className="flex h-screen w-[79px] flex-shrink-0 flex-col gap-[62px] pt-[40px]">
-          <IconList menuList={SIDE_MENUS} />
-          <IconList menuList={SIDE_MENUS2} />
-        </div>
-      )}
+            <DashboardList title="서비스" menuList={SIDE_MENUS2} />
+          </>
+        ) : (
+          <>
+            <IconList menuList={SIDE_MENUS} />
+            <IconList menuList={SIDE_MENUS2} />
+          </>
+        )}
+      </div>
     </div>
   );
 };
@@ -47,13 +58,15 @@ const DashboardList = ({
       <p className="mb-[18px] text-[14px] font-bold">{title}</p>
       <div className="flex flex-col gap-[14px]">
         {menuList.map((item) => (
-          <Style.SideItemContainer
-            $isInActive={pathName === item.link}
-            key={item.title}
-          >
-            <SvgComp icon={item.icon} size={24} />
-            <p>{item.title}</p>
-          </Style.SideItemContainer>
+          <Link href={item.link} key={item.title}>
+            <Style.SideItemContainer
+              $isInActive={`/${pathName?.split('/')[1]}` === item.link}
+              key={item.title}
+            >
+              <SvgComp icon={item.icon} size={24} />
+              <p>{item.title}</p>
+            </Style.SideItemContainer>
+          </Link>
         ))}
       </div>
     </ul>
@@ -64,15 +77,17 @@ const IconList = ({ menuList }: { menuList: SideMenus[] }) => {
   const pathName = usePathname();
 
   return (
-    <ul className="px-[12px]">
+    <ul className="px-[12px] pt-[39px]">
       <div className="flex flex-col gap-[14px]">
         {menuList.map((item) => (
-          <Style.IconItemContainer
-            $isInActive={pathName === item.link}
-            key={item.title}
-          >
-            <SvgComp icon={item.icon} size={24} />
-          </Style.IconItemContainer>
+          <Link href={item.link} key={item.title}>
+            <Style.IconItemContainer
+              $isInActive={`/${pathName?.split('/')[1]}` === item.link}
+              key={item.title}
+            >
+              <SvgComp icon={item.icon} size={24} />
+            </Style.IconItemContainer>
+          </Link>
         ))}
       </div>
     </ul>
@@ -87,46 +102,41 @@ interface SideMenus {
 
 const SIDE_MENUS: SideMenus[] = [
   {
-    title: '콘텐츠 키워드 찾기',
+    title: '콘텐츠 소재',
     icon: 'SideMain',
-    link: '/contents',
+    link: '/keyword',
   },
   {
     title: '내 채널 분석',
-    icon: 'SideUser',
-    link: '/preparing',
+    icon: 'SideChannel',
+    link: '/channel',
   },
   {
-    title: '키워드 순위 분석',
-    icon: 'SideTrendUp',
-    link: '/trending',
-  },
-  {
-    title: '커뮤니티',
-    icon: 'SideMessage',
-    link: '/preparing',
+    title: '인기 키워드 분석',
+    icon: 'SideTrend',
+    link: '/trendingsearches',
   },
 ];
 
 const SIDE_MENUS2: SideMenus[] = [
   {
     title: '커뮤니티',
-    icon: 'SideMessage',
-    link: '/preparing',
+    icon: 'SideCommunity',
+    link: '/community',
   },
   {
     title: '멤버십',
-    icon: 'SideUser',
-    link: '/preparing',
+    icon: 'SideMembership',
+    link: '/membership',
   },
   {
     title: '블로그',
-    icon: 'SideTrendUp',
-    link: '/trending',
+    icon: 'SideBlog',
+    link: '/blog/views-count',
   },
   {
     title: '도움말',
-    icon: 'SideTrendUp',
-    link: '/trending',
+    icon: 'SideHelp',
+    link: '/help',
   },
 ];
