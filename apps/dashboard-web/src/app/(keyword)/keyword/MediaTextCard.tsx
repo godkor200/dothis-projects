30f10d2474a03f76858ed0cf0b38e9e630e99b79
mini <jma1020@naver.com>
2024-05-27 11:42:48 +0900

    feat: Media News Card 데이터 연결 및 스켈레톤 삽입

--- apps/dashboard-web/src/app/(keyword)/keyword/MediaTextCard.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/MediaTextCard.tsx
@@ -1,40 +1,73 @@
-import MediaDigestSummary from '@/components/MainContents/MediaArticles/MediaDigestSummary';
+'use client';
 
-const MediaTextCard = () => {
+import dayjs from 'dayjs';
+import type { Route } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import type { MediaDigestData } from '@/components/MainContents/MediaArticles';
+import MediaDigestSummary from '@/components/MainContents/MediaArticles/MediaDigestSummary';
+import useGetNewsInfiniteQuery from '@/hooks/react-query/query/useGetNewsInfiniteQuery';
+import { useSelectedWord } from '@/store/selectedWordStore';
+import { externaImageLoader, getMainImage } from '@/utils/imagesUtil';
+import { handleImageError } from '@/utils/imagesUtil';
+
+const MediaTextCard = ({ keyword }: { keyword: string }) => {
+  const selectedWord = useSelectedWord();
+  const { data: newsData, isLoading } = useGetNewsInfiniteQuery();
+
+  const flattenNewsData = newsData?.pages.flatMap(
+    (item) => item.return_object.documents,
+  );
+
+  const mediaDigestData: MediaDigestData[] | undefined = flattenNewsData
+    ?.map((item) => {
+      return {
+        title: item.title,
+        provider: item.provider,
+        element: item.category[0],
+        uploadDate: dayjs(`${item.dateline}`).format('YYYY.MM.DD'),
+        image: externaImageLoader(getMainImage(item.images)),
+        link: item.provider_link_page,
+        hilight: item.hilight,
+      };
+    })
+    .slice(0, 3);
+
+  if (!mediaDigestData) {
+    return Array.from({ length: 3 }).map((item, i) => (
+      <div
+        key={i}
+        className="rounded-10 bg-grey200 h-[100px] w-[480px] px-[30px] py-[17px]"
+      ></div>
+    ));
+  }
+
   return (
     <>
-      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
-        <p>
-          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
-          아십니까]
-        </p>
+      <div className="rounded-10 bg-grey200 flex w-[480px] flex-col  justify-between px-[30px] py-[17px]">
+        <p>{mediaDigestData[0].title}</p>
         <MediaDigestSummary
-          element="안녕"
-          provider="경향신몬"
-          uploadDate="2024.09.05"
+          element={mediaDigestData[0].element}
+          provider={mediaDigestData[0].provider}
+          uploadDate={mediaDigestData[0].uploadDate}
         />
       </div>
 
-      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
-        <p>
-          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
-          아십니까]
-        </p>
+      <div className="rounded-10 bg-grey200 flex w-[480px] flex-col  justify-between px-[30px] py-[17px]">
+        <p>{mediaDigestData[1].title}</p>
         <MediaDigestSummary
-          element="안녕"
-          provider="경향신몬"
-          uploadDate="2024.09.05"
+          element={mediaDigestData[1].element}
+          provider={mediaDigestData[1].provider}
+          uploadDate={mediaDigestData[1].uploadDate}
         />
       </div>
-      <div className="rounded-10 bg-grey200 w-[480px] px-[30px] py-[17px]">
-        <p>
-          인간은 대체로 합리적이지 않다…금융 투자에서는[법률·부동산·금융, 얼마나
-          아십니까]
-        </p>
+      <div className="rounded-10 bg-grey200 flex w-[480px] flex-col  justify-between px-[30px] py-[17px]">
+        <p>{mediaDigestData[2].title}</p>
         <MediaDigestSummary
-          element="안녕"
-          provider="경향신몬"
-          uploadDate="2024.09.05"
+          element={mediaDigestData[2].element}
+          provider={mediaDigestData[2].provider}
+          uploadDate={mediaDigestData[2].uploadDate}
         />
       </div>
     </>
