23a59665ac123061aabf74145cc034f2563cdbc0
mini <jma1020@naver.com>
2024-05-28 00:08:29 +0900

    feat: SIdebar 라우팅 추가에 따른 페이지 생성 (준비중)

---
+++ apps/dashboard-web/src/app/(keyword)/help/page.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <div className="flex h-full flex-col items-center justify-center text-[48px] font-bold">
+      <h2 className="text-grey500 border-grey500 mb-[1rem] flex h-20  w-20 items-center justify-center rounded-full border-4 text-[48px] font-bold">
+        !
+      </h2>
+      준비 중인 서비스입니다.
+    </div>
+  );
+};
+
+export default Page;
