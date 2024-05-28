b33f7687df970401feb539d6f693cb475bdd457a
godkor200 <godkor200@gmail.com>
2024-05-28 15:57:21 +0900

    refacter : input arg change to class

--- apps/server/apps/api/src/modules/channel-history/infrastructure/adapters/channel-history.by-channel-id.adapter.ts
+++ apps/server/apps/api/src/modules/channel-history/infrastructure/adapters/channel-history.by-channel-id.adapter.ts
@@ -1,4 +1,3 @@
-import { ChannelHistoryBaseAdapter } from '@Apps/modules/channel-history/infrastructure/adapters/channel-history.base.adapter';
 import {
   ChannelHistoryByChannelIdOutboundPort,
   TChannelHistoryByChannelIdRes,
@@ -12,6 +11,7 @@ import { DateUtil } from '@Libs/commons/src/utils/date.util';
 import { IgniteResultToObjectMapper } from '@Apps/common/ignite/mapper';
 import { IgniteService } from '@Apps/common/ignite/service/ignite.service';
 import { Injectable } from '@nestjs/common';
+import { GetChannelHistoryByChannelIdV2Dao } from '@Apps/modules/video-history/infrastructure/daos/video-history.dao';
 @Injectable()
 export class ChannelHistoryByChannelIdAdapter
   implements ChannelHistoryByChannelIdOutboundPort
@@ -21,20 +21,17 @@ export class ChannelHistoryByChannelIdAdapter
    * 채널 아이디로 CHANNEL_SUBSCRIBERS 불러오는 어뎁터,
    * @param ids 채널 아이디
    */
-  async execute(ids: string[]): Promise<TChannelHistoryByChannelIdRes> {
+  async execute(
+    dao: GetChannelHistoryByChannelIdV2Dao,
+  ): Promise<TChannelHistoryByChannelIdRes> {
     const { year, month, day } = DateUtil.currentDate();
     const cacheName = CacheNameMapper.getChannelHistoryCacheName(year, month);
     const queryString = `
     SELECT DISTINCT ch.channel_id, ch.CHANNEL_SUBSCRIBERS, ch.CHANNEL_AVERAGE_VIEWS,ch.YEAR, ch.MONTH, ch.DAY
     FROM ${cacheName} ch 
-    WHERE ch.channel_id IN ('${ids.join(`', '`)}') 
-    AND (
-    (DAY = ${day})
-    OR 
-    (DAY = ${
-      Number(day) - 1
-    } AND NOT EXISTS (SELECT 1 FROM ${cacheName} WHERE DAY = ${day}))
-    );`;
+    WHERE ch.channel_id IN ('${dao.channelIds.join(`', '`)}') 
+    AND ch.day = (SELECT MIN(DAY) FROM ${cacheName})`;
+
     try {
       const query = this.igniteService.createDistributedJoinQuery(queryString);
       const cache = await this.igniteService.getClient().getCache(cacheName);
