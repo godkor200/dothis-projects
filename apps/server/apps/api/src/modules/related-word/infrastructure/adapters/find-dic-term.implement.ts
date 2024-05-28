9319b69bcd788a126f5c23e764a8e99eb76f3a56
godkor200 <godkor200@gmail.com>
2024-05-28 15:58:20 +0900

    refacter : changed id type

--- apps/server/apps/api/src/modules/related-word/infrastructure/adapters/find-dic-term.implement.ts
+++ apps/server/apps/api/src/modules/related-word/infrastructure/adapters/find-dic-term.implement.ts
@@ -1,14 +1,14 @@
 import { FindDicTermQuery } from '@Apps/modules/related-word/application/dtos/find-dic-term.query';
 import { FindSearchTermOutboundPort } from '@Apps/modules/related-word/domain/ports/find-search-term.outbound.port';
-import { FindSearchTermRes } from '@Apps/modules/related-word/domain/ports/find-search-term.res';
-import { RedisClientService } from '@Apps/common/redis/service/redis.client.service';
 import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import { Redis } from 'ioredis';
+import { FindSearchTermRes } from '@Apps/modules/related-word/domain/ports/find-search-term.res';
 
 @Injectable()
-export class FindDicTermImplement
-  extends RedisClientService
-  implements FindSearchTermOutboundPort
-{
+export class FindDicTermImplement implements FindSearchTermOutboundPort {
+  constructor(@InjectRedis() private readonly redisClient: Redis) {} // Redis client with 'master2' namespace) {}
+
   findAll(options: FindDicTermQuery): Promise<FindSearchTermRes> {
     return this.redisClient.hgetall(options.key);
   }
