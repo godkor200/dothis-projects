f6af4effcad536fb5cf111a622b6caee161928da
godkor200 <godkor200@gmail.com>
2024-05-28 16:06:54 +0900

    refactor : delete unnecessary

--- apps/server/apps/api/src/common/redis/service/redis.client.service.ts
+++ apps/server/apps/api/src/common/redis/service/redis.client.service.ts
@@ -1,23 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { Redis } from 'ioredis';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 
 @Injectable()
-export class RedisClientService {
-  public readonly redisClient: Redis;
+export class RedisClientService implements OnModuleDestroy {
+  private readonly redisClient: Redis;
 
   constructor(private readonly redisService: RedisService) {
     this.redisClient = this.redisService.getClient();
+    console.log('RedisClientService', this.redisClient);
   }
-  async findAllKeys(): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-      this.redisClient.keys('*', (err, keys) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(keys);
-        }
-      });
-    });
+
+  getClient(): Redis {
+    return this.redisClient;
+  }
+
+  async onModuleDestroy() {
+    await this.redisClient.quit();
   }
 }
