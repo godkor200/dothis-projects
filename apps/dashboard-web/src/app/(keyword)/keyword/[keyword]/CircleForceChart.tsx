1c56b0ef7ed85613cdb43aa32cf5d213beba0e41
mini <jma1020@naver.com>
2024-05-30 09:30:07 +0900

    fix: CircleForce summary  조건 수정

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/CircleForceChart.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/CircleForceChart.tsx
@@ -6,12 +6,14 @@ const CircleForceChart = ({ keyword }: { keyword: string }) => {
   const { data } = useGetRankingWordList([keyword]);
 
   return (
-    <p className="text-grey900 truncate text-center text-[18px] font-bold">
+    <>
       {!!data.length && (
-        <span className="text-primary500">{data[0].relword}</span>
-      )}{' '}
-      를 소재로 만들어진 영상의 성과가 가장 좋아요
-    </p>
+        <p className="text-grey900 truncate text-center text-[18px] font-bold">
+          <span className="text-primary500">{data[0].relword}</span>를 소재로
+          만들어진 영상의 성과가 가장 좋아요
+        </p>
+      )}
+    </>
   );
 };
 
