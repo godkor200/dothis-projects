fef8e7c4458647bc687c2a82035434e1567b9c32
godkor200 <godkor200@gmail.com>
2024-05-30 16:30:04 +0900

    refacter : imple ranking v2

--- apps/server/apps/api/src/modules/related-word/related-words.enum.di-token.constant.ts
+++ apps/server/apps/api/src/modules/related-word/related-words.enum.di-token.constant.ts
@@ -19,3 +19,7 @@ export const CACHE_CLIENT = Symbol('CACHE_CLIENT');
 export const CACHE_FIND_ALL_QUERY = Symbol('CACHE_FIND_ALL_QUERY');
 
 export const CACHE_SET_DIC_TERM = Symbol('CACHE_SET_DIC_TERM');
+
+export const RANKING_V2_SERVICE_DI_TOKEN = Symbol(
+  'RANKING_V2_SERVICE_DI_TOKEN',
+);
