3874866dc762b39d9700878ef29e2e79ff7121b0
mini <jma1020@naver.com>
2024-05-31 14:03:19 +0900

    feat: useGetWeeklyKeyword query hook 생성

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetWeeklyKeyword.ts
@@ -0,0 +1,65 @@
+import type { apiRouter } from '@dothis/dto';
+import type { UseInfiniteQueryOptions } from '@ts-rest/react-query';
+
+import type { TrendingQuery } from '@/app/(main)/(searchGNB)/trending/TrendingQueryContext';
+import { WEEKLY_KEYWORD_KEY } from '@/constants/querykey';
+import type { SortingQuery } from '@/types/common';
+import { apiClient } from '@/utils/api/apiClient';
+
+import type { Weekly_Sort_Key } from './useGetTrendingKeywords';
+
+const useGetWeeklyKeyword = (
+  {
+    startDate,
+    keywordList,
+    selectOptions,
+    sort,
+    order,
+  }: Partial<SortingQuery<Weekly_Sort_Key>> & Partial<TrendingQuery>,
+  queryOptions?: UseInfiniteQueryOptions<
+    typeof apiRouter.hits.getWeeklyKeywordSome
+  >,
+) => {
+  const date = startDate?.format('YYYY-MM-DD');
+
+  const queryResult = apiClient(1).hits.getWeeklyKeywordSome.useInfiniteQuery(
+    WEEKLY_KEYWORD_KEY.list([
+      {
+        date,
+        keywordList,
+        selectOptions,
+        sort,
+        order,
+      },
+    ]),
+    ({ pageParam = 1 }) => {
+      return {
+        query: {
+          page: pageParam,
+          limit: String(30),
+          from: '2024-04-29',
+          keywords: keywordList?.join(','),
+          categoryNumbers: selectOptions?.map((item) => item.value).join(','),
+          sort: sort,
+          order: order,
+        },
+      };
+    },
+    {
+      ...queryOptions,
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.body.data.data.length < 30
+          ? undefined
+          : allPages.length + 1;
+      },
+    },
+  );
+
+  return {
+    ...queryResult,
+    data: queryResult.data?.pages.flatMap((item) => item.body.data.data),
+    total: queryResult.data?.pages[0].body.data.total,
+  };
+};
+
+export default useGetWeeklyKeyword;
