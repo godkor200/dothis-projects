34dace361527b43c03a350b899a232c2bdfa8cd7
mini <jma1020@naver.com>
2024-06-05 19:31:49 +0900

    feat: useYAxis 추상화 hook 생성

---
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/summary/useYAxis.ts
@@ -0,0 +1,76 @@
+import * as D3 from 'd3';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+import type { DataItem } from './SummaryChart';
+
+interface YAxisRef {
+  y: D3.ScaleLinear<number, number>;
+  render: () => void;
+}
+
+interface Dimensions {
+  width: number;
+  height: number;
+  marginLeft: number;
+  marginRight: number;
+  marginTop: number;
+  marginBottom: number;
+}
+
+interface Props {
+  chartSelector: D3.Selection<SVGSVGElement | null, unknown, null, undefined>;
+  data: DataItem[];
+  dimensions: Dimensions;
+  styleMethod?: (selection: D3.Selection<any, any, any, any>) => void;
+}
+const useYAxis = ({ chartSelector, data, dimensions, styleMethod }: Props) => {
+  const { width, height, marginTop, marginRight, marginBottom, marginLeft } =
+    dimensions;
+
+  const yAxisRef = useRef<YAxisRef | null>(null);
+
+  const [min = 0, max = 100] = D3.extent(data, (data) => data.value as number);
+  const y = D3.scaleLinear()
+    .domain([0, max === 0 ? 100 : max])
+    .nice()
+    .range([height - marginBottom, marginTop]);
+
+  useImperativeHandle(
+    yAxisRef,
+    () => ({
+      render: () => {
+        const yAxisCallback = (
+          g: D3.Selection<SVGGElement, unknown, null, undefined>,
+        ) =>
+          g
+            .attr('transform', `translate(${marginLeft}, 0)`)
+            .call(D3.axisLeft(y).tickSize(-width).ticks(3));
+
+        chartSelector
+          .append('g')
+          // .attr('transform', `translate(${marginLeft},0)`)
+          .call(yAxisCallback)
+          .call((g: D3.Selection<SVGGElement, unknown, null, undefined>) =>
+            g.select('.domain').remove(),
+          )
+          .attr('class', 'grid')
+          .attr('class', function (d) {
+            return 'dashed';
+          })
+          .call((yAxis) => {
+            if (styleMethod) {
+              styleMethod(yAxis);
+            }
+          })
+          .selectAll('text')
+          .remove();
+      },
+      y: y,
+    }),
+    [chartSelector, width, data],
+  );
+
+  return yAxisRef;
+};
+
+export default useYAxis;
