bf3b4ce607224bd2204fbef7d5d7b5e7321da1df
mini <jma1020@naver.com>
2024-06-14 17:04:51 +0900

    feat: News Data 단일 query hook 생성

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetSingleNews.ts
@@ -0,0 +1,169 @@
+import type { QueryOptions } from '@tanstack/query-core';
+import type { UseQueryOptions as RQUseQueryOptions } from '@tanstack/react-query';
+
+export type UseQueryOptions<
+  F extends (...args: any) => Promise<unknown>,
+  S = Awaited<ReturnType<F>>,
+> = Omit<RQUseQueryOptions<Awaited<ReturnType<F>>, any, S, any[]>, 'queryKey'>;
+
+import {
+  useQuery,
+  useQueryClient,
+  type UseQueryResult,
+} from '@tanstack/react-query';
+import axios from 'axios';
+
+import { NEWS_DATA_KEY } from '@/constants/querykey';
+import { useEndDate, useStartDate } from '@/store/dateStore';
+import type { NewsResponse, ServerResponse } from '@/types/news';
+
+interface PaginationQuery {
+  limit?: number;
+  page?: number;
+}
+
+interface DateQuery {
+  from?: string;
+  to?: string;
+}
+
+interface KeywordQuery {
+  searchKeyword: string;
+  relatedkeyword?: string;
+}
+
+const retrievePosts = async ({
+  keyword,
+  startDate,
+  endDate,
+  page,
+}: {
+  keyword: string;
+  startDate: string;
+  endDate: string;
+  page?: number;
+}): Promise<NewsResponse> => {
+  const obj = {
+    access_key: 'eb75ee2e-b1f6-4ada-a964-9bf94c5a2f26',
+    argument: {
+      query: { title: keyword || '먹방' },
+
+      published_at: {
+        from: startDate,
+        until: endDate,
+      },
+      // provider: ['경향신문'],
+      category: ['정치', '경제', '사회', '국제', '지역', '스포츠', 'IT_과학'],
+      // category_incident: ['범죄', '교통사고', '재해>자연재해'],
+      // byline: '',
+
+      sort: [
+        { date: 'desc' },
+        {
+          _score: 'desc',
+        },
+      ],
+      hilight: 200,
+      return_from: page || 1,
+      // 페이지네이션을 위해 25개로 수정하였습니다.
+      return_size: 1,
+      fields: [
+        'byline',
+        'category',
+        'category_incident',
+        'provider_news_id',
+        'images',
+        'provider_link_page',
+      ],
+    },
+  };
+
+  const response = await axios.post<NewsResponse>(
+    'https://tools.kinds.or.kr/search/news?access_key=eb75ee2e-b1f6-4ada-a964-9bf94c5a2f26',
+    JSON.stringify(obj),
+  );
+
+  return response.data;
+};
+
+type Test = {
+  meta?: {
+    fetchedAt: string;
+  };
+} & NewsResponse['return_object']['documents'][number];
+
+const useGetSingleNews = <T extends any = ServerResponse<NewsResponse>>(
+  {
+    searchKeyword,
+    relatedkeyword,
+    from,
+    to,
+    limit,
+    page,
+    queryKeyIndex,
+  }: KeywordQuery & PaginationQuery & DateQuery & { queryKeyIndex: number },
+  queryOptions?: UseQueryOptions<
+    typeof retrievePosts,
+    NewsResponse['return_object']['documents'][number]
+  >,
+  test?: boolean,
+) => {
+  const startDate = useStartDate();
+  const endDate = useEndDate();
+
+  const queryClient = useQueryClient();
+
+  const cachedData = queryClient.getQueryData([
+    'news',
+    {
+      searchKeyword,
+      relatedkeyword,
+      from,
+      to,
+      limit,
+      page,
+    },
+  ]);
+
+  return useQuery(
+    [
+      NEWS_DATA_KEY.detail([
+        {
+          searchKeyword,
+          relatedkeyword,
+          from,
+          to,
+          limit,
+          page,
+          queryKeyIndex,
+        },
+      ]),
+    ],
+
+    () => retrievePosts({ keyword: searchKeyword, startDate, endDate, page }),
+    {
+      ...queryOptions,
+      enabled:
+        (queryOptions?.enabled ?? false) &&
+        !!searchKeyword &&
+        !!startDate &&
+        !!endDate,
+
+      staleTime: Infinity,
+
+      //   onSuccess(data) {
+      //     // console.log(cachedData);
+      //     console.log(queryClient);
+      //     console.log(cachedData);
+      //     if (cachedData) {
+      //       queryOptions?.onSuccess?.(data);
+      //     }
+      //   },
+
+      select: (data) => {
+        return data.return_object.documents[0];
+      },
+    },
+  );
+};
+export default useGetSingleNews;
