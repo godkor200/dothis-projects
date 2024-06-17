23ce065d528303550393db1ad63a04be514c9c1d
mini <jma1020@naver.com>
2024-06-17 13:40:41 +0900

    feat: 키워드 클릭 시 라우팅 연결

--- apps/dashboard-web/src/app/(keyword)/WeeklyKeyword.tsx
+++ apps/dashboard-web/src/app/(keyword)/WeeklyKeyword.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import useGetWeeklyTrendKeyword from '@/hooks/react-query/query/useGetWeeklyTrendKeyword';
 import { convertCompetitionScoreFormatToHTML } from '@/utils/contents/competitionScore';
 
@@ -11,32 +13,39 @@ const WeeklyKeyword = () => {
   return (
     <ul className="flex flex-col gap-[15px]">
       {data?.map((item, i) => (
-        <li key={i} className="gap-30 flex items-center p-[10px]">
-          <p className="text-grey500">{i + 1}</p>
-          <p className="flex-grow">{item.recommendedKeyword}</p>
-          <div className="h-[20px] w-[20px]">
-            {Math.sign(item.rankChange) === 0 ? (
-              <div className="text-center">
-                <span className="">-</span>
-              </div>
-            ) : Math.sign(item.rankChange) === 1 ? (
-              <div className="flex items-center  justify-center">
-                <span className="text-[12px] text-[#F00]">
-                  {item.rankChange}
-                </span>
-                <span className="h-0 w-0 border-x-[4px] border-b-[8px]  border-x-transparent border-b-[#F00]"></span>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center">
-                <span className="text-[12px] text-[#3183FF]">
-                  {' '}
-                  {item.rankChange}
-                </span>
-                <span className="h-0 w-0 border-x-[4px] border-t-[8px]  border-x-transparent border-t-[#3183FF]"></span>
-              </div>
-            )}
-          </div>
-        </li>
+        <Link
+          href={`/keyword/${item.recommendedKeyword}/${
+            item.topAssociatedWord.split(',')[0]
+          }`}
+          key={item.recommendedKeyword}
+        >
+          <li key={i} className="gap-30 flex items-center p-[10px]">
+            <p className="text-grey500">{i + 1}</p>
+            <p className="flex-grow">{item.recommendedKeyword}</p>
+            <div className="h-[20px] w-[20px]">
+              {Math.sign(item.rankChange) === 0 ? (
+                <div className="text-center">
+                  <span className="">-</span>
+                </div>
+              ) : Math.sign(item.rankChange) === 1 ? (
+                <div className="flex items-center  justify-center">
+                  <span className="text-[12px] text-[#F00]">
+                    {item.rankChange}
+                  </span>
+                  <span className="h-0 w-0 border-x-[4px] border-b-[8px]  border-x-transparent border-b-[#F00]"></span>
+                </div>
+              ) : (
+                <div className="flex items-center justify-center">
+                  <span className="text-[12px] text-[#3183FF]">
+                    {' '}
+                    {item.rankChange}
+                  </span>
+                  <span className="h-0 w-0 border-x-[4px] border-t-[8px]  border-x-transparent border-t-[#3183FF]"></span>
+                </div>
+              )}
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );
