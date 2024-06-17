bcd6660445c666b186065d397ebb97e5fc82bf50
mini <jma1020@naver.com>
2024-06-17 10:38:54 +0900

    feat: react query fetch순서에 따른 순서 보장 테스트 코드

--- apps/dashboard-web/src/app/zzz/page.tsx
+++ apps/dashboard-web/src/app/zzz/page.tsx
@@ -1,86 +1,11 @@
-'use client';
+import { c } from '@dothis/dto';
 
-import dayjs from 'dayjs';
-
-import {
-  useSearchRatioFormatter,
-  useSearchRatioFormatterD3,
-} from '@/hooks/contents/useChartFormatter';
-// import DashboardAreaChart from '@/components/common/Charts/DashboardAreaChart';
-import { useEndDate, useStartDate } from '@/store/dateStore';
-import { getDateObjTime } from '@/utils/contents/dateObject';
+// import ZZZTEST from './test';
 
 const Page = () => {
-  const startDate = useStartDate();
+  const randomChoice = Math.random() < 0.5 ? 'news' : 'youtube';
 
-  // 1. true의 개수 세기
-
-  const rangeTargetData = [[0.59], [1.4], [2], [3], [0], [0]];
-
-  const data = useSearchRatioFormatterD3({
-    keyword: '한국어',
-    relword: '한국어',
-  });
-
-  console.log(data);
-
-  const rangeData = [
-    [0.55, 0.61],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-  ];
-
-  return (
-    <div>
-      <div className="ml-[2px] flex h-3/6 justify-center [&_svg]:overflow-visible">
-        <div>xptmxm</div>
-        {/* <DashboardAreaChart
-          series={[
-            {
-              type: 'rangeArea',
-              name: '평균성과 기대치',
-              data: rangeData.map((item, index) => ({
-                x: getDateObjTime(
-                  dayjs(startDate).add(index, 'day').format('YYYY-MM-DD'),
-                ),
-                y: item,
-              })),
-            },
-
-            {
-              type: 'line',
-              name: '평균성과',
-              data: rangeTargetData.map((item, index) => ({
-                x: getDateObjTime(
-                  dayjs(startDate).add(index, 'day').format('YYYY-MM-DD'),
-                ),
-                y: item,
-              })),
-            },
-          ]}
-        /> */}
-
-        {/* <DashboardLineChart
-          series={[
-            {
-              name: '일일 조회 수',
-              type: 'line',
-              color: '#F0516D',
-              data: [0.15, 0.45, 0.11, 0.22, 0.43, 0.02].map((item, index) => [
-                getDateObjTime(
-                  dayjs(startDate).add(index, 'day').format('YYYY-MM-DD'),
-                ),
-                item,
-              ]),
-            },
-          ]}
-        /> */}
-      </div>
-    </div>
-  );
+  return <>{/* <ZZZTEST randomChoice={randomChoice} /> */}</>;
 };
 
 export default Page;
