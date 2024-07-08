f98938a059547de33a39487964479798b6700c4d
mini <jma1020@naver.com>
2024-07-08 17:01:04 +0900

    fix: dataObject dto 삽입

--- packages/dto/src/lib/user/user.zod.ts
+++ packages/dto/src/lib/user/user.zod.ts
@@ -1,7 +1,8 @@
 import { zChannelData } from '../channel';
+import { dataObject } from '../common.model';
 import { zUserModel } from './user.model';
 import { z } from 'zod';
-export const zOwnInfo = zUserModel.shape.data.merge(
-  z.object({ channel: zChannelData.optional() }),
+export const zOwnInfo = dataObject(
+  zUserModel.shape.data.merge(z.object({ channel: zChannelData.optional() })),
 );
 export type TOwnInfoRes = z.TypeOf<typeof zOwnInfo>;
