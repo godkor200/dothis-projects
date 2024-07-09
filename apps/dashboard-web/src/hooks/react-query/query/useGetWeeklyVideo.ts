6a17cdb4d0e8a7fbece56869c3cb6b41fce0b283
mini <jma1020@naver.com>
2024-07-09 15:29:52 +0900

    feat : useGetWeeklyVideo 쿼리 훅 생성

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetWeeklyVideo.ts
@@ -0,0 +1,19 @@
+import { WEEKLY_TREND_VIDEO_KEY } from '@/constants/querykey';
+import { apiClient } from '@/utils/api/apiClient';
+
+const useGetWeeklyVideo = () => {
+  const queryResult = apiClient(1).video.getIssueToday.useQuery(
+    WEEKLY_TREND_VIDEO_KEY.all,
+    {},
+    {
+      enabled: true,
+    },
+  );
+
+  return {
+    ...queryResult,
+    data: queryResult.data?.body,
+  };
+};
+
+export default useGetWeeklyVideo;
