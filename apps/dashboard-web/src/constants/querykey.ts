fdc47e034cbf74d858bf28cbae46432b95a3d146
mini <jma1020@naver.com>
2024-07-09 15:28:50 +0900

    feat : Weekly Trend keyword 쿼리키 생성

--- apps/dashboard-web/src/constants/querykey.ts
+++ apps/dashboard-web/src/constants/querykey.ts
@@ -11,6 +11,7 @@ export const VIDEO_COUNT_KEY = createQueryKeys('videocount');
 export const RANK_RELATIONWORD_KEY = createQueryKeys('rank_relationword');
 export const TRENDING_KEYWORD_KEY = createQueryKeys('trending_keyword');
 export const WEEKLY_KEYWORD_KEY = createQueryKeys('weekly_keyword');
+export const WEEKLY_TREND_KEYWORD_KEY = createQueryKeys('weekly_trend_keyword');
 export const USER_CHANNEL_KEY = createQueryKeys('user_channel');
 export const STORYBOARD_KEY = createQueryKeys('storyboard');
 export const INFLUENTIAL_CHANNEL_KEY = createQueryKeys('influential_channel');
