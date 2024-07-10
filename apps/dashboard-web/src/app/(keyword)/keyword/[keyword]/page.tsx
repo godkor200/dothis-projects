909be95956eeea34ab3ad2c0557618ceaba9a80a
mini <jma1020@naver.com>
2024-07-10 15:43:49 +0900

    feat : Tooltip 컴포넌트 추가 (키워드 페이지)

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/page.tsx
@@ -1,5 +1,6 @@
 import D3Axis from '@/components/common/Charts/D3Axis';
 import D3Chart from '@/components/common/Charts/D3Chart';
+import TooltipComponent from '@/components/common/Tooltip/Tooltip';
 
 import BoxFrame from '../BoxFrame';
 import MediaImageCard from '../MediaImageCard';
@@ -64,9 +65,14 @@ const Page = ({ params }: { params: { keyword: string } }) => {
             <div className="grid grid-cols-[repeat(2,minmax(600px,1fr))] gap-[20px]">
               <BoxFrame isPositionProperty={true}>
                 <div className="flex h-full flex-col">
-                  <p className="text-grey600 mb-[20px] text-[14px]">
-                    콘텐츠 추이
-                  </p>
+                  <div className="text-grey600 mb-[30px] flex gap-[10px] text-[14px] font-[500]">
+                    <p>콘텐츠 추이</p>
+                    <TooltipComponent
+                      title={
+                        '검색한 키워드가 포함된 영상들이 획득한 조회수의 합계와 영상이 발행된 횟수를 나타냅니다. \n 같은 기간 동안 변화한 검색량과 비교해 콘텐츠의 수요와 공급을 예측하세요.'
+                      }
+                    />
+                  </div>
 
                   <ChartContainer keyword={keyword} relatedKeyword={null} />
                 </div>
