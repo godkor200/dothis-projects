bb1e50318fc825134462f2702d3d27d81830e62a
godkor200 <godkor200@gmail.com>
2024-07-11 15:27:56 +0900

    refactor : Modify deployment automation script

---
+++ scripts/deploy-external-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo '외부 공개 api 배포 빌드'
+echo 'amd64 아키텍처로 빌드'
+docker build -t external-sales-api -f ~/Desktop/project/dothis/apps/server/apps/external-sales-api/Dockerfile.production ~/Desktop/project/dothis
+echo '로그인'
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 505558602960.dkr.ecr.ap-northeast-2.amazonaws.com
+echo '태깅'
+docker tag dothis-external-sales-api:latest 505558602960.dkr.ecr.ap-northeast-2.amazonaws.com/dothis-external-sales-api:latest
+echo 'ECR push'
+docker push 505558602960.dkr.ecr.ap-northeast-2.amazonaws.com/dothis-external-sales-api:latest
