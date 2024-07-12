7fa1a1ba8ac0db9bf280af5840b47032ce129d5d
mini <jma1020@naver.com>
2024-07-12 15:44:03 +0900

    fix : box layout 버그 수정

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/CompetitionRate.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/CompetitionRate.tsx
@@ -42,7 +42,7 @@ const CompetitionRate = ({ keyword }: { keyword: string }) => {
     <div>
       {score}
 
-      <p className="text-grey600 mt-2 text-center font-[500]">
+      <p className="text-grey600 mt-2 whitespace-nowrap text-center font-[500]">
         연관 영상의 조회수 평균{' '}
         <span className="text-primary500">
           {isNaN(totalIncreaseViews / totalVideoCount)
