89f270279d88d30cea1d3b7d2d5f4cc472349a55
mini <jma1020@naver.com>
2024-07-12 15:45:16 +0900

    fix : zChannelData -> zChannelSchema

--- packages/dto/src/lib/user/user.zod.ts
+++ packages/dto/src/lib/user/user.zod.ts
@@ -1,8 +1,8 @@
-import { zChannelData } from '../channel';
+import { zChannelSchema } from '../channel';
 import { dataObject } from '../common.model';
 import { zUserModel } from './user.model';
 import { z } from 'zod';
 export const zOwnInfo = dataObject(
-  zUserModel.shape.data.merge(z.object({ channel: zChannelData.optional() })),
+  zUserModel.shape.data.merge(z.object({ channel: zChannelSchema.optional() })),
 );
 export type TOwnInfoRes = z.TypeOf<typeof zOwnInfo>;
