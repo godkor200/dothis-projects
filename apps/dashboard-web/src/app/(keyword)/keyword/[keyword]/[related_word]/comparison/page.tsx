4c21c758823bcd9b4843646b69f30c481680fa58
mini <jma1020@naver.com>
2024-07-16 09:42:06 +0900

    feat : 비교 탭 페이지 컴포넌트 사용 및 레이아웃

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/comparison/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/comparison/page.tsx
@@ -6,6 +6,10 @@ import { cn } from '@/utils/cn';
 
 import BoxFrame from '../../../BoxFrame';
 import SummaryChart from '../summary/SummaryChart';
+import ComparisonContainer from './ComparisonContainer';
+import ComparisonSummary from './ComparisonSummary';
+import RelatedKeywordList from './RelatedKeywordList';
+import SelectedKeywordProvider from './SelectedKeywordProvider';
 
 const Page = ({
   params,
@@ -23,50 +27,39 @@ const Page = ({
       >
         비교
       </Link>
+      <SelectedKeywordProvider relatedKeyword={relatedKeyword}>
+        <div className="grid grid-cols-[minmax(300px,1fr)_minmax(1000px,3fr)] gap-[20px]">
+          <BoxFrame>
+            <RelatedKeywordList
+              baseKeyword={baseKeyword}
+              relatedKeyword={relatedKeyword}
+            />
+          </BoxFrame>
 
-      <div className="grid grid-cols-[minmax(300px,1fr)_minmax(1000px,3fr)] gap-[20px]">
-        <BoxFrame>
-          <div>
-            <p className="text-grey600 mb-[30px] text-[14px] font-[500]">
-              {baseKeyword}의 다른 연관 키워드
-            </p>
-            <ul className="gap-30 flex flex-col py-[25px]">
-              {['치킨매니아', '가라아게', '맥도날드'].map(
-                (item, index, arr) => {
-                  const [blue, green] = arr.filter(
-                    (item) => item !== '맥도날드',
-                  );
+          <div className="grid grid-rows-[1fr_1fr] gap-[20px]">
+            <BoxFrame>
+              <div>
+                <p className="text-grey600 mb-[30px] text-[14px] font-[500]">
+                  키워드 분석 결과 비교
+                </p>
+                <ComparisonSummary
+                  baseKeyword={baseKeyword}
+                  relatedKeyword={relatedKeyword}
+                />
+              </div>
+            </BoxFrame>
 
-                  return (
-                    <li
-                      className={cn(
-                        'flex  items-center justify-between px-[36px] text-error ',
-                        {
-                          'text-[#34D399]': green === item,
-                          'text-[#818CF8]': blue === item,
-                        },
-                      )}
-                    >
-                      <span className="bg-grey200 h-[24px] w-[24px] rounded-[4px] text-center ">
-                        {index + 1}
-                      </span>
-                      <span className="text-[20px] font-bold">{item}</span>
-                    </li>
-                  );
-                },
-              )}
-            </ul>
+            <BoxFrame>
+              <div>
+                <p className="text-grey600 mb-[30px] text-[14px] font-[500]">
+                  키워드 분석 결과 비교
+                </p>
+                <SummaryChart />
+              </div>
+            </BoxFrame>
           </div>
-        </BoxFrame>
-        <BoxFrame>
-          <div>
-            <p className="text-grey600 mb-[30px] text-[14px] font-[500]">
-              키워드 분석 결과 비교
-            </p>
-            <SummaryChart />
-          </div>
-        </BoxFrame>
-      </div>
+        </div>
+      </SelectedKeywordProvider>
     </div>
   );
 };
