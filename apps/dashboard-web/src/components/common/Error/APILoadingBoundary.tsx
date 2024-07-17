4a7301f591971d9609c18b5cfb095a649db111f5
mini <jma1020@naver.com>
2024-07-18 07:08:00 +0900

    fix : APILoadingBoundary fallback 삽입이 되도록 수정

--- apps/dashboard-web/src/components/common/Error/APILoadingBoundary.tsx
+++ apps/dashboard-web/src/components/common/Error/APILoadingBoundary.tsx
@@ -1,18 +1,25 @@
+import { Suspense } from 'react';
+
 import BoxLoadingComponent from '@/app/(keyword)/keyword/BoxLoadingComponent';
 import { cn } from '@/utils/cn';
 
 const APILoadingBoundary = ({
   isLoading,
+  loadingComponent,
   children,
 }: {
   isLoading: boolean;
+  loadingComponent?: React.ReactNode;
   children: React.ReactNode;
 }) => {
   if (isLoading) {
     return (
-      <BoxLoadingComponent
-        classname={cn('absolute top-0 right-3 w-[80px] h-[80px]')}
-      />
+      <>
+        <BoxLoadingComponent
+          classname={cn('absolute top-0 right-3 w-[80px] h-[80px]')}
+        />
+        {loadingComponent}
+      </>
     );
   }
 
