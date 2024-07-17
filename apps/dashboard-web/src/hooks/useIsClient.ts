57b17424ff223dcc179fe1c6068a1a11467ab3e5
mini <jma1020@naver.com>
2024-07-17 12:52:12 +0900

    feat : 서버사이드 에러로 인한 useIsClient hook 생성

---
+++ apps/dashboard-web/src/hooks/useIsClient.ts
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
+
+export const useIsClient = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
