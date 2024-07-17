d62e3da6da9831094531d0511b64e9208b8a0b11
mini <jma1020@naver.com>
2024-07-18 07:52:19 +0900

    remove : import

--- apps/dashboard-web/src/app/(keyword)/keyword/BoxLoadingComponent.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/BoxLoadingComponent.tsx
@@ -2,8 +2,6 @@
 
 import Lottie from 'lottie-react';
 import BoxLoading from 'public/aseets/Lottie/BoxLoading.json';
-import LoadingLottie from 'public/aseets/Lottie/loading-lagacy.json';
-import { useEffect, useState } from 'react';
 
 const BoxLoadingComponent = ({ classname }: { classname: string }) => {
   return (
