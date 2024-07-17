71ee2df23fdcff28b57423f839eeaae7320294a7
mini <jma1020@naver.com>
2024-07-18 07:23:14 +0900

    remove : import

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/ChartContainer.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/ChartContainer.tsx
@@ -2,16 +2,13 @@
 
 import { useState } from 'react';
 
-import ApiErrorComponent from '@/components/common/Charts/ApiErrorComponent ';
 import D3Axis from '@/components/common/Charts/D3Axis';
 import APIErrorBoundary from '@/components/common/Error/APIErrorBoundary';
 import APILoadingBoundary from '@/components/common/Error/APILoadingBoundary';
 import useGetDailyViewV2 from '@/hooks/react-query/query/useGetDailyViewV2';
 import useGetNaverSearchRatio from '@/hooks/react-query/query/useGetNaverSearchRatio';
 import useGetVideoUploadCount from '@/hooks/react-query/query/useGetVideoUploadCount';
-import { cn } from '@/utils/cn';
 
-import BoxLoadingComponent from '../BoxLoadingComponent';
 import ChartSummaryCards from './ChartSummaryCards';
 
 const ChartContainer = ({
