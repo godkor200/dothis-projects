b5b4d66351e69d1d7b4ee02604c6e997b4e12905
mini <jma1020@naver.com>
2024-07-24 10:06:02 +0900

    fix : Props 추가로 인한 props 주입

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/comparison/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/comparison/page.tsx
@@ -54,7 +54,10 @@ const Page = ({
                 <p className="text-grey600 mb-[30px] text-[14px] font-[500]">
                   키워드 분석 결과 비교
                 </p>
-                <SummaryChart />
+                <SummaryChart
+                  baseKeyword={baseKeyword}
+                  relatedKeyword={relatedKeyword}
+                />
               </div>
             </BoxFrame>
           </div>
