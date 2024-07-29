1d217fbf3dd92e4b8b1ebce892cc2b5b5f807588
mini <jma1020@naver.com>
2024-07-29 16:17:29 +0900

    feat : MediaFetchTimeHanlde 컴포넌트 생성 (rules of hook ->  반복문 속 hook 사용불가로 인하여 생성)

---
+++ apps/dashboard-web/src/app/(keyword)/MediaFetchTimeHandle.tsx
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+
+import { useGetRandomMedia } from '@/hooks/react-query/query/useGetRandomMedia';
+import type { TKeywords } from '@/types/common';
+
+import type { MediaList } from './MediaBanner';
+
+interface Props extends TKeywords {
+  currentIndex: number;
+  startIndexQuery?: number;
+  mediaType: 'youtube' | 'news';
+  visibleMediaCount: number;
+  mediaErrorCount: number;
+  setMediaErrorCount: React.Dispatch<React.SetStateAction<number>>;
+  setFetchTimeMediaList: React.Dispatch<React.SetStateAction<MediaList[]>>;
+}
+
+const MediaFetchTimeHanlde = ({
+  baseKeyword,
+  relatedKeyword,
+  currentIndex,
+  startIndexQuery,
+  mediaType,
+  visibleMediaCount,
+  mediaErrorCount,
+  setMediaErrorCount,
+  setFetchTimeMediaList,
+}: Props) => {
+  console.log(visibleMediaCount, mediaErrorCount, currentIndex);
+  const randomMedia = useGetRandomMedia({
+    searchKeyword:
+      visibleMediaCount + mediaErrorCount > currentIndex
+        ? baseKeyword
+        : undefined,
+    relatedkeyword:
+      visibleMediaCount + mediaErrorCount > currentIndex
+        ? relatedKeyword
+        : undefined,
+    mediaCategory: mediaType,
+    page: startIndexQuery,
+    index: 0,
+    setIndexNumber: setMediaErrorCount,
+  });
+
+  const { mediaResult, mediaCategory, fetchTime, searchKeyword } = randomMedia;
+
+  useEffect(() => {
+    if (mediaResult && fetchTime) {
+      setFetchTimeMediaList((prev) => {
+        const updatedList = prev.filter(
+          (item) => item.baseKeyword !== searchKeyword,
+        );
+        return [
+          ...updatedList,
+          {
+            baseKeyword: searchKeyword!,
+            mediaData: mediaResult,
+            mediaType: mediaCategory,
+            fetchTime,
+          },
+        ];
+      });
+    }
+  }, [JSON.stringify(randomMedia.mediaResult), fetchTime]);
+
+  return null;
+};
+
+export default MediaFetchTimeHanlde;
