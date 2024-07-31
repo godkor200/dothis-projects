bfb53dffd0c86cd3af5f48ebda1ed815a52b2d8b
mini <jma1020@naver.com>
2024-07-31 15:22:34 +0900

    feat : useD3HoverLine 생성

---
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/summary/useD3HoverLine.ts
@@ -0,0 +1,81 @@
+import type * as D3 from 'd3';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+
+import type { DataItem } from './SummaryChart';
+
+interface LineRef {
+  //   line: D3.Line<DataItem>;
+  render: () => void;
+  remove: () => void;
+}
+
+interface Dimensions {
+  width: number;
+  height: number;
+  marginLeft: number;
+  marginRight: number;
+  marginTop: number;
+  marginBottom: number;
+}
+
+interface Props {
+  chartSelector: D3.Selection<D3.BaseType, unknown, HTMLElement, any>;
+  data: DataItem[];
+  dimensions: Dimensions;
+  xScale: D3.ScaleBand<string> | undefined;
+}
+
+const useD3HoverLine = ({ chartSelector, data, dimensions, xScale }: Props) => {
+  const { width, height, marginTop, marginRight, marginBottom, marginLeft } =
+    dimensions;
+
+  const lineHoverRef = useRef<LineRef | null>(null);
+
+  const hoverLineGroup = chartSelector.select('.hover-line-group');
+
+  //   console.log(hoverLineGroup.empty());
+  //   if (hoverLineGroup.empty()) {
+  //     chartSelector.append('g').attr('class', 'hover-line-group');
+  //   }
+
+  useImperativeHandle(
+    lineHoverRef,
+    () => ({
+      render: () => {
+        if (!xScale) return;
+
+        chartSelector
+          .append('g')
+          .attr('class', 'hover-line-group')
+          .selectAll('rect')
+          .data(data)
+          .enter()
+          .append('rect')
+          .attr(
+            'x',
+            (d) => (xScale(d?.date) as number) + xScale.bandwidth() / 2,
+          )
+          .attr('y', marginTop)
+          .attr('width', 1)
+          .attr('height', height - marginBottom - marginTop)
+          .attr('fill', '#A1A1AA')
+          .style('opacity', 0);
+      },
+      remove: () => {
+        hoverLineGroup.transition().duration(1000).style('opacity', 0).remove();
+      },
+    }),
+    [width, data],
+  );
+
+  return { lineHoverRef, hoverLineGroup };
+};
+
+export default useD3HoverLine;
