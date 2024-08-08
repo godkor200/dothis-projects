79b9d627a80fccf2f358aa05ffe134d2564ae24e
mini <jma1020@naver.com>
2024-08-08 12:04:26 +0900

    refactor : bar update 코드 추가 및 remove 시퀀스 추가

--- apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/summary/useD3Bars.ts
+++ apps/dashboard-web/src/app/(keyword)/keyword/[keyword]/[related_word]/summary/useD3Bars.ts
@@ -1,4 +1,4 @@
-import type * as D3 from 'd3';
+import * as D3 from 'd3';
 import {
   forwardRef,
   useEffect,
@@ -30,7 +30,11 @@ interface Props {
   chartSelector: D3.Selection<D3.BaseType, unknown, HTMLElement, any>;
   data: DataItem[][];
   dimensions: Dimensions;
-  styleMethods?: ((selection: D3.Selection<any, any, any, any>) => void)[];
+  styleMethod?: (
+    selection: D3.Selection<any, any, any, any>,
+    index: number,
+    isUpdate: boolean,
+  ) => void;
   xPositionMethod?: (
     xScale: D3.ScaleBand<string>,
     data: DataItem,
@@ -39,6 +43,7 @@ interface Props {
   xScale: D3.ScaleBand<string> | undefined;
   yScale: D3.ScaleLinear<number, number, never>[] | undefined;
   title: string;
+  keywordList: string[];
 }
 
 function usePrevious<T>(state: T) {
@@ -58,8 +63,9 @@ const useD3Bar = ({
   xScale,
   yScale,
   title,
-  styleMethods,
+  styleMethod,
   xPositionMethod,
+  keywordList,
 }: Props) => {
   const { width, height, marginTop, marginRight, marginBottom, marginLeft } =
     dimensions;
@@ -74,17 +80,62 @@ const useD3Bar = ({
       render: () => {
         if (!xScale || !yScale) return; // Ensure scales are defined before rendering
 
+        chartSelector.selectAll("[class*='bar-']").interrupt();
+
+        chartSelector.selectAll('.to-remove').remove();
+
+        chartSelector.selectAll("[class*='bar-']").each(function () {
+          const bar = D3.select(this);
+          const barClass = bar.attr('class');
+
+          const shouldRemove = keywordList.every(
+            (keyword) =>
+              !barClass
+                .split(' ')
+                .some((className) => className.includes(keyword)),
+          );
+
+          if (shouldRemove) {
+            bar
+              .classed('to-remove', true)
+              .transition()
+              .duration(200)
+              .style('opacity', 0)
+              .on('end', function () {
+                // transition이 끝난 후 요소 제거
+                if (bar.classed('to-remove')) {
+                  bar.remove();
+                }
+              }); // Remove the element
+          }
+        });
+
+        const arrayExists = (
+          dataArray: typeof prevData,
+          target: (typeof prevData)[number],
+        ) => {
+          return dataArray.some(
+            (subArray) =>
+              subArray.length === target.length &&
+              subArray.every(
+                (item, index) =>
+                  item.date === target[index].date &&
+                  item.value === target[index].value,
+              ),
+          );
+        };
+
         data.forEach((dataset, index) => {
           const y = yScale[index];
 
           const bars = chartSelector
-            .selectAll<SVGRectElement, DataItem>(`.bar-${title}-${index}`)
+            .selectAll<SVGRectElement, DataItem>(`.bar-${keywordList[index]}`)
             .data<DataItem>(dataset, (d) => d.date);
 
           bars
             .enter()
             .append('rect')
-            .attr('class', `bar-${title}-${index}`)
+            .attr('class', `bar-${keywordList[index]}`)
             .attr('x', (d) => {
               if (xPositionMethod) {
                 return xPositionMethod(xScale, d, index);
@@ -98,44 +149,73 @@ const useD3Bar = ({
             .attr('y', (d) => y(0) as number)
             .attr('width', 10) // Adjust bar width to prevent overlap
             .attr('height', 0)
-            .attr('rx', 5)
+            // .attr('rx', 5)
             .attr('fill', '#818cf8')
-            .call((bar) => {
-              if (styleMethods && styleMethods[index]) {
-                styleMethods[index](bar);
-              }
+            .call((selection) => {
+              styleMethod && styleMethod(selection, index, false);
             })
             .transition()
-            .delay(1000)
+
             .attr('y', (d) => y(d.value))
             .attr('height', (d) => y(0) - y(d.value));
 
-          if (JSON.stringify(prevData) !== JSON.stringify(data)) {
+          bars.call((selection) => {
+            styleMethod && styleMethod(selection, index, true);
+          });
+
+          if (arrayExists(prevData, dataset)) {
             bars
-              .transition()
-              .duration(1000)
+              .attr('x', (d) => {
+                if (xPositionMethod) {
+                  return xPositionMethod(xScale, d, index);
+                } else {
+                  return (
+                    (xScale(d.date) as number) +
+                    index * (xScale.bandwidth() / data.length)
+                  );
+                }
+              })
+              .attr('y', (d) => y(d.value))
+              .attr('height', (d) => y(0) - y(d.value))
+              .style('opacity', 1);
+          } else {
+            bars
+              .attr('x', (d) => {
+                if (xPositionMethod) {
+                  return xPositionMethod(xScale, d, index);
+                } else {
+                  return (
+                    (xScale(d.date) as number) +
+                    index * (xScale.bandwidth() / data.length)
+                  );
+                }
+              })
               .attr('y', y(0))
               .attr('height', 0)
               .transition()
-              .delay(500)
+              .duration(500)
+
+              .transition()
+
               .attr('y', (d) => y(d.value))
-              .attr('height', (d) => y(0) - y(d.value));
+              .attr('height', (d) => y(0) - y(d.value))
+              .style('opacity', 1);
           }
 
-          bars
-            .exit()
-            .transition()
-            .duration(1000)
-            .attr('y', y(0))
-            .attr('height', 0)
-            .remove();
+          // bars
+          //   .exit()
+          //   .transition()
+          //   .duration(1000)
+          //   .attr('y', y(0))
+          //   .attr('height', 0)
+          //   .remove();
         });
       },
 
       remove: () => {
         data.forEach((_, index) => {
           chartSelector
-            .selectAll(`.bar-${title}-${index}`)
+            .selectAll(`.bar-${keywordList[index]}`)
             .transition()
             .duration(500)
             .style('opacity', 0)
