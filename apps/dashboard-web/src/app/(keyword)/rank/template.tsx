c80ecbdd73ded0f09c8675ea4268e57a00944ddb
mini <jma1020@naver.com>
2024-08-09 13:35:09 +0900

    feat : rank - template 생성

---
+++ apps/dashboard-web/src/app/(keyword)/rank/template.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import * as Dialog from '@radix-ui/react-dialog';
+import type { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+import React, {
+  Children,
+  cloneElement,
+  createContext,
+  isValidElement,
+  type PropsWithChildren,
+  useState,
+} from 'react';
+
+import SearchGNB from '@/components/common/Layout/SearchGNB';
+import SvgComp from '@/components/common/SvgComp';
+
+import OpenFilterContextProvider from './OpenFilterContext';
+import TrendingQueryContextProvider, {
+  useTrendingQueryContext,
+} from './TrendingQueryContext';
+
+const TrendingPageTemplate = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // 필터링 컴포넌트로 인한 최상단 div 밒 fixed 버그를 활용한 translate
+    <>
+      <OpenFilterContextProvider>
+        <TrendingQueryContextProvider>
+          <>{children}</>
+        </TrendingQueryContextProvider>
+      </OpenFilterContextProvider>
+    </>
+  );
+};
+
+export default TrendingPageTemplate;
