2e61d62d6fd3be2e266f82f8d35a89a7fc0f6979
mini <jma1020@naver.com>
2024-08-14 15:28:01 +0900

    feat : NAVER_ADS_KEY 쿼리키 추가

--- apps/dashboard-web/src/constants/querykey.ts
+++ apps/dashboard-web/src/constants/querykey.ts
@@ -20,6 +20,7 @@ export const ADS_INFORMATION_KEY = createQueryKeys('ads_information');
 export const ADS_LIST_KEY = createQueryKeys('ads_list');
 export const DURATION_VIEWS_KEY = createQueryKeys('duration_views');
 export const NAVER_SEARCH_RATIO_KEY = createQueryKeys('naver_search_ratio');
+export const NAVER_ADS_KEY = createQueryKeys('naver_ads');
 export const NEWS_DATA_KEY = createQueryKeys('news_data');
 
 export const UPLOAD_VIDEO_COUNT_KEY = createQueryKeys('upload_video_count');
