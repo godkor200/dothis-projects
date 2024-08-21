7606d6a9f36cc42425dc63337c09d86dc0d7986a
mini <jma1020@naver.com>
2024-08-21 10:55:30 +0900

    feat : naverAds 유틸리티 추가

--- apps/dashboard-web/src/utils/naver-search/common.ts
+++ apps/dashboard-web/src/utils/naver-search/common.ts
@@ -1,4 +1,7 @@
-import type { NaverAPI_Response } from '@/hooks/react-query/query/useGetNaverSearchRatio';
+import type {
+  NaverAPI_Response,
+  NaverAPI_Results,
+} from '@/hooks/react-query/query/useGetNaverSearchRatio';
 
 export const getNaver_FluctuationRate = (
   data: NaverAPI_Response | undefined,
@@ -16,3 +19,36 @@ export const getNaver_FluctuationRate = (
     ) - 100
   );
 };
+
+export const handleNaverAdsLessThanToNumber = (queryCount: string | number) => {
+  if (queryCount === '<10') {
+    return 10; // "<10"을 9로 변환
+  }
+  const count = Number(queryCount);
+
+  return isNaN(count) ? 0 : count; // 숫자가 아닐 경우 0 반환
+};
+
+// export const getDailyQcCount = (queryCount: number, queryCount2: number) =>
+//   (queryCount + queryCount2) / 30;
+
+export const getTotalSearchRatio = (
+  searchRatioData: NaverAPI_Results['data'] | undefined,
+) => {
+  return searchRatioData
+    ? searchRatioData.reduce(
+        (total, { ratio }) => total + Math.floor(Number(ratio)),
+        0,
+      )
+    : 0;
+};
+
+export const calculateNormalizedSearchCount = ({
+  dailyRatio,
+  totalRatio,
+  totalQcCount,
+}: {
+  dailyRatio: number;
+  totalRatio: number;
+  totalQcCount: number;
+}) => (dailyRatio / totalRatio) * totalQcCount;
