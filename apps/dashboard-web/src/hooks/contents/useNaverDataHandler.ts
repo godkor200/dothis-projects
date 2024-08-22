55ad1b8ac76cb3c23f5480df84266ab4e3852f96
mini <jma1020@naver.com>
2024-08-22 11:09:36 +0900

    refactor : useNaverDataHandler api query 코드 분리

--- apps/dashboard-web/src/hooks/contents/useNaverDataHandler.ts
+++ apps/dashboard-web/src/hooks/contents/useNaverDataHandler.ts
@@ -4,31 +4,31 @@ import {
   handleNaverAdsLessThanToNumber,
 } from '@/utils/naver-search/common';
 
+import type { NaverAdsAPI_Response } from '../react-query/query/useGetNaverAds';
 import useGetNaverAds from '../react-query/query/useGetNaverAds';
+import type { NaverAPI_Response } from '../react-query/query/useGetNaverSearchRatio';
 import useGetNaverSearchRatio from '../react-query/query/useGetNaverSearchRatio';
 
-const useNaverDataHandler = ({ baseKeyword, relatedKeyword }: TKeywords) => {
+const useNaverDataHandler = ({
+  naverSearchRatioData,
+  naverAdsData,
+}: {
+  naverSearchRatioData: NaverAPI_Response | undefined;
+  naverAdsData: NaverAdsAPI_Response | undefined;
+}) => {
   /**
    * 네이버 통합검색량 수치 (퍼센테이지)
    */
 
-  const { data } = useGetNaverSearchRatio({
-    keyword: baseKeyword,
-    relword: relatedKeyword,
-  });
+  const dailyRatioList = naverSearchRatioData?.results[0].data;
 
-  const dailyRatioList = data?.results[0].data;
-
-  const totalSearchRatio = getTotalSearchRatio(data?.results[0].data);
+  const totalSearchRatio = getTotalSearchRatio(
+    naverSearchRatioData?.results[0].data,
+  );
 
   /**
    * 네이버 검색광고 (수치)
    */
-
-  const { data: naverAdsData } = useGetNaverAds({
-    baseKeyword,
-    relatedKeyword,
-  });
 
   const adsDataConfig = naverAdsData?.data.keywordList[0];
 
