653911b1d515cf88fe95161f59aa59a66a8cd890
mini <jma1020@naver.com>
2024-08-23 13:20:28 +0900

    docs

--- apps/dashboard-web/src/app/(keyword)/channel/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/channel/page.tsx
@@ -1,3 +1,4 @@
+// channel에 대한 페이지는 다른 사람의 채널에 대한 데이터를 볼 수 있는 구조가 아니다 보니깐,  dynamic Route 구조로 굳이할 필요가 없다고 생각함
 const Page = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center text-[48px] font-bold">
