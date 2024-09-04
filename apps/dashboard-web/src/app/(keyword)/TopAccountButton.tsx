672081fae0a2718040a3fd59f1f6f8c384074ee6
mini <jma1020@naver.com>
2024-09-04 15:20:00 +0900

    feat : TopAccountButton 컴포넌트 생성 (Dropdown 컴포넌트)

---
+++ apps/dashboard-web/src/app/(keyword)/TopAccountButton.tsx
@@ -0,0 +1,127 @@
+import './styles.css';
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+import SvgComp from '@/components/common/SvgComp';
+import useGetUserInfo from '@/hooks/react-query/query/useGetUserInfo';
+
+const TopAccountButton = () => {
+  const { data } = useGetUserInfo();
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <div className="rounded-8 flex cursor-pointer items-center gap-[10px] p-2 hover:shadow-md hover:transition-all">
+          <div className="rounded-8 bg-grey200 cursor-pointer p-3">
+            <SvgComp
+              icon="HeaderUserProfile"
+              size={24}
+              className="[&_path]:stroke-grey400 [&_path]:fill-grey400"
+            />
+          </div>
+
+          <div>
+            <p className="text-grey900 text-[14px]">{data?.userEmail}</p>
+            <p className="text-grey500 text-[12px]">{data?.plan}</p>
+          </div>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+          <DropdownMenu.Item>
+            <>
+              New Tab <div className="RightSlot">⌘+T</div>
+            </>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Window <div className="RightSlot">⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem" disabled>
+            New Private Window <div className="RightSlot">⇧+⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
+              More Tools
+              <div className="RightSlot">{/* <ChevronRightIcon /> */}</div>
+            </DropdownMenu.SubTrigger>
+            <DropdownMenu.Portal>
+              <DropdownMenu.SubContent
+                className="DropdownMenuSubContent"
+                sideOffset={2}
+                alignOffset={-5}
+              >
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Save Page As… <div className="RightSlot">⌘+S</div>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Create Shortcut…
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Name Window…
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator className="DropdownMenu.Separator" />
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Developer Tools
+                </DropdownMenu.Item>
+              </DropdownMenu.SubContent>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Sub>
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.CheckboxItem
+            className="DropdownMenuCheckboxItem"
+            // checked={bookmarksChecked}
+            // onCheckedChange={setBookmarksChecked}
+          >
+            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+              {/* <CheckIcon /> */}
+            </DropdownMenu.ItemIndicator>
+            Show Bookmarks <div className="RightSlot">⌘+B</div>
+          </DropdownMenu.CheckboxItem>
+          <DropdownMenu.CheckboxItem
+            className="DropdownMenuCheckboxItem"
+            // checked={urlsChecked}
+            // onCheckedChange={setUrlsChecked}
+          >
+            <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+              {/* <CheckIcon /> */}
+            </DropdownMenu.ItemIndicator>
+            Show Full URLs
+          </DropdownMenu.CheckboxItem>
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.Label className="DropdownMenuLabel">
+            People
+          </DropdownMenu.Label>
+          <DropdownMenu.RadioGroup>
+            <DropdownMenu.RadioItem
+              className="DropdownMenuRadioItem"
+              value="pedro"
+            >
+              <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                {/* <DotFilledIcon /> */}
+              </DropdownMenu.ItemIndicator>
+              Pedro Duarte
+            </DropdownMenu.RadioItem>
+            <DropdownMenu.RadioItem
+              className="DropdownMenuRadioItem"
+              value="colm"
+            >
+              <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                {/* <DotFilledIcon /> */}
+              </DropdownMenu.ItemIndicator>
+              Colm Tuite
+            </DropdownMenu.RadioItem>
+          </DropdownMenu.RadioGroup>
+
+          <DropdownMenu.Arrow className="DropdownMenuArrow" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+};
+
+export default TopAccountButton;
