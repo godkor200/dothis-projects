8369d6f51b89b64982cc205649aa6abd69442496
mini <jma1020@naver.com>
2024-09-05 12:58:35 +0900

    feat : 임시 plans 페이지  생성

--- apps/dashboard-web/src/app/(keyword)/account/plans/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/account/plans/page.tsx
@@ -1,3 +1,21 @@
-const Page = () => {};
+'use client';
+
+import MembershipModalFrame from '@/components/Membership/MembershipModalFrame';
+import Price from '@/components/Pricing';
+import useGetUserInfo from '@/hooks/react-query/query/useGetUserInfo';
+
+const Page = () => {
+  const { data: userData } = useGetUserInfo();
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bg-grey200 rounded-10 mb-[20px] flex w-[600px] flex-col gap-[20px] p-10">
+        <p className="mb-[10px] text-[16px] font-bold">내 멤버십</p>
+        <p className="text-[14px] font-[500]">{userData?.plan}</p>
+      </div>
+      <MembershipModalFrame />
+    </div>
+  );
+};
 
 export default Page;
