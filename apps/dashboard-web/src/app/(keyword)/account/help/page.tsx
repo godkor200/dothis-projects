a82d9eb25f096756c9d7ada81655e6fc7e79dce4
mini <jma1020@naver.com>
2024-09-05 12:57:26 +0900

    feat : 임시 help 페이지 생성

--- apps/dashboard-web/src/app/(keyword)/account/help/page.tsx
+++ apps/dashboard-web/src/app/(keyword)/account/help/page.tsx
@@ -1,3 +1,32 @@
-const Page = () => {};
+const Page = () => {
+  return (
+    <div className="flex  flex-col ">
+      <h2 className="mb-[20px] text-[20px] font-bold">Q & A</h2>
+
+      <div className=" grid grid-cols-[1fr,3fr]">
+        {/* Header */}
+        <div className="text-grey500 border-grey400 border-b py-[30px] text-[14px] font-bold">
+          구분
+        </div>
+        <div className="text-grey500 border-grey400 border-b py-[30px] text-[14px] font-bold">
+          제목
+        </div>
+        {/* Rows */}
+        <div className="text-grey500 border-grey200 border-b-2 py-[30px] text-[14px] font-bold">
+          결제
+        </div>
+        <div className="text-grey500 border-grey200 border-b-2 py-[30px] text-[14px] font-bold">
+          플랜은 어디서 구매하나요?
+        </div>
+        <div className="text-grey500 border-grey200 border-b-2 py-[30px] text-[14px] font-bold">
+          결제
+        </div>
+        <div className="text-grey500 border-grey200 border-b-2 py-[30px] text-[14px] font-bold">
+          카드 등록은 어디서 하나요?
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Page;
